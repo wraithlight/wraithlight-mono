34cc7853388f8cddcc5cfd2aba00428a5b4821bd
kfarkasHU <sohamar@farkask.eu>
2023-03-31 00:45:28 +0200

    refactor(guid): use randomNumberBetween() instead

--- applications/packages/core.types/src/guid/guid.fns.ts
+++ applications/packages/core.types/src/guid/guid.fns.ts
@@ -1,3 +1,5 @@
+import { randomNumberBetween } from "../_internal";
+
 import { Guid } from "./guid.type";
 
 /**
@@ -16,7 +18,7 @@ export function isGuid(guidLike: string): guidLike is Guid {
  */
 export function newGuid(): Guid {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random() * 16 | 0,
+        var r = randomNumberBetween() * 16 | 0,
           v = c == 'x' ? r : (r & 0x3 | 0x8);
         return v.toString(16);
     });
