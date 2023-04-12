50327cb5055ae2aa875f1c9cbb2896a4f6ae873d
kfarkasHU <sohamar@farkask.eu>
2023-04-12 22:17:38 +0200

    feat(auth): add logs to scopes

--- applications/apps/auth/node/src/_internal/const/scope-map.const.ts
+++ applications/apps/auth/node/src/_internal/const/scope-map.const.ts
@@ -7,5 +7,6 @@ export const SCOPE_NAME_MAP: {[key: string]: Guid } = Object.freeze({
     [LoginScope.GameApplication]: "8cf5631d-8bb4-4697-8c43-85b3a94da8dc",
     [LoginScope.Content]: "bb2dbe19-705d-4ddf-98a7-14c19a9c0c60",
     [LoginScope.UserManagement]: "a3d0db9e-3fb5-414a-9f6c-1bf034339322",
-    [LoginScope.Forum]: "8e61724d-ff95-4f8e-89d8-f644d8cb6c8c"
+    [LoginScope.Forum]: "8e61724d-ff95-4f8e-89d8-f644d8cb6c8c",
+    [LoginScope.Logs]: "7ac64569-5a99-4afb-9180-ca8c1bec3822"
 });
