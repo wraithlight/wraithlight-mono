da6453447c81a9c403d2b1c3a5ae15c3d3e7d2f6
kfarkasHU <sohamar@farkask.eu>
2023-04-12 23:36:17 +0200

    fix(core env static): folder structure

--- applications/packages/core.env-static/src/internal/server.model.ts
+++ applications/packages/core.env-static/src/internal/server.model.ts
@@ -1,5 +1,5 @@
 import { ServerAuthModel } from "./auth/server-auth.model";
-import { ServerLogsModel } from "./auth/server-logs.model";
+import { ServerLogsModel } from "./logs/server-logs.model";
 
 export interface ServerModel {
     auth: ServerAuthModel;
