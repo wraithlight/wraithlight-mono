e6a736cda135cb24a0d8ae1052a6f4b36008295a
kfarkasHU <sohamar@farkask.eu>
2023-04-14 15:06:30 +0200

    feat(content db): implement

---
+++ database/content/data.sh
@@ -0,0 +1,8 @@
+CONTAINER_NAME=wraithlight-db-content
+SQL_ROOT_PASSWORD=wraithlight-content-pw
+SQL_USERNAME=wraithlight-content-user-1
+SQL_PASSWORD=wraithlight-content-user-1-pw
+OUT_PORT=9002
+IN_PORT=3306
+DATABASE_NAME=WL_CONTENT
+NETWORK_NAME=WL_Internal
