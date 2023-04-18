d504000a3b7b589f8d605f9ecaa0008316fd62f4
kfarkasHU <sohamar@farkask.eu>
2023-04-18 14:59:15 +0200

    fix(content): value property

--- database/content/src/tables/content.sql
+++ database/content/src/tables/content.sql
@@ -1,7 +1,8 @@
 CREATE TABLE `Content` (
     `Id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `ApplicationId` VARCHAR(36) NOT NULL,
+    `Key` VARCHAR(64) NOT NULL,
     `Value` VARCHAR(MAX) NOT NULL,
+    `ApplicationId` VARCHAR(36) NOT NULL,
     `LanguageId` VARCHAR(36) NOT NULL,
     `Type` TINYINT(4) UNSIGNED NOT NULL,
     `IsActive` BIT NOT NULL,
