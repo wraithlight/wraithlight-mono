c5bf840d1fe712141232517bfb106dd8efbc6b23
kfarkasHU <sohamar@farkask.eu>
2023-04-18 14:59:00 +0200

    feat(application): isActive flag

--- database/content/src/tables/application.sql
+++ database/content/src/tables/application.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `Application` (
     `Id` VARCHAR(36) NOT NULL,
     `DisplayName` VARCHAR(64) NOT NULL,
-    `Value` VARCHAR(5) NOT NULL,
+    `Value` VARCHAR(64) NOT NULL,
+    `IsActive` BIT NOT NULL,
     PRIMARY KEY (`Id`)
 ) ENGINE = MyISAM DEFAULT CHARSET=utf8;
