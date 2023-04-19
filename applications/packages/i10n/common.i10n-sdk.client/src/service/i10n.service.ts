e6516ecdbc6ab08b69765acf2b4476501d8a0c2c
kfarkasHU <sohamar@farkask.eu>
2023-04-19 19:35:07 +0200

    feat(*): i10n lib

---
+++ applications/packages/i10n/common.i10n-sdk.client/src/service/i10n.service.ts
@@ -0,0 +1,18 @@
+import { Dictionary } from "@wraithlight/core.types";
+
+export class I10NService {
+
+    
+    constructor(
+        private readonly _dictionary: Dictionary<string>
+    ) { }
+
+    public translate(key: string): string {
+        const value = this._dictionary.find(key);
+        if (!value) {
+            return key;
+        }
+        return value;
+    }
+
+}
