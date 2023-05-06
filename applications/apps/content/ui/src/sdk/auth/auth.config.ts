cb2ff17faf826445e58248fb2002aaa35d373589
kfarkasHU <sohamar@farkask.eu>
2023-05-06 23:00:17 +0200

    feat(content ui): add base url for auth

--- applications/apps/content/ui/src/sdk/auth/auth.config.ts
+++ applications/apps/content/ui/src/sdk/auth/auth.config.ts
@@ -1,1 +1,3 @@
-export const AUTH_API_BASE_URL = "";
+import { COMMON_STATIC } from "@wraithlight/core.env-static";
+
+export const AUTH_API_BASE_URL = `${COMMON_STATIC.content.address.host}:${COMMON_STATIC.content.address.port}`;
