4cd47d2f058e87cf504ccd89bd9cee27a4d48195
kfarkasHU <sohamar@farkask.eu>
2023-05-06 23:01:04 +0200

    feat(content node): implement

---
+++ applications/apps/content/node/src/server.ts
@@ -0,0 +1,22 @@
+import { COMMON_STATIC } from "@wraithlight/core.env-static";
+import {
+    ControllerBinder,
+    createServer
+} from "@wraithlight/core.node";
+import { ServerAuthControllerV1 } from "@wraithlight/common.auth-sdk.server";
+import { LoginScope } from "@wraithlight/auth/core.auth.types";
+
+const CONTROLLERS = [
+    new ServerAuthControllerV1(LoginScope.Content)
+];
+
+const server = createServer(true);
+
+ControllerBinder.bindControllers(
+    server.app,
+    CONTROLLERS
+)
+
+server.start(COMMON_STATIC.content.address.port, () => {
+    console.log(`CONTENT server is running at port ${COMMON_STATIC.content.address.port}`)
+});
