bff7b0eba2e5ff1bef900236597e9c30ad8c8788
kfarkasHU <sohamar@farkask.eu>
2023-05-06 18:25:58 +0200

    style(core logs common): align to strict

--- applications/packages/core.logs-common/src/model/request/entry.model.ts
+++ applications/packages/core.logs-common/src/model/request/entry.model.ts
@@ -1,9 +1,11 @@
+import { Nullable } from "@wraithlight/core.types";
+
 import { ApplicationName, LogSeverity } from "../../enum";
 
 export interface EntryRequest {
     severity: LogSeverity;
     application: ApplicationName;
     message: string;
-    additionalFields?: string;
+    additionalFields: Nullable<string>;
     logDate: Date;
 }
