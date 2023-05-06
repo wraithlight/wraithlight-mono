a7c257dd04c3bdd5dc522967cb0199844cfb7efe
kfarkasHU <sohamar@farkask.eu>
2023-05-06 16:10:16 +0200

    feat(game ws ui): render empty scene

--- applications/apps/game-website/ui/src/app/component/body/body.component.ts
+++ applications/apps/game-website/ui/src/app/component/body/body.component.ts
@@ -1,8 +1,20 @@
 import "./body.component.scss";
 
+import { GameContextService } from "@wraithlight/game.context";
+import { observable } from "knockout";
+
 import { addComponent } from "../../../framework";
 
 export class BodyComponent {
+
+    private readonly _gameContextService = new GameContextService();
+
+    public readonly canvasId = observable<string>("gameContent");
+
+    public koDescendantsComplete(): void {
+        const canvas = document.getElementById(this.canvasId()) as HTMLCanvasElement;
+        this._gameContextService.initGame(canvas, "");
+    }
 
 }
 
