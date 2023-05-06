4462cc03f33f240548b422df89e8c89d090460e7
kfarkasHU <sohamar@farkask.eu>
2023-05-06 16:28:47 +0200

    chore(db): build and run

--- database/run.sh
+++ database/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-for d in ./*/ ; do (
+for d in $(find . -not -path ./.build -mindepth 1 -maxdepth 1 -type d); do (
+    echo "============="
+    echo $d;
     cd "$d" &&
     source ./build.sh &&
     source ./run.sh &&
