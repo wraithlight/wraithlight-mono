2019da4627cb74e5c74cdf9a498815db47e8cfe3
kfarkasHU <sohamar@farkask.eu>
2023-05-06 16:09:10 +0200

    feat(game scene): lib implementation

---
+++ applications/packages/game.scene/src/scene.service.ts
@@ -0,0 +1,36 @@
+import { Color } from "@wraithlight/game.contract";
+import { LayerService } from "@wraithlight/game.layer";
+
+export class SceneService {
+
+    private readonly _layerService = new LayerService(this._canvas);
+
+    constructor(
+        private readonly _canvas: HTMLCanvasElement
+    ) { }
+
+    public renderLoginScene(): void {
+        this._layerService.reset();
+        const background = this._layerService.addLayer();
+        const form = this._layerService.addLayer();
+
+        background.fillColor(
+            { x: 0, y: 0 },
+            { x: this._canvas.width, y: this._canvas.height },
+            Color.MISSING_TEXTURE
+        );
+    }
+
+    public renderLoadingScene(): void {
+        this._layerService.reset();
+    }
+
+    public renderCharacterSelectionScene(): void {
+        this._layerService.reset();
+    }
+
+    public renderGameScene(): void {
+        this._layerService.reset();
+    }
+
+}
