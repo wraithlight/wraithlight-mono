ca6152d56f3045110a547182bd943d8d15ef9ac9
kfarkasHU <sohamar@farkask.eu>
2023-05-06 22:53:39 +0200

    refactor(core sql): select rejection error

--- applications/packages/core.sql/src/context/query/select.query-context.ts
+++ applications/packages/core.sql/src/context/query/select.query-context.ts
@@ -51,7 +51,7 @@ export class SelectQueryContext<T extends Object>
         return new Promise((resolve, reject) => {
             this._context.Connection.execute(command, (error, rows) => {
                 if (error) {
-                    reject();
+                    reject(error);
                 }
                 const rawResult = rows && rows.length && rows.length > 0
                     ? rows
