9228c70bdd163d68a044607037f0057b27b54bd4
kfarkasHU <sohamar@farkask.eu>
2023-05-07 20:26:03 +0200

    refactor(core auth sdk): move api models to proper place

--- applications/packages/auth/core.auth.types/src/types/response/index.ts
+++ applications/packages/auth/core.auth.types/src/types/response/index.ts
@@ -4,4 +4,3 @@ export * from "./keep-alive-session-response.model";
 export * from "./login-response.model";
 export * from "./logout-response.model";
 export * from "./validate-session-response.model";
-export * from "./register-response.model";
