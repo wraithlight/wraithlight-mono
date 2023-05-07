bef02aebb050aa05f8ce50081d945027a7bbafc4
kfarkasHU <sohamar@farkask.eu>
2023-05-07 21:09:47 +0200

    feat(auth sdk): account controller

---
+++ applications/packages/auth/common.auth-sdk.server/src/controller/account.controller.ts
@@ -0,0 +1,38 @@
+import {
+    BaseController,
+    HttpController,
+    HttpPost
+} from "@wraithlight/core.node";
+import { API_ROUTES } from "@wraithlight/core.auth.constant";
+import { ApiRegisterErrorResponse, ApiRegisterSuccessResponse, RegisterErrorResponse, RegisterRequest, RegisterSuccessResponse } from "@wraithlight/core.auth.types";
+
+import { ServerAccountService } from "../service";
+
+@HttpController(API_ROUTES.v1.account.root)
+export class ServerAccountControllerV1 extends BaseController {
+
+    private readonly _accountService = new ServerAccountService();
+
+    @HttpPost(API_ROUTES.v1.account.register)
+    public async login(model: RegisterRequest): Promise<void> {
+        const result = await this._accountService.register(
+            model.username,
+            model.password,
+            model.passwordVerify,
+            model.emailAddress
+        );
+        if (!result.success) {
+            const data: RegisterErrorResponse = {
+                success: false,
+                errors: (result as ApiRegisterErrorResponse).errors
+            }
+            return super.unauthorized(data);
+        }
+
+        const data: RegisterSuccessResponse = {
+            success: true
+        }
+        return super.ok(data);
+    }
+
+}
