563c754312516637e75999499172109dc0472d88
kfarkasHU <sohamar@farkask.eu>
2023-05-07 21:09:28 +0200

    feat(auth types): client side models

--- applications/packages/auth/core.auth.types/src/types/response/index.ts
+++ applications/packages/auth/core.auth.types/src/types/response/index.ts
@@ -4,3 +4,4 @@ export * from "./keep-alive-session-response.model";
 export * from "./login-response.model";
 export * from "./logout-response.model";
 export * from "./validate-session-response.model";
+export * from "./register-response.model";
