e25e749af8b0a63af52e09e6ebb937161206540e
kfarkasHU <sohamar@farkask.eu>
2023-05-07 20:20:50 +0200

    style(core auth types): mark register models as deprecated

--- applications/packages/core.auth-common/src/model/request/register.model.ts
+++ applications/packages/core.auth-common/src/model/request/register.model.ts
@@ -1,3 +1,6 @@
+/**
+ * @deprecated Use `RegisterRequest` from `core.auth.types` instead.
+ */
 export interface RegisterModel {
     username: string;
     emailAddress: string;
--- applications/packages/core.auth-common/src/model/response/register.model.ts
+++ applications/packages/core.auth-common/src/model/response/register.model.ts
@@ -1,11 +1,20 @@
+/**
+ * @deprecated Use `BaseRegisterResponse` from `core.auth.types` instead.
+ */
 export interface BaseRegisterResponse {
     success: boolean;
 }
 
+/**
+ * @deprecated Use `ErrorRegisterResponse` from `core.auth.types` instead.
+ */
 export interface ErrorRegisterResponse extends BaseRegisterResponse {
     errors: Array<string>;
 }
 
+/**
+ * @deprecated Use `SuccessRegisterResponse` from `core.auth.types` instead.
+ */
 export interface SuccessRegisterResponse extends BaseRegisterResponse {
     
 }
