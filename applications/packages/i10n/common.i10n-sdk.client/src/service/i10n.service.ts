c6ef1a04f8e0d2b0a23eb088b68b0a055e71e5ca
kfarkasHU <sohamar@farkask.eu>
2023-05-13 13:25:13 +0200

    refactor(i10n): singleton service

--- applications/packages/i10n/common.i10n-sdk.client/src/service/i10n.service.ts
+++ applications/packages/i10n/common.i10n-sdk.client/src/service/i10n.service.ts
@@ -1,10 +1,26 @@
-import { Dictionary } from "@wraithlight/core.types";
+import { Dictionary, Nullable } from "@wraithlight/core.types";
 
 export class I10NService {
 
-    constructor(
+    private static _instance: Nullable<I10NService>;
+
+    private constructor(
         private readonly _dictionary: Dictionary<string>
     ) { }
+
+    public static initialize(dictionary: Dictionary<string>): I10NService {
+        if (!this._instance) {
+            this._instance = new I10NService(dictionary);
+        }
+        return this._instance;
+    }
+
+    public static getInstance(): I10NService {
+        if (!this._instance) {
+            throw `The service has not been initialized!`;
+        }
+        return this._instance;
+    }
 
     public translate(key: string): string {
         const value = this._dictionary.find(key);
