dda1e8bd55888860374971ab2d2c23cbcb0315f3
kfarkasHU <sohamar@farkask.eu>
2023-05-18 21:43:13 +0200

    fix(content db): build and deployment

--- database/content/src/data/application.sql
+++ database/content/src/data/application.sql
@@ -1,69 +1,83 @@
 INSERT INTO `Application` (
     `Id`,
     `DisplayName`,
-    `Value`
+    `Value`,
+    `IsActive`
 ) VALUES (
     "7777ba27-da56-4ddd-ae97-f38a1e48effa",
     "Website",
-    "WEBSTIE"
+    "WEBSTIE",
+    1
 );
 
 INSERT INTO `Application` (
     `Id`,
     `DisplayName`,
-    `Value`
+    `Value`,
+    `IsActive`
 ) VALUES (
     "d97a2422-ed75-46cb-90c6-6172a3e5949e",
     "Game Website",
-    "GAME_WEBSITE"
+    "GAME_WEBSITE",
+    1
 );
 
 INSERT INTO `Application` (
     `Id`,
     `DisplayName`,
-    `Value`
+    `Value`,
+    `IsActive`
 ) VALUES (
     "8cf5631d-8bb4-4697-8c43-85b3a94da8dc",
     "Game Application",
-    "GAME_APPLICATION"
+    "GAME_APPLICATION",
+    1
 );
 
 INSERT INTO `Application` (
     `Id`,
     `DisplayName`,
-    `Value`
+    `Value`,
+    `IsActive`
 ) VALUES (
     "a3d0db9e-3fb5-414a-9f6c-1bf034339322",
     "User Management",
-    "USER_MANAGEMENT"
+    "USER_MANAGEMENT",
+    1
 );
 
 INSERT INTO `Application` (
     `Id`,
     `DisplayName`,
-    `Value`
+    `Value`,
+    `IsActive`
 ) VALUES (
     "8e61724d-ff95-4f8e-89d8-f644d8cb6c8c",
     "Forum",
-    "FORUM"
+    "FORUM",
+    1
 );
 
 INSERT INTO `Application` (
     `Id`,
     `DisplayName`,
-    `Value`
+    `Value`,
+    `IsActive`
 ) VALUES (
     "7ac64569-5a99-4afb-9180-ca8c1bec3822",
     "Logs",
-    "LOGS"
+    "LOGS",
+    1
 );
 
 INSERT INTO `Application` (
     `Id`,
     `DisplayName`,
-    `Value`
+    `Value`,
+    `IsActive`
 ) VALUES (
     "61723a11-1deb-43fe-92c8-834420051706",
     "Editor",
-    "EDITOR"
+    "EDITOR",
+    1
 );
--- database/content/src/data/language.sql
+++ database/content/src/data/language.sql
@@ -1,7 +1,7 @@
 INSERT INTO `Language` (
     `Id`,
-    `DisplayName`
-    `IsoCode`
+    `DisplayName`,
+    `IsoCode`,
     `IsActive`
 ) VALUES (
     "3685fd5d-d99c-4463-8061-c4bd9d7ccf89",
