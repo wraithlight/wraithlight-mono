27fb0e4a2beb22cc341553fac6598dd55b94dcae
kfarkasHU <sohamar@farkask.eu>
2023-05-19 19:34:38 +0200

    feat(user management const): lib

---
+++ applications/packages/user-management/core.user-management.constant/src/api-endpoints/api-endpoints.const.ts
@@ -0,0 +1,19 @@
+const API_V1_BASE = "/api/v1";
+const USER_V1 = `${API_V1_BASE}/user`
+
+export const API_ENDPOINTS = Object.freeze({
+    v1: {
+        user: {
+            root: USER_V1,
+            all: `${USER_V1}/all`,
+            byApplication: {
+                server: `${USER_V1}/application`,
+                client: (applicationId: string) => `${USER_V1}/application?id=${applicationId}`
+            },
+            id: {
+                server: `${USER_V1}/findById`,
+                client: (id: string) => `${USER_V1}/findById?id=${id}`
+            },
+        }
+    }
+});
