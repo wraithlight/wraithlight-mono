c2ed165aaf31ac4be8d4e72837acee9efd7dbc5e
kfarkasHU <sohamar@farkask.eu>
2023-05-21 21:18:41 +0200

    refactor(auth common): align to new structure

--- applications/packages/core.auth-common/src/constant/api-endpoint.const.ts
+++ applications/packages/core.auth-common/src/constant/api-endpoint.const.ts
@@ -1,20 +1,30 @@
 const API_BASE_URL = "/api";
-const API_V1 = "/v1";
+const API_V1 = "/external/v1";
 const AUTH_ROOT = "/auth";
 const ACCOUNT_ROOT = "/account";
 
 const V1_AUTH_ROOT = `${API_BASE_URL}${API_V1}${AUTH_ROOT}`;
 const V1_ACCOUNT_ROOT = `${API_BASE_URL}${API_V1}${ACCOUNT_ROOT}`;
 
+/**
+ * @deprecated
+ */
 export const AUTH_API_ENDPOINTS = Object.freeze({
-    root: V1_AUTH_ROOT,
-    login: "/login",
-    logout: "/logout",
-    sessionValid: "/validate",
-    keepAlive: "/keep-alive"
+    external: {
+        root: V1_AUTH_ROOT,
+        login: "/login",
+        logout: "/logout",
+        sessionValid: "/validate",
+        keepAlive: "/keep-alive"
+    }
 });
 
+/**
+ * @deprecated
+ */
 export const ACCOUNT_API_ENDPOINTS = Object.freeze({
-    root: V1_ACCOUNT_ROOT,
-    register: "/register"
+    external: {
+        root: V1_ACCOUNT_ROOT,
+        register: "/register"
+    }
 });
