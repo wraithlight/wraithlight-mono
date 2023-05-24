52562cf15ffb6b37c45acb166904e406109e07ab
kfarkasHU <sohamar@farkask.eu>
2023-05-24 18:55:49 +0200

    refactor(core logs types): applicaion name

--- applications/packages/logs/core.logs.types/src/model/entry.model.ts
+++ applications/packages/logs/core.logs.types/src/model/entry.model.ts
@@ -1,6 +1,8 @@
 import { LogSeverity } from "@wraithlight/core.logger.types";
+import { ApplicationName } from "@wraithlight/core.common-constant";
 
 export interface BeaconLogEntry {
     severity: LogSeverity;
     data: string;
+    applicationName: ApplicationName;
 }
