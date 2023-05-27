ac03f3bfa9cafd5dd47dd1cfd9d023772592d52e
kfarkasHU <sohamar@farkask.eu>
2023-05-27 13:07:34 +0200

    chore(editor node): build script

---
+++ applications/apps/editor/node/webpack.config.js
@@ -0,0 +1,37 @@
+const { resolve } = require("path");
+
+const config = (env) => {
+    const isProduction = env.production;
+    const filename = "node.js";
+    const cfg = {
+        mode: isProduction ? "production" : "development",
+        entry: [
+            "./src/main.ts"
+        ],
+        output: {
+            path: resolve(__dirname, "./dist"),
+            filename,
+            clean: true
+        },
+        target: "node",
+        node: {
+            __dirname: false,
+            __filename: false,
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: "ts-loader",
+                    exclude: /node_modules/
+                },
+            ]
+        },
+        resolve: {
+            extensions: [".ts", ".js"]
+        }
+    };
+    return cfg;
+};
+
+module.exports = config;
