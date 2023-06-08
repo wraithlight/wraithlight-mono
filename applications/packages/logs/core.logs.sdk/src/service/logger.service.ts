b9d7e6ad861430bb2bbc13300a2300f14b7588f4
kfarkasHU <sohamar@farkask.eu>
2023-06-08 16:25:55 +0200

    refactor(beacon logger): use core.http beacon client

--- applications/packages/logs/core.logs.sdk/src/service/logger.service.ts
+++ applications/packages/logs/core.logs.sdk/src/service/logger.service.ts
@@ -1,14 +1,14 @@
-import { getNavigatorRef } from "@wraithlight/core.dom";
 import { LogSeverity } from "@wraithlight/core.logger.types";
 import { BeaconLogEntry } from "@wraithlight/core.logs.types";
+import { HttpBeaconClient } from "@wraithlight/core.http";
 
 import { LoggerServiceConfig } from "./logger.config";
 import { ApplicationName } from "@wraithlight/core.common-constant";
 
 export class BeaconLoggerService {
 
-    private readonly _navigator = getNavigatorRef();
     private readonly _config = new LoggerServiceConfig();
+    private readonly _beaconService = new HttpBeaconClient();
 
     public logDebug(
         applicationName: ApplicationName,
@@ -49,7 +49,7 @@ export class BeaconLoggerService {
             data: JSON.stringify(data.map(m => typeof m === "object" ? JSON.stringify(m) : m))
         };
         const url = this._config.getLogUrl();
-        this._navigator.sendBeacon(url, JSON.stringify(entry));
+        this._beaconService.beacon(url, entry);
     }
 
 }
