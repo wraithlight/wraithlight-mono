b9efad77d002a3c26f34d184206950832c423020
kfarkasHU <sohamar@farkask.eu>
2023-07-14 16:39:42 +0200

    feat(auth): use env static config

--- applications/apps/auth/ui/src/sdk/auth/auth.config.ts
+++ applications/apps/auth/ui/src/sdk/auth/auth.config.ts
@@ -1,11 +1,3 @@
-// import { COMMON_STATIC } from "@wraithlight/core.env-static";
+import { COMMON_STATIC } from "@wraithlight/core.env-static";
 
-const COMMON_STATIC = {
-    website: {
-        address: {
-            host: "alma",
-            port: 2
-        }
-    }
-}
-export const AUTH_API_BASE_URL = `${COMMON_STATIC.website.address.host}:${COMMON_STATIC.website.address.port}`;
+export const AUTH_API_BASE_URL = `${COMMON_STATIC.auth.address.host}:${COMMON_STATIC.auth.address.port}`;
