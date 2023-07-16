82eb0980302c52e7ae48f816e29375006d58ffc0
sohamar <sohamar@farkask.eu>
2023-07-16 14:50:14 +0200

    feat(auth node): watch FE and BE together

--- applications/apps/auth/node/src/server.ts
+++ applications/apps/auth/node/src/server.ts
@@ -1,9 +1,10 @@
-import { COMMON_STATIC } from "@wraithlight/core.env-static";
+import { COMMON_STATIC, SERVER_STATIC } from "@wraithlight/core.env-static";
 import {
     ControllerBinder,
     createServer
 } from "@wraithlight/core.node";
 import { LoggerService } from "@wraithlight/core.logger";
+import { join } from "path";
 
 import {
     AccountControllerV2,
@@ -21,6 +22,9 @@ ControllerBinder.bindControllers(
     server.app,
     CONTROLLERS
 )
+
+server.serveStatic(SERVER_STATIC.common.paths.base, join(__dirname, SERVER_STATIC.common.files.frontend));
+server.serveStatic(SERVER_STATIC.common.paths.wildcard, join(__dirname, SERVER_STATIC.common.files.frontend));
 
 const logger = LoggerService.getInstance();
 server.start(COMMON_STATIC.auth.address.port, () => {
