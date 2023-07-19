9f14e43030a0cb0451a63e23b02cabffe10e2096
kfarkasHU <sohamar@farkask.eu>
2023-07-19 07:48:49 +0200

    feat(predicate): implement

--- applications/packages/core/core.types/src/index.ts
+++ applications/packages/core/core.types/src/index.ts
@@ -5,5 +5,6 @@ export * from "./string";
 export * from "./boolean";
 export * from "./counter";
 export * from "./nullable";
+export * from "./predicate";
 export * from "./primitive";
 export * from "./dictionary";
