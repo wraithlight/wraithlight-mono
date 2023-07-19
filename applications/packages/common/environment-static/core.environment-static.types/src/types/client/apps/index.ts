091d955a3e9512bb63cc8fbb41ddffdf64297759
kfarkasHU <kolos.farkas@dakai.io>
2023-07-19 09:28:00 +0200

    feat(client): finish it

--- applications/packages/common/environment-static/core.environment-static.types/src/types/client/apps/index.ts
+++ applications/packages/common/environment-static/core.environment-static.types/src/types/client/apps/index.ts
@@ -2,3 +2,7 @@ export * from "./content";
 export * from "./editor";
 export * from "./forum";
 export * from "./game-application";
+export * from "./game-website";
+export * from "./logs";
+export * from "./user-management";
+export * from "./website";
