8fab8262e5438ceabd2bef0f3c193fa02ca7db91
kfarkasHU <kolos.farkas@dakai.io>
2023-07-24 18:51:20 +0200

    fix(logs test): remove DB

--- applications/packages/common/environment-static/common.environment-static.client/src/configuration/logs/test/logs-test.config.ts
+++ applications/packages/common/environment-static/common.environment-static.client/src/configuration/logs/test/logs-test.config.ts
@@ -1,11 +1,4 @@
 import { LogsClient } from "@wraithlight/core.environment-static.types";
 
 export const CLIENT_TEST_LOGS_CONFIG: Readonly<LogsClient> = {
-    database: {
-        host: "",
-        port: 0,
-        username: "",
-        password: "",
-        database: "",
-    }
 };
