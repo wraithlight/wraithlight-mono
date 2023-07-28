cbdb88534e437477bca9d89e64ef783624f369d2
kfarkasHU <sohamar@farkask.eu>
2023-07-29 00:22:39 +0200

    feat(application): add notifier application
    References #17

--- applications/packages/core.common-constant/src/constants/application/application-id.const.ts
+++ applications/packages/core.common-constant/src/constants/application/application-id.const.ts
@@ -14,5 +14,6 @@ export const ApplicationId: ApplicationIdMap = Object.freeze({
     [ApplicationName.GameWebsite]: "d97a2422-ed75-46cb-90c6-6172a3e5949e",
     [ApplicationName.Logs]: "7ac64569-5a99-4afb-9180-ca8c1bec3822",
     [ApplicationName.UserManagement]: "a3d0db9e-3fb5-414a-9f6c-1bf034339322",
-    [ApplicationName.Website]: "7777ba27-da56-4ddd-ae97-f38a1e48effa"
+    [ApplicationName.Website]: "7777ba27-da56-4ddd-ae97-f38a1e48effa",
+    [ApplicationName.Notifier]: "f9c2412e-3d44-4cd3-9a80-409f0d113775"
 });
