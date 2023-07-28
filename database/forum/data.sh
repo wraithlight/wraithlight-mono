e0c76f6b3e953c46d22e7a85c580e8318afe3279
kfarkasHU <sohamar@farkask.eu>
2023-07-28 16:21:49 +0200

    feat(forum): add DB
    References #33

---
+++ database/forum/data.sh
@@ -0,0 +1,8 @@
+CONTAINER_NAME=wraithlight-db-forum
+SQL_ROOT_PASSWORD=wraithlight-forum-pw
+SQL_USERNAME=wraithlight-forum-user-1
+SQL_PASSWORD=wraithlight-forum-user-1-pw
+OUT_PORT=9003
+IN_PORT=3306
+DATABASE_NAME=WL_FORUM
+NETWORK_NAME=WL_Internal
