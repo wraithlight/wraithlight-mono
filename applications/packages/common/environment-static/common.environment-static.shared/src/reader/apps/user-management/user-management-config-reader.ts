09463e4bc1ce9c870b0a5b1902395812100fe4ad
kfarkasHU <sohamar@farkask.eu>
2023-08-06 23:16:15 +0200

    fix(user management): reader name

--- applications/packages/common/environment-static/common.environment-static.shared/src/reader/apps/user-management/user-management-config-reader.ts
+++ applications/packages/common/environment-static/common.environment-static.shared/src/reader/apps/user-management/user-management-config-reader.ts
@@ -4,11 +4,11 @@ import { ApplicationName, EnvironmentType } from "@wraithlight/core.common-const
 
 import { SharedConfigurationReader } from "../../config-reader";
 
-export class ClientUserManagementConfigReader extends SharedConfigurationReader<UserManagementShared> {
+export class SharedUserManagementConfigReader extends SharedConfigurationReader<UserManagementShared> {
 
     private static _instance: Nullable<SharedConfigurationReader<UserManagementShared>>;
 
-    public static getInstance(environment: EnvironmentType): ClientUserManagementConfigReader {
+    public static getInstance(environment: EnvironmentType): SharedUserManagementConfigReader {
         if (!this._instance) {
             this._instance = new SharedConfigurationReader<UserManagementShared>(ApplicationName.UserManagement, environment);
         }
