bad87d4f4968ed603cc9fbfb6af8363d98034846
kfarkasHU <sohamar@farkask.eu>
2023-08-06 17:18:15 +0200

    feat(*): add server skeleton
    Ref: #17

--- applications/apps/notifier/node/src/server.ts
+++ applications/apps/notifier/node/src/server.ts
@@ -1,4 +1,5 @@
-import { COMMON_STATIC, SERVER_STATIC } from "@wraithlight/core.env-static";
+import { ServerNotifierConfigReader } from "@wraithlight/common.environment-static.server";
+import { SharedNotifierConfigReader } from "@wraithlight/common.environment-static.shared";
 import {
     ControllerBinder,
     createServer
@@ -7,22 +8,30 @@ import { LoggerService } from "@wraithlight/common.logger.sdk";
 import { ServerAuthControllerV1 } from "@wraithlight/common.auth-sdk.server";
 import { LoginScope } from "@wraithlight/core.auth.types";
 import { join } from "path";
+import { EnvironmentType } from "@wraithlight/core.common-constant";
 
 const CONTROLLERS = [
     new ServerAuthControllerV1(LoginScope.Notifier)
 ];
+
+const environment = EnvironmentType.Local;
+const serverCfg = ServerNotifierConfigReader.getInstance(environment);
+const sharedCfg = SharedNotifierConfigReader.getInstance(environment);
 
 const server = createServer(true);
 
 ControllerBinder.bindControllers(
     server.app,
     CONTROLLERS
-)
+);
 
-server.serveStatic(SERVER_STATIC.common.paths.base, join(__dirname, SERVER_STATIC.common.files.frontend));
-server.serveStatic(SERVER_STATIC.common.paths.wildcard, join(__dirname, SERVER_STATIC.common.files.frontend));
+const port = sharedCfg.get(x => x.server.port);
+const frontendPath = serverCfg.getCommon(x => x.files.frontend.static);
+
+server.serveStatic(serverCfg.getCommon(x => x.paths.base), join(__dirname, frontendPath));
+server.serveStatic(serverCfg.getCommon(x => x.paths.wildcard), join(__dirname, frontendPath));
 
 const logger = LoggerService.getInstance();
-server.start(COMMON_STATIC.auth.address.port, () => {
-    logger.info(`AUTH server is running on http://localhost:${COMMON_STATIC.auth.address.port}`);
+server.start(port, () => {
+    logger.info(`AUTH server is running on http://localhost:${port}`);
 });
