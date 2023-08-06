02ff01b27497c01fbd58b0b24350c628a927449e
kfarkasHU <sohamar@farkask.eu>
2023-08-06 17:28:35 +0200

    fix(notifier): SHARED_NOTIFIER_CONFIGs type
    Ref: #17

--- applications/packages/common/environment-static/common.environment-static.shared/src/configuration/notifier/notifier.config.ts
+++ applications/packages/common/environment-static/common.environment-static.shared/src/configuration/notifier/notifier.config.ts
@@ -1,11 +1,11 @@
-import { WebsiteShared, EnvironmentStatic } from "@wraithlight/core.environment-static.types";
+import { WebsiteShared, EnvironmentStatic, NotifierShared } from "@wraithlight/core.environment-static.types";
 import { EnvironmentType } from "@wraithlight/core.common-constant";
 
 import { SHARED_LOCAL_NOTIFIER_CONFIG } from "./local";
 import { SHARED_DEV_NOTIFIER_CONFIG } from "./dev";
 import { SHARED_TEST_NOTIFIER_CONFIG } from "./test";
 
-export const SHARED_NOTIFIER_CONFIG: Readonly<EnvironmentStatic<WebsiteShared>> = {
+export const SHARED_NOTIFIER_CONFIG: Readonly<EnvironmentStatic<NotifierShared>> = {
     [EnvironmentType.Local]: SHARED_LOCAL_NOTIFIER_CONFIG,
     [EnvironmentType.Dev]: SHARED_DEV_NOTIFIER_CONFIG,
     [EnvironmentType.Test]: SHARED_TEST_NOTIFIER_CONFIG
