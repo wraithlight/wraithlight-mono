eacfd1ca544f8dc3ef96f4c5dcae9833d4013f5c
kfarkasHU <sohamar@farkask.eu>
2023-08-06 22:49:23 +0200

    chore(*): use a single webpack config
    Ref: #54

--- applications/packages/i10n/common.i10n-sdk.client/webpack.config.js
+++ applications/packages/i10n/common.i10n-sdk.client/webpack.config.js
@@ -1,38 +1,7 @@
-const { resolve } = require("path");
+const config = require("../../../webpack.config");
 
-const config = (env) => {
-  const isProduction = env.production;
-  const filename = "index.js";
-  const cfg = {
-    mode: isProduction ? "production" : "development",
-    entry: [
-      "./src/index.ts"
-    ],
-    output: {
-      path: resolve(__dirname, "./dist"),
-      filename,
-      clean: true,
-      libraryTarget: "commonjs2"
-    },
-    target: "node",
-    node: {
-      __dirname: false,
-      __filename: false,
-    },
-    module: {
-      rules: [
-        {
-          test: /\.ts$/,
-          use: "ts-loader",
-          exclude: /node_modules/
-        },
-      ]
-    },
-    resolve: {
-      extensions: [".ts", ".js"]
-    }
-  };
-  return cfg;
+module.exports = (env) => {
+  return {
+    ...config(env, __dirname)
+  }
 };
-
-module.exports = config;
