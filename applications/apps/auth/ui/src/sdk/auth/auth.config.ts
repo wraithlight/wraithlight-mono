f20d7ee3508d49f18879b54d90e08e35f4e7fbdf
kfarkasHU <sohamar@farkask.eu>
2023-08-06 23:20:52 +0200

    chore(auth): remove references to COMMON_STATIC
    Ref: #52

--- applications/apps/auth/ui/src/sdk/auth/auth.config.ts
+++ applications/apps/auth/ui/src/sdk/auth/auth.config.ts
@@ -1,3 +1,16 @@
-import { COMMON_STATIC } from "@wraithlight/core.env-static";
 
-export const AUTH_API_BASE_URL = `${COMMON_STATIC.auth.address.host}:${COMMON_STATIC.auth.address.port}`;
+import { SharedUserManagementConfigReader } from "@wraithlight/common.environment-static.shared";
+import { EnvironmentType } from "@wraithlight/core.common-constant";
+
+export class AuthServiceConfig {
+
+    // TODO: use `getEnvironment()` once its ready.
+    private readonly _userManagementSharedConfigReader = SharedUserManagementConfigReader.getInstance(EnvironmentType.Dev);
+
+    public getAuthApiBaseUrl(): string {
+        const baseApi = this._userManagementSharedConfigReader.get(x => x.server.baseUrl);
+        const port = this._userManagementSharedConfigReader.get(x => x.server.port);
+        return `${baseApi}:${port}`;
+    }
+
+}
