bf8895d5d89e1218ee13015d3e44e5a3c4ffa8ee
kfarkasHU <sohamar@farkask.eu>
2023-08-06 17:11:30 +0200

    feat(*): add support for common config

--- applications/packages/common/environment-static/common.environment-static.client/src/reader/config-reader.ts
+++ applications/packages/common/environment-static/common.environment-static.client/src/reader/config-reader.ts
@@ -1,15 +1,19 @@
 import { ConfigurationReader } from "@wraithlight/core.environment-static.sdk";
 import { ApplicationName, EnvironmentType } from "@wraithlight/core.common-constant";
+import { CommonClient } from "@wraithlight/core.environment-static.types";
 
 import { CLIENT_CONFIG } from "../configuration";
 
-export class ClientConfigurationReader<TApplication> extends ConfigurationReader<TApplication> {
+export class ClientConfigurationReader<TApplication> extends ConfigurationReader<TApplication, CommonClient> {
 
     constructor(
         readonly application: ApplicationName,
         readonly environment: EnvironmentType
     ) {
-        super(CLIENT_CONFIG[application][environment] as TApplication);
+        super(
+            CLIENT_CONFIG[application][environment] as TApplication,
+            CLIENT_CONFIG.common
+        );
     }
 
 }
--- applications/packages/common/environment-static/common.environment-static.server/src/reader/config-reader.ts
+++ applications/packages/common/environment-static/common.environment-static.server/src/reader/config-reader.ts
@@ -1,15 +1,19 @@
 import { ConfigurationReader } from "@wraithlight/core.environment-static.sdk";
 import { ApplicationName, EnvironmentType } from "@wraithlight/core.common-constant";
+import { CommonServer } from "@wraithlight/core.environment-static.types";
 
 import { SERVER_CONFIG } from "../configuration";
 
-export class ServerConfigurationReader<TApplication> extends ConfigurationReader<TApplication> {
+export class ServerConfigurationReader<TApplication> extends ConfigurationReader<TApplication, CommonServer> {
 
     constructor(
         readonly application: ApplicationName,
         readonly environment: EnvironmentType
     ) {
-        super(SERVER_CONFIG[application][environment] as TApplication);
+        super(
+            SERVER_CONFIG[application][environment] as TApplication,
+            SERVER_CONFIG.common
+        );
     }
 
 }
