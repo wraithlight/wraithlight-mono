0833d2e1dab1f8298aa0b8f4e9f2f4336019d27b
kfarkasHU <sohamar@farkask.eu>
2023-08-06 16:48:17 +0200

    feat(*): add notifier config reader
    Ref: #17

---
+++ applications/packages/common/environment-static/common.environment-static.server/src/reader/apps/notifier/notifier-config-reader.ts
@@ -0,0 +1,18 @@
+import { Nullable } from "@wraithlight/core/core.types";
+import { EditorServer } from "@wraithlight/core.environment-static.types";
+import { ApplicationName, EnvironmentType } from "@wraithlight/core.common-constant";
+
+import { ServerConfigurationReader } from "../../config-reader";
+
+export class ServerNotifierConfigReader extends ServerConfigurationReader<EditorServer> {
+
+    private static _instance: Nullable<ServerConfigurationReader<EditorServer>>;
+
+    public static getInstance(environment: EnvironmentType): ServerNotifierConfigReader {
+        if (!this._instance) {
+            this._instance = new ServerConfigurationReader<EditorServer>(ApplicationName.Notifier, environment);
+        }
+        return this._instance;
+    }
+
+}
