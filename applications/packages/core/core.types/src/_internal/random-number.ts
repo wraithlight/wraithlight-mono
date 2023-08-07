5fac6c7d4fb43c1de34b2a9c4a6bbf991f03112f
kfarkasHU <sohamar@farkask.eu>
2023-08-08 00:08:53 +0200

    refactor(internal): use crypto instead of Math.random()
    Ref: #19

--- applications/packages/core/core.types/src/_internal/random-number.ts
+++ applications/packages/core/core.types/src/_internal/random-number.ts
@@ -1,3 +1,5 @@
+import * as crypto from "crypto";
+
 export function randomNumberBetween(
     min: number = 0,
     max: number = 1
@@ -5,5 +7,32 @@ export function randomNumberBetween(
     if (max < min) {
         throw "Minimum value should be bigger than maximum value!";
     }
-    return Math.floor(Math.random() * (max - min + 1) + min)
+    return Math.floor(cryptoRandom() * (max - min + 1) + min)
+}
+
+function cryptoRandom(): number {
+    if (hasNodeCrypto()) {
+        return nodeCrypto0and1();
+    }
+    if (hasBrowserCrypto()) {
+        return browserCrypto0an1();
+    }
+    throw `No crypto was found!`;
+}
+
+function hasNodeCrypto(): boolean {
+    return !!(crypto && crypto.randomInt);
+}
+
+function nodeCrypto0and1(): number {
+    return crypto.randomInt(0, 1);
+}
+
+function hasBrowserCrypto(): boolean {
+    return !!(crypto && crypto.getRandomValues);
+}
+
+function browserCrypto0an1(): number {
+    const randomBuffer = new Uint32Array(1);
+    return crypto.getRandomValues(randomBuffer)[0] / (0xffffffff + 1);
 }
