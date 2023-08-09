276c0453b0f8fe28fe8f8d6b4db241b62c857ae1
kfarkasHU <sohamar@farkask.eu>
2023-08-09 21:31:10 +0200

    feat(game website): add server property
    Ref: #83
    Ref: #98

--- applications/packages/common/environment-static/common.environment-static.shared/src/configuration/game-website/staging/game-website-staging.config.ts
+++ applications/packages/common/environment-static/common.environment-static.shared/src/configuration/game-website/staging/game-website-staging.config.ts
@@ -1,4 +1,8 @@
 import { GameWebsiteShared } from "@wraithlight/core.environment-static.types";
 
 export const SHARED_STAGING_GAME_WEBSITE_CONFIG: Readonly<GameWebsiteShared> = {
+    server: {
+        port: 8080,
+        baseUrl: "http://www.staging.game-web.wraithlight.ai"
+    }
 };
