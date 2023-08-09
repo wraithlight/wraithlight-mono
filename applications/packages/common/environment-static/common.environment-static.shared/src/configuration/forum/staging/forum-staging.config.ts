d7675d60f40417e1c1f01ab2dc14fa6e453e0896
kfarkasHU <sohamar@farkask.eu>
2023-08-09 21:23:47 +0200

    feat(forum): add server properties
    Ref: #83
    Ref: #98

--- applications/packages/common/environment-static/common.environment-static.shared/src/configuration/forum/staging/forum-staging.config.ts
+++ applications/packages/common/environment-static/common.environment-static.shared/src/configuration/forum/staging/forum-staging.config.ts
@@ -1,4 +1,8 @@
 import { ForumShared } from "@wraithlight/core.environment-static.types";
 
 export const SHARED_STAGING_FORUM_CONFIG: Readonly<ForumShared> = {
+    server: {
+        port: 8080,
+        baseUrl: "http://staging.forum.wraithlight.ai"
+    }
 };
