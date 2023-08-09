f68d977b8a307c2aecf9a5a5a3a514f9196a75ff
kfarkasHU <sohamar@farkask.eu>
2023-08-09 21:44:35 +0200

    refactor(server): use `common.logger.sdk`
    Ref: #92

--- applications/packages/core/core.node/src/server/appref.ts
+++ applications/packages/core/core.node/src/server/appref.ts
@@ -1,4 +1,4 @@
-import { LoggerService } from "@wraithlight/core.logger";
+import { LoggerService } from "@wraithlight/common.logger.sdk";
 import { Application, static as serveStatic } from "express";
 import { Server } from "http";
 import {
