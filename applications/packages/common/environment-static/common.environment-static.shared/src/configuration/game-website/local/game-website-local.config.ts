ea06d0795e82d2f8c4a26aee934eecb73fbb5643
kfarkasHU <sohamar@farkask.eu>
2023-08-09 21:31:10 +0200

    feat(game website): add server property
    Ref: #83
    Ref: #98

--- applications/packages/common/environment-static/common.environment-static.shared/src/configuration/game-website/local/game-website-local.config.ts
+++ applications/packages/common/environment-static/common.environment-static.shared/src/configuration/game-website/local/game-website-local.config.ts
@@ -1,4 +1,8 @@
 import { GameWebsiteShared } from "@wraithlight/core.environment-static.types";
 
 export const SHARED_LOCAL_GAME_WEBSITE_CONFIG: Readonly<GameWebsiteShared> = {
+    server: {
+        port: 4505,
+        baseUrl: "http://www.local.game-web.wraithlight.ai"
+    }
 };
