08919f651332e094eedce56133f1e547ed103bb2
kfarkasHU <sohamar@farkask.eu>
2023-08-09 17:21:41 +0200

    refactor(*): remove `SERVER_STATIC` completely
    Ref: #52

--- applications/packages/core/core.env-static/src/static.dev.ts
+++ applications/packages/core/core.env-static/src/static.dev.ts
@@ -1,59 +1,6 @@
 import {
-    ClientModel,
-    CommonModel,
-    ServerModel
+    CommonModel
 } from "./internal";
-
-/**
- * @deprecated Use the proper reader from
- * ```
- * @wraithlight/common.environment-static.client
- * @wraithlight/common.environment-static.server
- * @wraithlight/common.environment-static.shared
- * ```
- */
-export const SERVER_STATIC: ServerModel = Object.freeze({
-    auth: {
-        database: {
-            host: "localhost",
-            port: 9000,
-            username: "wraithlight-auth-user-1",
-            password: "wraithlight-auth-user-1-pw",
-            database: "WL_AUTH"
-        }
-    },
-    logs: {
-        database: {
-            host: "",
-            port: 0,
-            username: "",
-            password: "",
-            database: ""
-        },
-        address: {
-            host: "",
-            port: 0
-        }
-    },
-    content: {
-        database: {
-            host: "",
-            port: 0,
-            username: "",
-            password: "",
-            database: ""
-        }
-    },
-    common:  {
-        paths: {
-            base: "/",
-            wildcard: "*"
-        },
-        files: {
-            frontend: "../../dist/ui",
-        }
-    },
-});
 
 /**
  * @deprecated Use the proper reader from
--- applications/packages/core/core.env-static/src/static.prod.ts
+++ applications/packages/core/core.env-static/src/static.prod.ts
@@ -1,59 +1,6 @@
 import {
-    ClientModel,
-    CommonModel,
-    ServerModel
+    CommonModel
 } from "./internal";
-
-/**
- * @deprecated Use the proper reader from
- * ```
- * @wraithlight/common.environment-static.client
- * @wraithlight/common.environment-static.server
- * @wraithlight/common.environment-static.shared
- * ```
- */
-export const SERVER_STATIC: ServerModel = Object.freeze({
-    auth: {
-        database: {
-            host: "",
-            port: 0,
-            username: "",
-            password: "",
-            database: ""
-        }
-    },
-    logs: {
-        database: {
-            host: "",
-            port: 0,
-            username: "",
-            password: "",
-            database: ""
-        },
-        address: {
-            host: "",
-            port: 0
-        }
-    },
-    content: {
-        database: {
-            host: "",
-            port: 0,
-            username: "",
-            password: "",
-            database: ""
-        }
-    },
-    common:  {
-        paths: {
-            base: "/",
-            wildcard: "*"
-        },
-        files: {
-            frontend: "../../dist/ui",
-        }
-    },
-});
 
 /**
  * @deprecated Use the proper reader from
