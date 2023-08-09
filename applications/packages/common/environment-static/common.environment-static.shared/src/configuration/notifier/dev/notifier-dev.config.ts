362223edce24e8446f6425a9ce3bd0adbdf2a4da
kfarkasHU <sohamar@farkask.eu>
2023-08-09 16:18:45 +0200

    feat(*): add notifier app url data

--- applications/packages/common/environment-static/common.environment-static.shared/src/configuration/notifier/dev/notifier-dev.config.ts
+++ applications/packages/common/environment-static/common.environment-static.shared/src/configuration/notifier/dev/notifier-dev.config.ts
@@ -2,8 +2,7 @@ import { NotifierShared } from "@wraithlight/core.environment-static.types";
 
 export const SHARED_DEV_NOTIFIER_CONFIG: Readonly<NotifierShared> = {
     server: {
-        // TODO: Populate
-        port: 0,
-        baseUrl: ""
+        port: 5509,
+        baseUrl: "http://dev.notifier.wraithlight.ai"
     }
 };
