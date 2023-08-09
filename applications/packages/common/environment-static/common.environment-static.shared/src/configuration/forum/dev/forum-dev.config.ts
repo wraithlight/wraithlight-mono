020704a71a9553e061383744b690e1ce519989b3
kfarkasHU <sohamar@farkask.eu>
2023-08-09 21:23:47 +0200

    feat(forum): add server properties
    Ref: #83
    Ref: #98

--- applications/packages/common/environment-static/common.environment-static.shared/src/configuration/forum/dev/forum-dev.config.ts
+++ applications/packages/common/environment-static/common.environment-static.shared/src/configuration/forum/dev/forum-dev.config.ts
@@ -1,4 +1,8 @@
 import { ForumShared } from "@wraithlight/core.environment-static.types";
 
 export const SHARED_DEV_FORUM_CONFIG: Readonly<ForumShared> = {
+    server: {
+        port: 4503,
+        baseUrl: "http://www.local.forum.wraithlight.ai"
+    }
 };
--- applications/packages/common/environment-static/common.environment-static.shared/src/configuration/forum/production/forum-production.config.ts
+++ applications/packages/common/environment-static/common.environment-static.shared/src/configuration/forum/production/forum-production.config.ts
@@ -1,4 +1,8 @@
 import { ForumShared } from "@wraithlight/core.environment-static.types";
 
 export const SHARED_PRODUCTION_FORUM_CONFIG: Readonly<ForumShared> = {
+    server: {
+        port: 8080,
+        baseUrl: "http://forum.wraithlight.ai"
+    }
 };
