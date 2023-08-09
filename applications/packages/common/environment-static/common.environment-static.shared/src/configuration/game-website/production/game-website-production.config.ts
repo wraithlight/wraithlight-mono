5250636d87587ed8c0b719bd5b22a1b085062ec5
kfarkasHU <sohamar@farkask.eu>
2023-08-09 21:51:59 +0200

    fix(production): game website url
    Ref: #99

--- applications/packages/common/environment-static/common.environment-static.shared/src/configuration/game-website/production/game-website-production.config.ts
+++ applications/packages/common/environment-static/common.environment-static.shared/src/configuration/game-website/production/game-website-production.config.ts
@@ -3,6 +3,6 @@ import { GameWebsiteShared } from "@wraithlight/core.environment-static.types";
 export const SHARED_PRODUCTION_GAME_WEBSITE_CONFIG: Readonly<GameWebsiteShared> = {
     server: {
         port: 8080,
-        baseUrl: "http://www.game.wraithlight.ai"
+        baseUrl: "http://www.play.wraithlightgame.ai"
     }
 };
