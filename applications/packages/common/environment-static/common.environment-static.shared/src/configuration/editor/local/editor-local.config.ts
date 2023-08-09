c51c2cba6074c754e677368c3ae42ddd51a65cc4
kfarkasHU <sohamar@farkask.eu>
2023-08-09 18:03:27 +0200

    feat(editor): add server property
    Ref: #83

--- applications/packages/common/environment-static/common.environment-static.shared/src/configuration/editor/local/editor-local.config.ts
+++ applications/packages/common/environment-static/common.environment-static.shared/src/configuration/editor/local/editor-local.config.ts
@@ -1,4 +1,8 @@
 import { EditorShared } from "@wraithlight/core.environment-static.types";
 
 export const SHARED_LOCAL_EDITOR_CONFIG: Readonly<EditorShared> = {
+    server: {
+        port: 0,
+        baseUrl: ""
+    }
 };
