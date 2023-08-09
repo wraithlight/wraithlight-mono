29a6b94a03cddd042109e8fd686fd71113499c3c
kfarkasHU <sohamar@farkask.eu>
2023-08-09 21:27:21 +0200

    feat(game application): add server props
    Ref: #83
    Ref: #98

--- applications/packages/common/environment-static/common.environment-static.shared/src/configuration/game-application/local/game-application-local.config.ts
+++ applications/packages/common/environment-static/common.environment-static.shared/src/configuration/game-application/local/game-application-local.config.ts
@@ -1,4 +1,8 @@
 import { GameApplicationShared } from "@wraithlight/core.environment-static.types";
 
 export const SHARED_LOCAL_GAME_APPLICATION_CONFIG: Readonly<GameApplicationShared> = {
+    server: {
+        port: 4504,
+        baseUrl: "http://www.local.game-app.wraithlight.ai"
+    }
 };
