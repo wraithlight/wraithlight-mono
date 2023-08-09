a75e6ff373062b94c20e2969d4c226e4eec2fb2f
kfarkasHU <sohamar@farkask.eu>
2023-08-09 17:57:52 +0200

    fix(website): reader name

--- applications/packages/common/environment-static/common.environment-static.shared/src/reader/apps/website/website-config-reader.ts
+++ applications/packages/common/environment-static/common.environment-static.shared/src/reader/apps/website/website-config-reader.ts
@@ -4,11 +4,11 @@ import { ApplicationName, EnvironmentType } from "@wraithlight/core.common-const
 
 import { SharedConfigurationReader } from "../../config-reader";
 
-export class ClientWebsiteConfigReader extends SharedConfigurationReader<WebsiteShared> {
+export class SharedWebsiteConfigReader extends SharedConfigurationReader<WebsiteShared> {
 
     private static _instance: Nullable<SharedConfigurationReader<WebsiteShared>>;
 
-    public static getInstance(environment: EnvironmentType): ClientWebsiteConfigReader {
+    public static getInstance(environment: EnvironmentType): SharedWebsiteConfigReader {
         if (!this._instance) {
             this._instance = new SharedConfigurationReader<WebsiteShared>(ApplicationName.Website, environment);
         }
