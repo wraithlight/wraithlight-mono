48c583aaae8257c43f35f292758109a2daa07b7e
kfarkasHU <sohamar@farkask.eu>
2023-08-09 16:38:10 +0200

    refactor(context): remove SERVER_STATIC
    Ref: #52

--- applications/apps/auth/node/src/repository/context/auth.dbcontext.factory.ts
+++ applications/apps/auth/node/src/repository/context/auth.dbcontext.factory.ts
@@ -1,11 +1,13 @@
 import { Nullable } from "@wraithlight/core.types";
-import { SERVER_STATIC } from "@wraithlight/core.env-static";
+import { getEnvironment } from "@wraithlight/core.env";
+import { ServerUserManagementConfigReader } from "@wraithlight/common.environment-static.server";
 
 import { AuthDbContext } from "./auth.dbcontext";
 
 export class AuthDbContextFactory {
 
     private static _dbContext: Nullable<AuthDbContext>;
+    private static _configReader = ServerUserManagementConfigReader.getInstance(getEnvironment());
 
     public static getAuthDbContext(): AuthDbContext {
         if (!this._dbContext) {
@@ -16,11 +18,11 @@ export class AuthDbContextFactory {
 
     private static createDbContext(): AuthDbContext {
         return new AuthDbContext(
-            SERVER_STATIC.auth.database.host,
-            SERVER_STATIC.auth.database.port,
-            SERVER_STATIC.auth.database.username,
-            SERVER_STATIC.auth.database.password,
-            SERVER_STATIC.auth.database.database
+            this._configReader.get(x => x.database.host),
+            this._configReader.get(x => x.database.port),
+            this._configReader.get(x => x.database.username),
+            this._configReader.get(x => x.database.password),
+            this._configReader.get(x => x.database.database)
         );
     }
 
