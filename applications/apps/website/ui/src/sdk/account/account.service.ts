b8536e9e24f848d4a6ebd100fef914049b155b29
kfarkasHU <sohamar@farkask.eu>
2023-08-09 17:39:36 +0200

    refactor(sdk): remove `COMMON_STATIC` references
    Ref: #52

--- applications/apps/website/ui/src/sdk/account/account.service.ts
+++ applications/apps/website/ui/src/sdk/account/account.service.ts
@@ -1,11 +1,12 @@
 import { ClientAccountService, RegisterResponse } from "@wraithlight/common.auth-sdk.client";
 
-import { AUTH_API_BASE_URL } from "./account.config";
+import { AccountServiceConfig } from "./account.config";
 import { RegisterModel } from "./model/register.model";
 
 export class AccountService {
 
-    private readonly _accountService = new ClientAccountService(AUTH_API_BASE_URL);
+    private readonly _config = new AccountServiceConfig();
+    private readonly _accountService = new ClientAccountService(this._config.getBaseApiUrl());
 
     public register(payload: RegisterModel): Promise<RegisterResponse> {
         return this._accountService.register(
