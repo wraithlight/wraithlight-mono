253bd9d774480747c504ddd8cc6140eec4966408
kfarkasHU <sohamar@farkask.eu>
2023-08-09 18:06:08 +0200

    refactor(server): purge `COMMON_STATIC` references
    Ref: #52

--- applications/apps/editor/node/src/server.ts
+++ applications/apps/editor/node/src/server.ts
@@ -1,22 +1,26 @@
-import { COMMON_STATIC } from "@wraithlight/core.env-static";
+import { SharedEditorConfigReader } from "@wraithlight/common.environment-static.shared";
 import {
     ControllerBinder,
     createServer
 } from "@wraithlight/core.node";
 import { ServerAuthControllerV1 } from "@wraithlight/common.auth-sdk.server";
 import { LoginScope } from "@wraithlight/auth/core.auth.types";
+import { getEnvironment } from "@wraithlight/core.env";
 
 const CONTROLLERS = [
     new ServerAuthControllerV1(LoginScope.Content)
 ];
 
 const server = createServer(true);
+const reader = SharedEditorConfigReader.getInstance(getEnvironment());
 
 ControllerBinder.bindControllers(
     server.app,
     CONTROLLERS
-)
+);
 
-server.start(COMMON_STATIC.editor.address.port, () => {
-    console.log(`EDITOR server is running at port ${COMMON_STATIC.editor.address.port}`)
+const port = reader.get(x => x.server.port);
+
+server.start(port, () => {
+    console.log(`EDITOR server is running at port ${port}`)
 });
