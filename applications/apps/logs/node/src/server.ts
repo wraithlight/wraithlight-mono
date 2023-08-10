25c93cedb3684cbcf2ef324b752455269afebf81
kfarkasHU <sohamar@farkask.eu>
2023-08-10 23:59:19 +0200

    refactor(*): use `createNodeServer`
    Ref: #112

--- applications/apps/logs/node/src/server.ts
+++ applications/apps/logs/node/src/server.ts
@@ -1,26 +1,18 @@
-import { getEnvironment } from "@wraithlight/core.env";
 import { SharedLogsConfigReader } from "@wraithlight/common.environment-static.shared";
-import {
-    ControllerBinder,
-    createServer
-} from "@wraithlight/core.node";
+import { ApplicationName } from "@wraithlight/core.common-constant";
+import { createNodeServer } from "@wraithlight/core.server";
+import { getEnvironment } from "@wraithlight/core.env";
 
 import { LogsEntryController } from "./controller";
+
+const sharedCfg = SharedLogsConfigReader.getInstance(getEnvironment());
 
 const CONTROLLERS = [
     new LogsEntryController()
 ];
 
-const reader = SharedLogsConfigReader.getInstance(getEnvironment());
-const server = createServer(true);
-
-ControllerBinder.bindControllers(
-    server.app,
-    CONTROLLERS
-)
-
-const port = reader.get(x => x.server.port);
-
-server.start(port, () => {
-    console.log(`LOGS server is running at port ${port}`)
-});
+createNodeServer(
+    ApplicationName.Website,
+    CONTROLLERS,
+    sharedCfg.get(x => x.server.port),
+);
