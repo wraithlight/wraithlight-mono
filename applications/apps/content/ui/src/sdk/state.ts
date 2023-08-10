0acc41de9b01e906829439bfda550ef02fc938f2
kfarkasHU <sohamar@farkask.eu>
2023-08-10 20:57:31 +0200

    style(*): mark `initializeSdk()` as deprecated in favor of `initializeAuthSdk()`
    Ref: #12

--- applications/apps/content/ui/src/sdk/state.ts
+++ applications/apps/content/ui/src/sdk/state.ts
@@ -6,6 +6,9 @@ import { INITIAL_STATE } from "./state.const";
 
 export const GLOBAL_STORE = () => Store.getInstance<GlobalState>();
 
+/**
+ * @deprecated Use `initializeAuthSdk(baseApiUrl)` from `@wraithlight/common.auth-sdk.client`.
+ */
 export function initializeSdk(): void {
     Store.initialize<GlobalState>(INITIAL_STATE);
     const store = Store.getInstance<GlobalState>();
