4b67d565a0c5c5dacef4a50433043a8e88144536
kfarkasHU <sohamar@farkask.eu>
2023-08-11 00:12:05 +0200

    refactor(*): use `createNodeServer`
    Ref: #112

--- applications/apps/editor/node/src/server.ts
+++ applications/apps/editor/node/src/server.ts
@@ -1,26 +1,18 @@
 import { SharedEditorConfigReader } from "@wraithlight/common.environment-static.shared";
-import {
-    ControllerBinder,
-    createServer
-} from "@wraithlight/core.node";
 import { ServerAuthControllerV1 } from "@wraithlight/common.auth-sdk.server";
 import { LoginScope } from "@wraithlight/auth/core.auth.types";
+import { ApplicationName } from "@wraithlight/core.common-constant";
+import { createNodeServer } from "@wraithlight/core.server";
 import { getEnvironment } from "@wraithlight/core.env";
+
+const sharedCfg = SharedEditorConfigReader.getInstance(getEnvironment());
 
 const CONTROLLERS = [
     new ServerAuthControllerV1(LoginScope.Content)
 ];
 
-const server = createServer(true);
-const reader = SharedEditorConfigReader.getInstance(getEnvironment());
-
-ControllerBinder.bindControllers(
-    server.app,
-    CONTROLLERS
+createNodeServer(
+    ApplicationName.Editor,
+    CONTROLLERS,
+    sharedCfg.get(x => x.server.port),
 );
-
-const port = reader.get(x => x.server.port);
-
-server.start(port, () => {
-    console.log(`EDITOR server is running at port ${port}`)
-});
