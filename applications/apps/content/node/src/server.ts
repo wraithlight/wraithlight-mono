d8b45ec55c038dba40b7b59688c933c3c15b661c
kfarkasHU <sohamar@farkask.eu>
2023-08-11 00:11:47 +0200

    refactor(*): use `createNodeServer`
    Ref: #112

--- applications/apps/content/node/src/server.ts
+++ applications/apps/content/node/src/server.ts
@@ -1,28 +1,18 @@
 import { SharedContentConfigReader } from "@wraithlight/common.environment-static.shared";
-import { EnvironmentType } from "@wraithlight/core.common-constant";
-import {
-    ControllerBinder,
-    createServer
-} from "@wraithlight/core.node";
 import { ServerAuthControllerV1 } from "@wraithlight/common.auth-sdk.server";
-import { LoginScope } from "@wraithlight/core.auth.types";
+import { LoginScope } from "@wraithlight/auth/core.auth.types";
+import { ApplicationName } from "@wraithlight/core.common-constant";
+import { createNodeServer } from "@wraithlight/core.server";
+import { getEnvironment } from "@wraithlight/core.env";
+
+const sharedCfg = SharedContentConfigReader.getInstance(getEnvironment());
 
 const CONTROLLERS = [
     new ServerAuthControllerV1(LoginScope.Content)
 ];
 
-const environment = EnvironmentType.Local;
-const sharedCfg = SharedContentConfigReader.getInstance(environment);
-
-const server = createServer(true);
-
-ControllerBinder.bindControllers(
-    server.app,
-    CONTROLLERS
+createNodeServer(
+    ApplicationName.Content,
+    CONTROLLERS,
+    sharedCfg.get(x => x.server.port),
 );
-
-const port = sharedCfg.get(x => x.server.port);
-
-server.start(port, () => {
-    console.log(`CONTENT server is running at port ${port}`)
-});
