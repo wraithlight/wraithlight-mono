c5ece8a6e611fe2331132f79d9016ce915c80a49
kfarkasHU <sohamar@farkask.eu>
2023-08-12 22:29:13 +0200

    refactor(sdk): use `IAuthStore`
    Ref: #129

--- applications/packages/auth/common.auth-sdk.client/src/sdk/state.const.ts
+++ applications/packages/auth/common.auth-sdk.client/src/sdk/state.const.ts
@@ -1,17 +1,15 @@
-import { IAuthContainerStore } from "./state.model";
+import { IAuthStore } from "./state.model";
 
 /**
  * @deprecated This const wont be exported for login.
  * Once https://github.com/kfarkasHU/wraithlight/issues/115 is done, this will be removed.
  */
-export const INITIAL_AUTH_STATE: IAuthContainerStore = {
-    auth: {
-        session: {
-            isBusy: false,
-            isLoggedIn: false
-        },
-        account: {
-            isBusy: false   
-        }
+export const INITIAL_AUTH_STATE: IAuthStore = {
+    session: {
+        isBusy: false,
+        isLoggedIn: false
+    },
+    account: {
+        isBusy: false
     }
 };
