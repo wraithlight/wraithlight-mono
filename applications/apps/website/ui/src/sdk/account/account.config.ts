6f3fa374207d7ba0906600741ba929e4b7f2e668
kfarkasHU <sohamar@farkask.eu>
2023-08-19 16:36:15 +0200

    refactor(*): use `getEnvironmentType()` instead
    Ref: #151

--- applications/apps/website/ui/src/sdk/account/account.config.ts
+++ applications/apps/website/ui/src/sdk/account/account.config.ts
@@ -1,9 +1,9 @@
-import { getEnvironment } from "@wraithlight/core.env";
+import { getEnvironmentType } from "@wraithlight/core.env";
 import { SharedUserManagementConfigReader } from "@wraithlight/common.environment-static.shared";
 
 export class AccountServiceConfig {
 
-    private readonly _configReader = SharedUserManagementConfigReader.getInstance(getEnvironment());
+    private readonly _configReader = SharedUserManagementConfigReader.getInstance(getEnvironmentType());
 
     public getBaseApiUrl(): string {
         const baseUrl = this._configReader.get(x => x.server.baseUrl);
