cbf12d9bebac815b8bd12847b030d47eaf54b0e4
kfarkasHU <sohamar@farkask.eu>
2023-08-19 21:36:32 +0200

    chore(*): add unit tests for coverage report
    Ref: #162

---
+++ applications/packages/core/core.node/src/crud/decorator/get.decorator.spec.ts
@@ -0,0 +1,28 @@
+jest.mock("./http.decorator", () => {
+    return {
+        HttpDecorator: jest.fn()
+    }
+})
+
+import { HttpDecorator } from "./http.decorator";
+import { HttpGet } from "./get.decorator";
+
+describe("HttpGetSpecs", () => {
+    
+    const MOCK_PATH = "wraithlight/test";
+    let decorator = HttpGet;
+
+    describe("given the decorator is initalized", () => {
+
+        const result = decorator(MOCK_PATH);
+
+        it("should call the underlying HttpDecorator function", () => {
+            expect(HttpDecorator).toHaveBeenCalled();
+            expect(HttpDecorator).toHaveBeenCalledTimes(1);
+            expect(HttpDecorator).toHaveBeenCalledWith("GET", MOCK_PATH);
+        });
+
+
+    });
+
+});
