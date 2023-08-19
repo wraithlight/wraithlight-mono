172ddc3cb7058e52e1d4dc865511635eb9ed914a
kfarkasHU <sohamar@farkask.eu>
2023-08-19 16:35:22 +0200

    refactor(*): use `getEnvironmentType()` instead
    Ref: #151

--- applications/apps/editor/node/src/server.ts
+++ applications/apps/editor/node/src/server.ts
@@ -3,9 +3,9 @@ import { ServerAuthControllerV1 } from "@wraithlight/common.auth-sdk.server";
 import { LoginScope } from "@wraithlight/core.auth.types";
 import { ApplicationName } from "@wraithlight/core.common-constant";
 import { createNodeServer } from "@wraithlight/core.server";
-import { getEnvironment } from "@wraithlight/core.env";
+import { getEnvironmentType } from "@wraithlight/core.env";
 
-const sharedCfg = SharedEditorConfigReader.getInstance(getEnvironment());
+const sharedCfg = SharedEditorConfigReader.getInstance(getEnvironmentType());
 
 const CONTROLLERS = [
     new ServerAuthControllerV1(LoginScope.Content)
