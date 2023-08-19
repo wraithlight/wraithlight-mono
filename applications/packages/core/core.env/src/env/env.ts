777d44af51b678754cfdb52fa44e697ba7e27466
kfarkasHU <sohamar@farkask.eu>
2023-08-19 23:50:48 +0200

    feat(env): add generic `get()` method
    Ref: #152

--- applications/packages/core/core.env/src/env/env.ts
+++ applications/packages/core/core.env/src/env/env.ts
@@ -1,7 +1,13 @@
 import { EnvironmentType } from "@wraithlight/core.common-constant";
+import { Predicate, Primitive } from "@wraithlight/core.types";
 
 import { WL_TYPE_PROP_NAME } from "./env.const";
+import { IEnvironment } from "./env.model";
 
 export function getEnvironmentType(): EnvironmentType {
     return process.env[WL_TYPE_PROP_NAME] as EnvironmentType || EnvironmentType.Local;
+}
+
+export function getFromEnvironment<T extends Primitive>(predicate: Predicate<IEnvironment, T>): T {
+    return predicate((process.env as unknown as IEnvironment)) as T;
 }
