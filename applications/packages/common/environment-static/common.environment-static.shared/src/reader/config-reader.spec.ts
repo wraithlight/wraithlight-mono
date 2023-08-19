c382352c90221c11f66c91a1e457aa3f34da6b0d
kfarkasHU <sohamar@farkask.eu>
2023-08-19 23:03:12 +0200

    chore(*): add unit tests for better coverage
    Ref: #162

---
+++ applications/packages/common/environment-static/common.environment-static.shared/src/reader/config-reader.spec.ts
@@ -0,0 +1,32 @@
+jest.mock("@wraithlight/core.environment-static.sdk", () => {
+    return {
+        ConfigurationReader: jest.fn()
+    }
+});
+
+import { ConfigurationReader } from "@wraithlight/core.environment-static.sdk";
+import { ApplicationName, EnvironmentType } from "@wraithlight/core.common-constant";
+import { WebsiteShared } from "@wraithlight/core.environment-static.types";
+
+import { SHARED_WEBSITE_CONFIG } from "../configuration/website";
+import { SHARED_CONFIG } from "../configuration";
+
+import { SharedConfigurationReader } from "./config-reader";
+
+describe("SharedConfigurationReaderSpecs", () => {
+
+    const MOCK_APPLICATION_NAME = ApplicationName.Website;
+    const MOCK_ENVIRONMENT = EnvironmentType.Dev;
+    let reader: SharedConfigurationReader<WebsiteShared>;
+
+    describe("given the service is initialized", () => {
+
+        reader = new SharedConfigurationReader(MOCK_APPLICATION_NAME, MOCK_ENVIRONMENT);
+
+        it("should have been called `ConfigurationReader`", () => {
+            expect(ConfigurationReader).toHaveBeenCalled();
+            expect(ConfigurationReader).toHaveBeenCalledTimes(1);
+            expect(ConfigurationReader).toHaveBeenCalledWith(SHARED_WEBSITE_CONFIG.DEV, SHARED_CONFIG.common.DEV);
+        })
+    });
+});
