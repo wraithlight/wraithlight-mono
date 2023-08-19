16a72190b65fe08701642bc4d4c1849e42d1c9dc
kfarkasHU <sohamar@farkask.eu>
2023-08-19 23:30:49 +0200

    refactor(sdk): remove `INITIAL_AUTH_STATE` from the public api and use it internally only
    Ref: #116

--- applications/packages/auth/common.auth-sdk.client/src/sdk/state.const.ts
+++ applications/packages/auth/common.auth-sdk.client/src/sdk/state.const.ts
@@ -1,9 +1,5 @@
 import { IAuthStore } from "./state.model";
 
-/**
- * @deprecated This const wont be exported for login.
- * Once https://github.com/kfarkasHU/wraithlight/issues/115 is done, this will be removed.
- */
 export const INITIAL_AUTH_STATE: IAuthStore = {
     session: {
         isBusy: false,
