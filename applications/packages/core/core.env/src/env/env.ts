cae152dc04d106907eb449fa6c5e15b2ed69bf0f
kfarkasHU <sohamar@farkask.eu>
2023-08-19 15:18:28 +0200

    refactor(env): mark `getEnvironment()` as deprecated
    Ref: #151

--- applications/packages/core/core.env/src/env/env.ts
+++ applications/packages/core/core.env/src/env/env.ts
@@ -2,6 +2,13 @@ import { EnvironmentType } from "@wraithlight/core.common-constant";
 
 import { WL_TYPE_PROP_NAME } from "./env.const";
 
+/**
+ * @deprecated Use `getEnvironmentType()` instead.
+ */
 export function getEnvironment(): EnvironmentType {
+    return process.env[WL_TYPE_PROP_NAME] as EnvironmentType || EnvironmentType.Local;
+}
+
+export function getEnvironmentType(): EnvironmentType {
     return process.env[WL_TYPE_PROP_NAME] as EnvironmentType || EnvironmentType.Local;
 }
