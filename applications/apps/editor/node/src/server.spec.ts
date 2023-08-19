7267d90e89aa6ff602d330379ee87e366cb46a61
kfarkasHU <sohamar@farkask.eu>
2023-08-19 19:03:31 +0200

    chore(*): add a test for proper coverage generation
    Ref: #162

---
+++ applications/apps/editor/node/src/server.spec.ts
@@ -0,0 +1,16 @@
+jest.mock("@wraithlight/core.server", () => {
+    return {
+        createNodeServer: jest.fn()
+    }
+});
+import { createNodeServer } from "@wraithlight/core.server";
+
+describe("ServerSpecs", () => {
+    import("./server");
+    describe("given the server has been started", () => {
+        it("should call `createNodeServer`", () => {
+            expect(createNodeServer).toHaveBeenCalled();
+            expect(createNodeServer).toHaveBeenCalledTimes(1);
+        });
+    });
+});
