e04ee19de5118a7805462ab92c41c0c473784798
kfarkasHU <sohamar@farkask.eu>
2023-08-19 21:55:40 +0200

    chore(*): add unit test for coverage report
    Ref: #162

--- applications/packages/core/core.redux/jest.config.js
+++ applications/packages/core/core.redux/jest.config.js
@@ -3,7 +3,14 @@ module.exports = {
   testEnvironment: "node",
   verbose: true,
   moduleFileExtensions: ["js", "ts"],
-  passWithNoTests: true,
+  collectCoverageFrom: [
+    "**/*.ts",
+    "!**/index.ts",
+    "!**/bootstrap.ts",
+    "!**/main.ts",
+    "!**/*.const.ts",
+    "!**/*.model.ts"
+  ],
   projects: [
     {
       preset: "ts-jest",
---
+++ applications/packages/core/core.redux/src/store/selector/selector-result.spec.ts
@@ -0,0 +1,30 @@
+import { SelectorResult } from "./selector-result";
+
+class TestSelectorResult extends SelectorResult<string> {
+    public getParam() {
+        return this.params;
+    }
+}
+
+describe("SelectorResultSpecs", () => {
+
+    const MOCK_PARAMS = (_val: string, callback: () => void) => {};
+    let result: TestSelectorResult;
+
+    describe("given the object has been initialized", () => {
+
+        result = new TestSelectorResult();
+
+        describe("when i call `onSelection`", () => {
+            beforeEach(() => {
+                result.onSelection(MOCK_PARAMS);
+            });
+
+            it("should update `params` accordingly", () => {
+                expect(result.getParam()).toBe(MOCK_PARAMS);
+            });
+        });
+
+    });
+
+});
