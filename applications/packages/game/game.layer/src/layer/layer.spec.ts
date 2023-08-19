b49e07812a6035b1a40f7a437977cfa088e15bd9
kfarkasHU <sohamar@farkask.eu>
2023-08-19 22:39:43 +0200

    chore(*): add unit test for better test coverage
    Ref: #162

---
+++ applications/packages/game/game.layer/src/layer/layer.spec.ts
@@ -0,0 +1,37 @@
+jest.mock("@wraithlight/game.renderer", () => {
+    return {
+        Renderer: jest.fn().mockImplementation(() => {
+            return {
+                useCanvas: jest.fn()
+            }
+        })
+    }
+});
+
+import { Renderer } from "@wraithlight/game.renderer";
+
+import { Layer } from "./layer";
+
+describe("LayerSpecs", () => {
+
+    const MOCK_CANVAS = document.createElement("canvas");
+    let layer: Layer;
+
+    describe("given the service is initialized", () => {
+
+        layer = new Layer(MOCK_CANVAS);
+
+        it("should have created a renderer instance", () => {
+            expect(Renderer).toHaveBeenCalled();
+            expect(Renderer).toHaveBeenCalledTimes(1);
+        });
+
+        it("should have set the canvas to the renderer", () => {
+            expect(layer["_renderer"].useCanvas).toHaveBeenCalled();
+            expect(layer["_renderer"].useCanvas).toHaveBeenCalledTimes(1);
+            expect(layer["_renderer"].useCanvas).toHaveBeenCalledWith(MOCK_CANVAS);
+        });
+
+    });
+
+});
