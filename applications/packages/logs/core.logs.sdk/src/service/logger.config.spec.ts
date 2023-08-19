158779f5555d3c5d7424e155020d99bae01a9413
kfarkasHU <sohamar@farkask.eu>
2023-08-19 20:44:00 +0200

    chore(*): add unit tests for proper coverage
    Ref: #162

---
+++ applications/packages/logs/core.logs.sdk/src/service/logger.config.spec.ts
@@ -0,0 +1,36 @@
+jest.mock("@wraithlight/common.environment-static.shared", () => {
+    return {
+        SharedLogsConfigReader: {
+            getInstance: jest.fn().mockImplementation(() => { 
+                return {
+                    get: jest.fn().mockImplementation(() => "test")
+                }
+             })
+        }
+    }
+});
+
+import { LoggerServiceConfig } from "./logger.config";
+
+describe("LoggerServiceConfigSpecs", () => {
+
+    let service: LoggerServiceConfig;
+
+    describe("given the service is initialized", () => {
+        service = new LoggerServiceConfig();
+
+        describe("when i call `getLogUrl()`", () => {
+            let loginV1Endpoint: string;
+
+            beforeEach(() => {
+                loginV1Endpoint = service.getLogUrl();
+            });
+
+            it("should return the proper v1 endpoint", () => {
+                expect(loginV1Endpoint).toBe(`test:test/api/v1/logs/create`);
+            });
+
+        });
+    });
+
+});
