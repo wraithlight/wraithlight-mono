9c8c943edfa699917b164f97eedfed94766c541c
kfarkasHU <sohamar@farkask.eu>
2023-08-19 20:26:54 +0200

    chore(*): add unit tests for coverage report
    Ref: #162

---
+++ applications/packages/logs/common.logs-sdk.client/src/service/logger.service.spec.ts
@@ -0,0 +1,31 @@
+jest.mock("@wraithlight/core.logs.sdk", () => {
+    return {
+        BeaconLoggerService: jest.fn().mockImplementation(() => { return { }})
+    }
+});
+import { BeaconLoggerService } from "@wraithlight/core.logs.sdk";
+import { ApplicationName } from "@wraithlight/core.common-constant";
+
+import { ClientLoggerService } from "./logger.service";
+
+describe("ClientLoggerServiceSpecs", () => {
+
+    const MOCK_APP_NAME = ApplicationName.Website;
+
+    let service: ClientLoggerService;
+
+    describe("given the service is initialized", () => {
+        service = new ClientLoggerService(MOCK_APP_NAME);
+
+        it("should be truthy", () => {
+            expect(service).toBeTruthy();
+        });
+
+        it("should initialize the underlying `BeaconLoggerService`", () => {
+            expect(BeaconLoggerService).toHaveBeenCalled();
+            expect(BeaconLoggerService).toHaveBeenCalledTimes(1);
+        });
+
+    });
+    
+});
