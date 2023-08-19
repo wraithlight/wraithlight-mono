b6041f5e4874e9f6c99db46359b967102fa86f23
kfarkasHU <sohamar@farkask.eu>
2023-08-19 23:03:19 +0200

    chore(*): add unit tests for better coverage
    Ref: #162

---
+++ applications/packages/common/environment-static/common.environment-static.client/src/reader/config-reader.spec.ts
@@ -0,0 +1,32 @@
+jest.mock("@wraithlight/core.environment-static.sdk", () => {
+    return {
+        ConfigurationReader: jest.fn()
+    }
+});
+
+import { ConfigurationReader } from "@wraithlight/core.environment-static.sdk";
+import { ApplicationName, EnvironmentType } from "@wraithlight/core.common-constant";
+import { WebsiteClient } from "@wraithlight/core.environment-static.types";
+
+import { CLIENT_WEBSITE_CONFIG } from "../configuration/website";
+import { CLIENT_CONFIG } from "../configuration";
+
+import { ClientConfigurationReader } from "./config-reader";
+
+describe("ClientConfigurationReaderSpecs", () => {
+
+    const MOCK_APPLICATION_NAME = ApplicationName.Website;
+    const MOCK_ENVIRONMENT = EnvironmentType.Dev;
+    let reader: ClientConfigurationReader<WebsiteClient>;
+
+    describe("given the service is initialized", () => {
+
+        reader = new ClientConfigurationReader(MOCK_APPLICATION_NAME, MOCK_ENVIRONMENT);
+
+        it("should have been called `ConfigurationReader`", () => {
+            expect(ConfigurationReader).toHaveBeenCalled();
+            expect(ConfigurationReader).toHaveBeenCalledTimes(1);
+            expect(ConfigurationReader).toHaveBeenCalledWith(CLIENT_WEBSITE_CONFIG.DEV, CLIENT_CONFIG.common.DEV);
+        })
+    });
+});
