9a933d257f619611b679dcc9d529261ca5a8376a
kfarkasHU <sohamar@farkask.eu>
2023-08-20 20:08:00 +0200

    fix(component): remove unused exports
    Ref: #177

--- applications/apps/content/ui/src/shared/component/input/input.model.ts
+++ applications/apps/content/ui/src/shared/component/input/input.model.ts
@@ -1,8 +1,8 @@
+type InputType = "input" | "password" | "tel";
+
 export interface InputComponentProps {
     label: string;
     type: InputType;
     elementId: string;
     onChange: (value: string) => void;
 }
-
-export type InputType = "input" | "password" | "tel";
