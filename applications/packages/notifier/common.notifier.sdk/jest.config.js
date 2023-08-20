17963834fc49f9d35908b9895be704b42a63bf07
kfarkasHU <sohamar@farkask.eu>
2023-08-20 22:24:03 +0200

    feat(*): add `common.notifier.sdk` lib
    Ref: #154

---
+++ applications/packages/notifier/common.notifier.sdk/jest.config.js
@@ -0,0 +1,29 @@
+/** @type {import("ts-jest").JestConfigWithTsJest} */
+module.exports = {
+  testEnvironment: "node",
+  verbose: true,
+  moduleFileExtensions: ["js", "ts"],
+  passWithNoTests: true,
+  collectCoverageFrom: [
+    "**/*.ts",
+    "!**/index.ts",
+    "!**/bootstrap.ts",
+    "!**/main.ts",
+    "!**/*.model.ts"
+  ],
+  projects: [
+    {
+      preset: "ts-jest",
+      displayName: "Packages/Notifier/SDK",
+      testMatch: [
+        "<rootDir>/src/**/*.spec.ts"
+      ],
+      testPathIgnorePatterns: [
+        "dist"
+      ],
+      coveragePathIgnorePatterns: [
+        "dist"
+      ]
+    },
+  ]
+};
---
+++ applications/packages/notifier/common.notifier.sdk/src/service/notifier.config.ts
@@ -0,0 +1,17 @@
+import { SharedNotifierConfigReader } from "@wraithlight/common.environment-static.shared";
+import { getEnvironmentType } from "@wraithlight/core.env";
+import { NOTIFIER_ENDPOINT_CONST } from "@wraithlight/core.notifier.const";
+
+export class NotifierServiceConfig {
+
+    private readonly _notifierSharedReader = SharedNotifierConfigReader.getInstance(getEnvironmentType());
+
+    public getSendMailV1Url(): string {
+        const baseUrl = this._notifierSharedReader.get(m => m.server.baseUrl);
+        const port = this._notifierSharedReader.get(m => m.server.port);
+        const pathRoot = NOTIFIER_ENDPOINT_CONST.v1.send.root;
+        const path = NOTIFIER_ENDPOINT_CONST.v1.send.mail;
+        return `${baseUrl}:${port}${pathRoot}${path}`;
+    }
+
+}
