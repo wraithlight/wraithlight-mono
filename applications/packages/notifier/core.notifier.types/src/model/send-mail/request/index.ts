fe0724c21ce5a7fc88bb6dc6e125a672db3b52de
kfarkasHU <sohamar@farkask.eu>
2023-08-20 21:59:42 +0200

    feat(*): add `core.notifier.types` lib
    Ref: #154

---
+++ applications/packages/notifier/core.notifier.types/src/model/send-mail/request/index.ts
@@ -0,0 +1,1 @@
+export * from "./send-mail-request-v1.model";
---
+++ applications/packages/notifier/core.notifier.types/src/model/send-mail/request/send-mail-request-v1.model.ts
@@ -0,0 +1,6 @@
+export interface SendMailRequestV1Model {
+    address: string;
+    subject: string;
+    content: string;
+    isHtml: string;
+}
