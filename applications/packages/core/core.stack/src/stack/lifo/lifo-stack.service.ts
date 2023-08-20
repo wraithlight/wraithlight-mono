be022e9928daea409feb78ec4b948a4ec759cd00
kfarkasHU <sohamar@farkask.eu>
2023-08-20 20:49:48 +0200

    feat(*): add `core.stack` lib
    Ref: #167

---
+++ applications/packages/core/core.stack/src/stack/lifo/lifo-stack.service.ts
@@ -0,0 +1,30 @@
+import { Nullable, Predicate } from "@wraithlight/core.types";
+
+export class LifoStackService<T> {
+
+    private readonly _cache: Array<T> = [];
+
+    public set(item: T): void {
+        this._cache.push(item);
+    }
+
+    public hasAny(): boolean {
+        return this._cache.length > 0;
+    }
+
+    public find(predicate: Predicate<T, boolean>): Nullable<T> {
+        return this._cache.filter(predicate)?.[0] ?? undefined;
+    }
+
+    public getNext(): T {
+        if (!this.hasAny) {
+            throw `No items in the cache!`;
+        }
+        return this._cache.shift()!;
+    }
+
+    public tryGetNext(): Nullable<T> {
+        return this._cache.shift();
+    }
+
+}
