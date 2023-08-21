d92ad574970fe3f7fbe9a49cccab6667346a465d
kfarkasHU <sohamar@farkask.eu>
2023-08-22 00:52:11 +0200

    feat(*): add sending feature
    Ref: #154

--- applications/apps/notifier/node/src/service/v1/send/send.service.ts
+++ applications/apps/notifier/node/src/service/v1/send/send.service.ts
@@ -1,10 +1,14 @@
 import { CqrsService } from "@wraithlight/core.cqrs";
 import { Guid } from "@wraithlight/core/core.types";
+import { LoggerService } from "@wraithlight/common.logger.sdk";
 
 import { SendEmailModelV1 } from "./send.model";
+import { NodemailerFacadeService } from "../../../nodemailer";
 
 export class SendServiceV1 {
 
+    private readonly _logger = LoggerService.getInstance();
+    private readonly _nodemailerFacade = NodemailerFacadeService.getInstance();
     private readonly _cqrsService = new CqrsService<SendEmailModelV1>((item, id) => this.sendWorker(item, id));
 
     public send(
@@ -21,8 +25,14 @@ export class SendServiceV1 {
         }) as Guid; // TODO: Fix this once ESM build has been added.
     }
 
-    private sendWorker(item: SendEmailModelV1, id: Guid) {
-
+    private async sendWorker(item: SendEmailModelV1, id: Guid): Promise<void> {
+        this._logger.warn(`Entry with id '${id}' is being processed!`);
+        try {
+            await this._nodemailerFacade.sendMail(item.address, item.subject, item.content, item.isHtml);
+            this._logger.warn(`Entry with id '${id}' has been processed succesfully!`);
+        } catch (e: unknown) {
+            this._logger.warn(`Entry processing ('${id}') has been failed`, JSON.stringify(e));
+        }
     }
 
 }
