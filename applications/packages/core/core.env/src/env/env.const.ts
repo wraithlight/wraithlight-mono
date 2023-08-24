ebaefc6ad95a919b482d668a6f16ae089126b07c
kfarkasHU <sohamar@farkask.eu>
2023-08-24 21:55:40 +0200

    refactor(env): use iterative testing
    Ref: #208

--- applications/packages/core/core.env/src/env/env.const.ts
+++ applications/packages/core/core.env/src/env/env.const.ts
@@ -1,1 +1,8 @@
+import { EnvironmentType } from "@wraithlight/core.common-constant";
+
+/**
+ * @deprecated Use `WL_ENV_TYPE_PROP_NAME` instead.
+ */
 export const WL_TYPE_PROP_NAME = "wlType";
+export const WL_ENV_TYPE_PROP_NAME = "wlType";
+export const WL_ENV_DEFAULT = EnvironmentType.Local;
