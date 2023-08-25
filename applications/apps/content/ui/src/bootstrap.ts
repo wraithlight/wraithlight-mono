9ed67b35ac9f741ab1ed854172e8b6427af891da
kfarkasHU <sohamar@farkask.eu>
2023-08-25 22:32:25 +0200

    style(*): ignore restricted globals

--- applications/apps/content/ui/src/bootstrap.ts
+++ applications/apps/content/ui/src/bootstrap.ts
@@ -1,10 +1,13 @@
+import { getDocumentRef, getWindowRef } from "@wraithlight/core.dom";
 import { mount } from "mithril";
 
 import { AppComponent  } from "./app.component";
 import { initializeSdk } from "./sdk/state";
 
-window.addEventListener("DOMContentLoaded", () => {
+const _document = getDocumentRef();
+const _window = getWindowRef();
+_window.addEventListener("DOMContentLoaded", () => {
     initializeSdk();
-    const body = document.body as HTMLBodyElement;
+    const body = _document.body as HTMLBodyElement;
     mount(body, AppComponent);
 });
