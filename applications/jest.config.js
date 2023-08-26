06d76ce932f71b64e998cf4c0552bc940b7d450d
kfarkasHU <sohamar@farkask.eu>
2023-08-26 19:16:32 +0200

    chore(*): add root level jest config
    Ref: #16

--- applications/jest.config.js
+++ applications/jest.config.js
@@ -1,22 +1,34 @@
 const ignoreTests = [
   "types",
-  "const"
+  "const",
+  // TODO: Align package names
+  "constant",
+  "constants",
 ]
 
-module.exports = (project) => ({
-  testEnvironment: "node",
-  verbose: true,
-  moduleFileExtensions: ["js", "ts"],
-  passWithNoTests: ignoreTests.map(m => project.name.toLowerCase().endsWith(m.toLowerCase())),
-  collectCoverageFrom: [
-    "**/*.ts",
-    "!**/index.ts",
-    "!**/bootstrap.ts",
-    "!**/main.ts",
-    "!**/*.const.ts",
-    "!**/*.model.ts"
-  ],
-  projects: [
-    project
-  ]
-});
+module.exports = (project) => {
+  if(!project.displayName) {
+    throw "Project should contain `displayName`!";
+  }
+  return {
+    testEnvironment: "node",
+    verbose: true,
+    moduleFileExtensions: ["js", "ts"],
+    passWithNoTests: ignoreTests
+      .map(m => project.displayName.toLowerCase().endsWith(m))
+      .concat(project.passWithNoTests || false)
+      .includes(true)
+    ,
+    collectCoverageFrom: [
+      "**/*.ts",
+      "!**/index.ts",
+      "!**/bootstrap.ts",
+      "!**/main.ts",
+      "!**/*.const.ts",
+      "!**/*.model.ts"
+    ],
+    projects: [
+      project
+    ]
+  }
+};
