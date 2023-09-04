5a5e64d2f1d433d6f0863c6c2933de8613fc31be
kfarkasHU <sohamar@farkask.eu>
2023-09-04 22:03:09 +0200

    test(*): add `passWithNoTests`

--- applications/packages/core/core.deepmerge/jest.config.js
+++ applications/packages/core/core.deepmerge/jest.config.js
@@ -2,6 +2,7 @@ const config = require("../../../jest.config.js");
 
 module.exports = config({
     preset: "ts-jest",
+    passWithNoTests: true,
     displayName: "Packages/Core/Deepmerge",
     testMatch: [
       "<rootDir>/src/**/*.spec.ts"
