01d351f8866afcc951302414988b6884db36267b
kfarkasHU <sohamar@farkask.eu>
2023-09-04 21:22:34 +0200

    style(*): add deprecation comments
    Ref: #228

--- applications/packages/game/core.game.core/src/model/position.model.ts
+++ applications/packages/game/core.game.core/src/model/position.model.ts
@@ -1,3 +1,6 @@
+/**
+ * @deprecated Use `Position` from `@wraithlight/core.game.types` instead
+ */
 export interface Position {
     x: number;
     y: number;
--- applications/packages/game/core.game.core/src/model/sprite.model.ts
+++ applications/packages/game/core.game.core/src/model/sprite.model.ts
@@ -1,5 +1,8 @@
 import { Position } from "./position.model";
 
+/**
+ * @deprecated Use `Sprite` from `@wraithlight/core.game.types` instead
+ */
 export interface Sprite {
     width: number;
     height: number;
