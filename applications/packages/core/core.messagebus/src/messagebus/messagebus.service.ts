2e48ae448d21b3ee1cb19c5a5306375d61ff257c
kfarkasHU <sohamar@farkask.eu>
2023-09-04 21:41:10 +0200

    feat(*): add `core.messagebus` package
    Ref: #169

---
+++ applications/packages/core/core.messagebus/src/messagebus/messagebus.service.ts
@@ -0,0 +1,33 @@
+import { Guid, newGuid } from "@wraithlight/core.types";
+
+import { MessagebusCallbackFn, MessagebusListener } from "./messagebus.model";
+
+export class MessagebusService {
+
+    private _listeners: Array<MessagebusListener> = [];
+
+    public removeListener(id: Guid): void {
+        const index = this._listeners.findIndex(m => m.id === id);
+        if (index > -1) {
+            this._listeners.splice(index, 1);
+        }
+    }
+
+    public push<T>(token: string, data: T): void {
+        this._listeners
+            .filter(m => m.token === token)
+            .forEach(m => m.callback(data))
+        ;
+    }
+
+    public sub<T>(token: string, callback: MessagebusCallbackFn<T>): Guid {
+        const id = newGuid();
+        this._listeners.push({
+            id: id,
+            token: token,
+            callback: callback
+        });
+        return id;
+    }
+
+}
