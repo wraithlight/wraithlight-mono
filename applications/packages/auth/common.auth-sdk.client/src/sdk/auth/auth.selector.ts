3c81669c97802f22ff9c93cb0e7b2f4b2ad74dd5
kfarkasHU <sohamar@farkask.eu>
2023-09-07 01:11:50 +0200

    feat(auth): add named selectors
    Ref: #286

--- applications/packages/auth/common.auth-sdk.client/src/sdk/auth/auth.selector.ts
+++ applications/packages/auth/common.auth-sdk.client/src/sdk/auth/auth.selector.ts
@@ -1,10 +1,18 @@
 import { Selector } from "@wraithlight/core.redux";
+import { Nullable } from "@wraithlight/core.types";
 
 import { IAuthContainerStore } from "../state.model";
 
 import { AuthState } from "./auth-state.model";
 
 export namespace AuthSelector {
+    /**
+     * @deprecated Use the named selectors instead.
+     */
     export const state: Selector<IAuthContainerStore, AuthState> = (state: IAuthContainerStore) => state.auth.session;
     export const isBusy: Selector<IAuthContainerStore, boolean> = (state: IAuthContainerStore) => state.auth.session.isBusy;
+    export const errors: Selector<IAuthContainerStore, Nullable<ReadonlyArray<string>>> = (state: IAuthContainerStore) => state.auth.session.errors;
+    export const isLoggedIn: Selector<IAuthContainerStore, boolean> = (state: IAuthContainerStore) => state.auth.session.isLoggedIn;
+    export const token: Selector<IAuthContainerStore, Nullable<string>> = (state: IAuthContainerStore) => state.auth.session.token;
+    export const tokenValidUntil: Selector<IAuthContainerStore, Nullable<Date>> = (state: IAuthContainerStore) => state.auth.session.tokenValidUntil;
 }
