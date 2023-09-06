4d84d731140568978e7d1fe8ad0d8da4450c2d79
kfarkasHU <sohamar@farkask.eu>
2023-09-07 00:26:17 +0200

    feat(*): add `common.notifier.nodemailer-sdk` package
    Ref: #203

---
+++ applications/packages/notifier/common.notifier.nodemailer-sdk/src/nodemailer/nodemailer-facade.service.ts
@@ -0,0 +1,79 @@
+import { Nullable } from "@wraithlight/core.types";
+import { IMailSender, IMailSenderSendMailResult } from "@wraithlight/core.notifier.types";
+import { createTransport } from "nodemailer";
+
+export class NodemailerFacadeService implements IMailSender {
+
+    private static _instance: Nullable<NodemailerFacadeService>;
+    private _transporter = createTransport({
+        host: this._host,
+        port: this._port,
+        secure: this._secure,
+        auth: {
+            user: this._authUsername,
+            pass: this._authPassword,
+        }
+    });
+
+    public static getInstance(
+        host: string,
+        port: number,
+        secure: boolean,
+        authUsername: string,
+        authPassword: string
+    ): NodemailerFacadeService {
+        if (!this._instance) {
+            this._instance = new NodemailerFacadeService(
+                host,
+                port,
+                secure,
+                authUsername,
+                authPassword
+            );
+        }
+        return this._instance;
+    }
+
+    private constructor(
+        private readonly _host: string,
+        private readonly _port: number,
+        private readonly _secure: boolean,
+        private readonly _authUsername: string,
+        private readonly _authPassword: string
+    ) { }
+    sendEmail(toAddress: string, fromAddress: string, subject: string, content: string, isHtml?: boolean | undefined, cc?: readonly string[] | undefined, bcc?: readonly string[] | undefined): Promise<IMailSenderSendMailResult> {
+        throw new Error("Method not implemented.");
+    }
+
+    public async sendMail(
+        toAddress: string,
+        fromAddress: string,
+        subject: string,
+        content: string,
+        isHtml: boolean = false,
+        cc: ReadonlyArray<string> = [],
+        bcc: ReadonlyArray<string> = []
+    ): Promise<IMailSenderSendMailResult> {
+        try {
+            await this._transporter.sendMail({
+                from: fromAddress,
+                to: toAddress,
+                subject,
+                html: isHtml ? content : undefined,
+                text: isHtml ? undefined : content,
+                cc: cc as Array<string>,
+                bcc: bcc as Array<string>
+            });
+            return {
+                success: true,
+                errors: []
+            };
+        } catch (e: unknown) {
+            return {
+                success: false,
+                errors: [JSON.stringify(e)]
+            }
+        }
+    }
+
+}
