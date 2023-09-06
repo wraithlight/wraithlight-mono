cda662a522405342935d753ba196b8d43d5bf4e4
kfarkasHU <sohamar@farkask.eu>
2023-09-07 00:51:02 +0200

    fix(notifier): add `smtp` property as well
    Ref: #282

--- applications/packages/common/environment-static/core.environment-static.types/src/types/server/apps/notifier/notifier.model.ts
+++ applications/packages/common/environment-static/core.environment-static.types/src/types/server/apps/notifier/notifier.model.ts
@@ -5,7 +5,11 @@ import { SmtpServer } from "./_internal";
 export interface NotifierServer {
     database: DatabaseServer;
     emailSending: {
+        /**
+         * @deprecated Use `smtp` instead.
+         */
         smpt: SmtpServer;
+        smtp?: SmtpServer;
         fromAddress: string
     }
 }
