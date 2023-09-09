a4e6a315c1fd117cc6916ee105676a0293b4018f
kfarkasHU <sohamar@farkask.eu>
2023-09-09 13:08:02 +0200

    feat(remote config): add `server`
    Ref: #9

--- applications/packages/common/environment-static/common.environment-static.shared/src/configuration/remote-config/dev/remote-config-dev.config.ts
+++ applications/packages/common/environment-static/common.environment-static.shared/src/configuration/remote-config/dev/remote-config-dev.config.ts
@@ -1,4 +1,8 @@
 import { RemoteConfigShared } from "@wraithlight/core.environment-static.types";
 
 export const SHARED_DEV_REMOTE_CONFIG_CONFIG: Readonly<RemoteConfigShared> = {
+    server: {
+        port: 0,
+        baseUrl: ""
+    }
 };
