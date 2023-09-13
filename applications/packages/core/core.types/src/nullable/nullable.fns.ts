075d428b7fafc7e4088e5ad3a6584d88adb9b6d8
kfarkasHU <sohamar@farkask.eu>
2023-09-13 21:38:30 +0200

    refactor(nullable): mark as deprecated
    Ref: #265

--- applications/packages/core/core.types/src/nullable/nullable.fns.ts
+++ applications/packages/core/core.types/src/nullable/nullable.fns.ts
@@ -2,6 +2,7 @@
  * Checks if the given value is `null` or `undefined`.
  * @param {unknown} valueLike The value to check.
  * @returns {boolean} True if the object is `null` or `undefined` otherwise false.
+ * @deprecated Import `isNil` from `@wraithlight/core.nullable`
  */
 export function isNil<T>(valueLike: unknown): valueLike is T {
     if (valueLike === null) {
