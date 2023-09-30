9ed8358a8137fe1d0c3688395f5741045bf395a6
kfarkasHU <sohamar@farkask.eu>
2023-09-30 16:25:43 +0200

    refactor(const): remove unused enum member

--- applications/apps/auth/node/src/_internal/const/user-status.const.ts
+++ applications/apps/auth/node/src/_internal/const/user-status.const.ts
@@ -2,7 +2,6 @@
  * @deprecated Use `UserStatus` from `@wraithlight/core.user-management.types` instead.
  */
 export const enum UserStatus {
-    Active = 1,
     EmailVerify = 2,
     LockedOutDueTooManyInvalidLogin = 3
 }
