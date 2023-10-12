138b91b00f04ef914799dca6b69a9c416bfd7d63
kfarkasHU <sohamar@farkask.eu>
2023-10-12 12:31:06 +0200

    test(predicate default): `predicateDefault`
    Ref: #371

--- applications/packages/core/core.linq/jest.config.js
+++ applications/packages/core/core.linq/jest.config.js
@@ -2,7 +2,6 @@ const config = require("../../../jest.config.js");
 
 module.exports = config({
     preset: "ts-jest",
-    passWithNoTests: true,
     displayName: "Packages/Core/LINQ",
     testMatch: [
       "<rootDir>/src/**/*.spec.ts"
