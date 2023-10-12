24a6849a566cb07761308050c1d3171d6dd93286
kfarkasHU <sohamar@farkask.eu>
2023-10-12 11:48:37 +0200

    test(*): `NodemailerFacadeService`
    Ref: #382

--- applications/packages/notifier/common.notifier.nodemailer-sdk/jest.config.js
+++ applications/packages/notifier/common.notifier.nodemailer-sdk/jest.config.js
@@ -3,7 +3,6 @@ const config = require("../../../jest.config.js");
 module.exports = config({
     preset: "ts-jest",
     displayName: "Packages/Notifier/Common/Nodemailer SDK",
-    passWithNoTests: true,
     testMatch: [
       "<rootDir>/src/**/*.spec.ts"
     ],
