b4273c19cdbcb35f43991bdbca229a8d742b0a50
kfarkasHU <sohamar@farkask.eu>
2023-10-12 11:48:37 +0200

    test(*): `NodemailerFacadeService`
    Ref: #382

---
+++ applications/packages/notifier/common.notifier.nodemailer-sdk/src/nodemailer/nodemailer-facade.service.spec.ts
@@ -0,0 +1,85 @@
+import { NodemailerFacadeService } from "./nodemailer-facade.service";
+
+const sendMailSpy = jest.fn()
+jest.mock("nodemailer", () => {
+    return {
+        createTransport: jest.fn().mockImplementation(() => {
+            return {
+                sendMail: sendMailSpy
+            }
+        })
+    }
+})
+
+import { createTransport } from "nodemailer";
+
+describe("NodemailerFacadeServiceSpecs", () => {
+
+    const MOCK_HOST = "";
+    const MOCK_PORT = 0;
+    const MOCK_SECURE = false;
+    const MOCK_USERNAME = "";
+    const MOCK_PASSWORD = "";
+    const MOCK_TOADDRESS = "";
+    const MOCK_FROMADDRESS = "";
+    const MOCK_SUBJECT = "";
+    const MOCK_CONTENT = "";
+    const MOCK_ISHTML = false;
+    const MOCK_CC: ReadonlyArray<string> = [];
+    const MOCK_BCC: ReadonlyArray<string> = [];
+
+    let service: NodemailerFacadeService;
+
+    describe("given the service is initialized", () => {
+        beforeEach(() => {
+            service = NodemailerFacadeService.getInstance(
+                MOCK_HOST,
+                MOCK_PORT,
+                MOCK_SECURE,
+                MOCK_USERNAME,
+                MOCK_PASSWORD
+            );
+        })
+        describe("when i call getInstance()", () => {
+            it("should create a new transport object", () => {
+                expect(createTransport).toHaveBeenCalled();
+                expect(createTransport).toHaveBeenCalledTimes(1);
+                expect(createTransport).toHaveBeenCalledWith({
+                    host: MOCK_HOST,
+                    port: MOCK_PORT,
+                    secure: MOCK_SECURE,
+                    auth: {
+                        user: MOCK_USERNAME,
+                        pass: MOCK_PASSWORD,
+                    }
+                });
+            });
+        });
+        describe("when i call sendMail", () => {
+            beforeEach(() => {
+                service.sendEmail(
+                    MOCK_TOADDRESS,
+                    MOCK_FROMADDRESS,
+                    MOCK_SUBJECT,
+                    MOCK_CONTENT,
+                    MOCK_ISHTML,
+                    MOCK_CC,
+                    MOCK_BCC
+                );
+            });
+            it("should call the underlying provider", () => {
+                expect(sendMailSpy).toHaveBeenCalled();
+                expect(sendMailSpy).toHaveBeenCalledTimes(1);
+                expect(sendMailSpy).toHaveBeenCalledWith({
+                    from: MOCK_FROMADDRESS,
+                    to: MOCK_TOADDRESS,
+                    subject: MOCK_SUBJECT,
+                    html: MOCK_ISHTML ? MOCK_CONTENT : undefined,
+                    text: MOCK_ISHTML ? undefined : MOCK_CONTENT,
+                    cc: MOCK_CC,
+                    bcc: MOCK_BCC
+                });
+            })
+        })
+    })
+})
