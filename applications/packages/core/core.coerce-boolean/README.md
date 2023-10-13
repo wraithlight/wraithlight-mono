866904e179c85fcdbc1bc6129afdc321a0be51fa
kfarkasHU <sohamar@farkask.eu>
2023-10-13 17:23:03 +0200

    refactor(*): create `coerce-boolean` lib
    Ref: #265

---
+++ applications/packages/core/core.coerce-boolean/README.md
@@ -0,0 +1,1 @@
+# core.coerce-boolean
---
+++ applications/packages/core/core.coerce-boolean/src/boolean/coerce.ts
@@ -0,0 +1,8 @@
+/**
+ * Coerces any value to its boolean representation.
+ * @param {unknown} booleanLike An unknown value to convert to its boolean value. 
+ * @returns {boolean} The boolean value.
+ */
+export function coerceBoolean(booleanLike: unknown): boolean {
+    return !!booleanLike;
+}
