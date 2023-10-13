a4ed8a73104078ed705682cd1a66641ff8be9c3a
kfarkasHU <sohamar@farkask.eu>
2023-10-13 17:47:10 +0200

    chore(*): remove `passWithNoTests`
    Ref: #378

--- applications/packages/game/core.game.const/jest.config.js
+++ applications/packages/game/core.game.const/jest.config.js
@@ -2,7 +2,6 @@ const config = require("../../../jest.config.js");
 
 module.exports = config({
     preset: "ts-jest",
-    passWithNoTests: true,
     displayName: "Packages/Game/Core/Const",
     testMatch: [
       "<rootDir>/src/**/*.spec.ts"
