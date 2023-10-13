abaadd332b47b6b8af82f6b27958b1e7e7bd1439
kfarkasHU <sohamar@farkask.eu>
2023-10-13 17:23:21 +0200

    refactor(boolean): make functions deprecated
    Ref: #265

--- applications/packages/core/core.types/src/boolean/coerce.ts
+++ applications/packages/core/core.types/src/boolean/coerce.ts
@@ -2,6 +2,7 @@
  * Coerces any value to its boolean representation.
  * @param {unknown} booleanLike An unknown value to convert to its boolean value. 
  * @returns {boolean} The boolean value.
+ * @deprecated Use `coerceBoolean` from `@wraithlight/core.coerce-boolean` instead.
  */
 export function coerceBoolean(booleanLike: unknown): boolean {
     return !!booleanLike;
