307c3b91fa74768c75bee5383da2b2128d6b0c9d
kfarkasHU <sohamar@farkask.eu>
2023-10-13 16:19:03 +0200

    chore(*): remove package level cjs builds
    Ref: #394

--- applications/packages/core.common-constant/webpack.config.js
+++
@@ -1,7 +0,0 @@
-const config = require("../../webpack.package.config");
-
-module.exports = (env) => {
-  return {
-    ...config(env, __dirname)
-  }
-};
--- applications/packages/core/core.server/webpack.config.js
+++
@@ -1,8 +0,0 @@
-const config = require("../../../webpack.package.config");
-
-module.exports = (env) => {
-  return {
-    ...config(env, __dirname),
-    target: "node"
-  }
-};
