2c2ac733ca0b89cbae28f59e86972433b2f8b08c
kfarkasHU <sohamar@farkask.eu>
2023-10-13 17:01:35 +0200

    chore(*): remove `passWithNoTests` flag
    Ref: #375

--- applications/packages/core.common-constants/jest.config.js
+++ applications/packages/core.common-constants/jest.config.js
@@ -3,7 +3,6 @@ const config = require("../../jest.config.js");
 module.exports = config({
     preset: "ts-jest",
     displayName: "Packages/Core/Common Constants",
-    passWithNoTests: true,
     testMatch: [
       "<rootDir>/src/**/*.spec.ts"
     ],
