0f4161bd9f277fd8ffc82cc768a0de8ca2bc8f0e
kfarkasHU <sohamar@farkask.eu>
2023-10-13 16:19:03 +0200

    chore(*): remove package level cjs builds
    Ref: #394

---
+++ applications/.scripts/webpack-config-remover.mjs
@@ -0,0 +1,39 @@
+/**
+ * This script was originally used to create the `tsconfig.build.json` files.
+ * It walks all the folders recursively using tree-walker, and if it founds
+ * a `tsconfig.json` it makes a copy of it then renames the copy to `tsconfig.build.json`
+ * 
+ * Usage:
+ * * place it to the root folder
+ * * run it by using `node webpack-config-remover.mjs`
+ */
+
+import { fileURLToPath } from 'url';
+import { dirname, join } from "path";
+import { cpSync, readdirSync, rmSync, statSync } from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const IGNORED_ITEMS = [
+    "node_modules",
+    "dist",
+    "coverage"
+];
+
+function recursive(dirname) {
+    const items = readdirSync(dirname);
+    for(const item of items) {
+        if(IGNORED_ITEMS.includes(item)) continue;
+        const stat = statSync(join(dirname, item));
+        if(stat.isFile()) {
+            if(item === "webpack.config.js") {
+                rmSync(join(dirname, item));
+            }
+        }
+        if(stat.isDirectory()) {
+            recursive(join(dirname, item));
+        }
+    }
+}
+recursive(__dirname);
