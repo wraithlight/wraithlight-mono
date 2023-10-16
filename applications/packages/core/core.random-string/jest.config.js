a1b1499ea0193264c0e8bf733fee9e43a3be3f12
kfarkasHU <sohamar@farkask.eu>
2023-10-17 00:14:48 +0200

    refactor(*): add `core.random-string` lib
    Ref: #265

---
+++ applications/packages/core/core.random-string/jest.config.js
@@ -0,0 +1,15 @@
+const config = require("../../../jest.config.js");
+
+module.exports = config({
+    preset: "ts-jest",
+    displayName: "Packages/Core/Random string",
+    testMatch: [
+      "<rootDir>/src/**/*.spec.ts"
+    ],
+    testPathIgnorePatterns: [
+      "dist"
+    ],
+    coveragePathIgnorePatterns: [
+      "dist"
+    ]
+});
---
+++ applications/packages/core/core.random-string/src/string/random.fns.ts
@@ -0,0 +1,20 @@
+import { randomNumberBetween } from "@wraithlight/core.random-number";
+
+/**
+ * Generates a random string based on the length and the alphabet.
+ * @param {number} length Length of the generated string.
+ * @param {Array<string>} alphabet Alphabets to use. 
+ * @returns {string} The generated random string.
+ */
+export function generateRandomString(
+    length: number,
+    alphabet: Array<string>
+): string {
+    const letters = alphabet.join("");
+    const result: Array<string> = [];
+    for (let i = 0; i < length; i++) {
+        const index = randomNumberBetween(0, letters.length - 1);
+        result.push(letters[index]);
+    }
+    return result.join("");
+}
