12b6145f3233a7c5c66d1f3aef7d9244b9b1aeb9
kfarkasHU <sohamar@farkask.eu>
2023-10-16 23:44:24 +0200

    refactor(*): update core deepmerge to use the facade layer
    Ref: #409

--- applications/packages/core/core.deepmerge/src/deepmerge/deepmerge.fn.ts
+++ applications/packages/core/core.deepmerge/src/deepmerge/deepmerge.fn.ts
@@ -1,9 +1,9 @@
 import { DeepPartial } from "@wraithlight/core.types";
-import { deepmerge } from "deepmerge-ts";
+import { deepmergeFacade } from "@wraithlight/facade.deepmerge";
 
 export function wlDeepmerge<T>(
     target: T,
     additional: DeepPartial<T> | Partial<T> | T
 ): T {
-    return deepmerge(target, additional) as T;
+    return deepmergeFacade(target, additional) as T;
 }
