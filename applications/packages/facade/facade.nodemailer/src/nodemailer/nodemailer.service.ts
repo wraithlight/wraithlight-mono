92aa8ce183f6dc0c2adc62f397e462bd7df6f8f9
kfarkasHU <sohamar@farkask.eu>
2023-10-19 22:13:42 +0200

    feat(*): add `facade.nodemailer` lib
    Ref: #412

---
+++ applications/packages/facade/facade.nodemailer/src/nodemailer/nodemailer.service.ts
@@ -0,0 +1,46 @@
+import { Transporter, createTransport } from "nodemailer";
+
+export class NodemailerFacadeService {
+
+    private readonly _transport: Transporter;
+
+    constructor(
+        host: string,
+        port: number,
+        isSecure: boolean,
+        username: string,
+        password: string
+    ) {
+        this._transport = createTransport({
+            host: host,
+            port: port,
+            secure: isSecure,
+            auth: {
+                user: username,
+                pass: password
+            }
+        });
+    }
+
+    public async sendMail(
+        fromAddress: string,
+        toAddress: string,
+        subject: string,
+        text?: string,
+        htmlText?: string,
+        bcc: Array<string> = [],
+        cc: Array<string> = []
+    ): Promise<string> {
+        const result: { messageId: string } = await this._transport.sendMail({
+            to: toAddress,
+            from: fromAddress,
+            subject: subject,
+            text: text,
+            html: htmlText,
+            bcc: bcc,
+            cc: cc
+        });
+        return result.messageId;
+    }
+
+}
