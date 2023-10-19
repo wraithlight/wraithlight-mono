a4a9f48582fa75e08d228cfca1306d0c3e138224
kfarkasHU <sohamar@farkask.eu>
2023-10-19 22:58:33 +0200

    chore(*): add `passWithNoTests`
    Ref: #265

--- applications/packages/core/core.primitive/jest.config.js
+++ applications/packages/core/core.primitive/jest.config.js
@@ -3,6 +3,7 @@ const config = require("../../../jest.config.js");
 module.exports = config({
     preset: "ts-jest",
     displayName: "Packages/Core/Primitive",
+    passWithNoTests: true,
     testMatch: [
       "<rootDir>/src/**/*.spec.ts"
     ],
