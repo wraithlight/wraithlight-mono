504461ea0abdb533c49cfd90eab02cebf1fdff33
kfarkasHU <sohamar@farkask.eu>
2023-10-20 20:02:49 +0200

    chore(*): make jest work
    Ref: #368

--- applications/apps/auth/ui/jest.config.js
+++ applications/apps/auth/ui/jest.config.js
@@ -1,4 +1,9 @@
 import config from "../../../jest.config.js";
+import { dirname, join } from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default config({
     preset: "ts-jest",
@@ -12,5 +17,8 @@ export default config({
     ],
     coveragePathIgnorePatterns: [
       "dist"
-    ]
+    ],
+    transform: {
+      '.ts': ['ts-jest', { tsconfig: join(__dirname, 'tsconfig.jest.json') }]
+    }
 });
