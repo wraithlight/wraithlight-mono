8ef806d27b9119092a5fcda977728ba259f6e306
kfarkasHU <sohamar@farkask.eu>
2023-10-20 20:03:13 +0200

    test(*): add main test
    Ref: #368

--- applications/apps/auth/ui/jest.config.js
+++ applications/apps/auth/ui/jest.config.js
@@ -8,7 +8,6 @@ const __dirname = dirname(__filename);
 export default config({
     preset: "ts-jest",
     displayName: "Apps/Auth/UI",
-    passWithNoTests: true,
     testMatch: [
       "<rootDir>/src/**/*.spec.ts"
     ],
