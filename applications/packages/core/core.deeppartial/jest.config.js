58388c79302e9f6c00c490742f5314f0913b68f8
kfarkasHU <sohamar@farkask.eu>
2023-10-20 19:13:45 +0200

    feat(*): add `core.deeppartial` lib
    Ref: #265

---
+++ applications/packages/core/core.deeppartial/jest.config.js
@@ -0,0 +1,16 @@
+const config = require("../../../jest.config.js");
+
+module.exports = config({
+    preset: "ts-jest",
+    displayName: "Packages/Core/Deeppartial",
+    passWithNoTests: true,
+    testMatch: [
+      "<rootDir>/src/**/*.spec.ts"
+    ],
+    testPathIgnorePatterns: [
+      "dist"
+    ],
+    coveragePathIgnorePatterns: [
+      "dist"
+    ]
+});
