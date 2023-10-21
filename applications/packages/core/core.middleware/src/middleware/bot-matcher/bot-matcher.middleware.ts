13a0d5746831d63ca4650daef165aeaf9d6e35f6
kfarkasHU <sohamar@farkask.eu>
2023-10-21 15:39:14 +0200

    feat(*): add bot handler middleware
    Ref: #438
    Ref: #201

---
+++ applications/packages/core/core.middleware/src/middleware/bot-matcher/bot-matcher.middleware.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from "express";
+import { Nullable, isNil } from "@wraithlight/core.nullable";
+
+import {
+    IS_BOT_HEADER_NAME,
+    USER_AGENT_HEADER_NAME
+} from "./bot-matcher.const";
+import { isBot } from "./_internal";
+
+export function botMatcherMiddleware(
+    req: Request,
+    _res: Response,
+    next: NextFunction
+): void {
+    const userAgent = req.get(USER_AGENT_HEADER_NAME)
+    const isVisitorABot = hasNoUserAgent(userAgent) || isBot(userAgent!);
+    req.headers[IS_BOT_HEADER_NAME] = isVisitorABot.toString();
+    next();
+}
+
+function hasNoUserAgent(userAgent: Nullable<string>): boolean {
+    return isNil(userAgent) || userAgent === "";
+}
