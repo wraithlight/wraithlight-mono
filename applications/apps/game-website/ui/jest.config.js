e4945662786995bb481b4357c4ed8edfc7eadd89
kfarkasHU <sohamar@farkask.eu>
2023-10-22 15:35:22 +0200

    test(framework): `addComponent`
    Ref: #367

--- applications/apps/game-website/ui/jest.config.js
+++ applications/apps/game-website/ui/jest.config.js
@@ -3,7 +3,6 @@ const config = require("../../../jest.config.js");
 module.exports = config({
     preset: "ts-jest",
     displayName: "Apps/Game Website/UI",
-    passWithNoTests: true,
     testMatch: [
       "<rootDir>/src/**/*.spec.ts"
     ],
