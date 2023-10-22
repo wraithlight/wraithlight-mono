883b6c01cd16632f9c9274f20fd19e199989627d
kfarkasHU <sohamar@farkask.eu>
2023-10-22 16:48:10 +0200

    style(main): use `"` instead of `'`

--- applications/apps/website/ui/src/main.ts
+++ applications/apps/website/ui/src/main.ts
@@ -1,11 +1,11 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { LoggerService } from "@wraithlight/common.logger.sdk";
 import { getDocumentRef } from "@wraithlight/core.dom";
-import { initializeAuthSdk } from '@wraithlight/common.auth-sdk.client';
-import { Store } from '@wraithlight/core.redux';
+import { initializeAuthSdk } from "@wraithlight/common.auth-sdk.client";
+import { Store } from "@wraithlight/core.redux";
 
 import { INITIAL_STATE, WebsiteGlobalState } from './sdk';
-import { AppModule } from './app/app.module';
+import { AppModule } from "./app/app.module";
 
 const _document = getDocumentRef();
 _document.addEventListener("DOMContentLoaded", () => {
