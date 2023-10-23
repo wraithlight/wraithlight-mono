500e7c502866999902d03b30151776267e8bfc89
kfarkasHU <sohamar@farkask.eu>
2023-10-23 18:37:21 +0200

    feat(sdk): force override initial state
    Ref: #474

--- applications/packages/auth/common.auth-sdk.client/src/sdk/state.ts
+++ applications/packages/auth/common.auth-sdk.client/src/sdk/state.ts
@@ -1,5 +1,4 @@
 import { Store } from "@wraithlight/core.redux";
-import { Predicate } from "@wraithlight/core.types";
 
 import { IAuthContainerStore, IAuthStore } from "./state.model";
 import { authInitialize } from "./auth/auth";
@@ -12,7 +11,11 @@ export function initializeAuthSdk(
     apiBaseUrl: string,
     storeRef: Store<IAuthContainerStore>
 ): void {
-    Store.initializePartial<IAuthContainerStore, IAuthStore>((m => m.auth), INITIAL_AUTH_STATE)
+    Store.initializePartial<IAuthContainerStore, IAuthStore>(
+        (m => m.auth),
+        INITIAL_AUTH_STATE,
+        true
+    );
     authInitialize(storeRef, apiBaseUrl);
     accountInitialize(storeRef, apiBaseUrl);
 }
