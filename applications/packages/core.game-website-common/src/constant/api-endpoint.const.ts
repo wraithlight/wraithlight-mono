0d7f23d0cadbb86e40ba99773897032ca4aa7de3
kfarkasHU <sohamar@farkask.eu>
2023-10-23 18:23:55 +0200

    refactor(*): make exported members as deprecated
    Ref: #494

--- applications/packages/core.game-website-common/src/constant/api-endpoint.const.ts
+++ applications/packages/core.game-website-common/src/constant/api-endpoint.const.ts
@@ -5,6 +5,9 @@ const V1_GAME_ROOT = `${API_BASE_URL}${API_V1}`;
 const V1_GAME_AUTH_ROOT = `${V1_GAME_ROOT}/auth`;
 const V1_GAME_CHARACTER_ROOT = `${V1_GAME_ROOT}/character`;
 
+/**
+ * @deprecated Import it from `core.game-website.constats` instead.
+ */
 export const GAME_API_ENDPOINTS = Object.freeze({
     auth: {
         root: V1_GAME_AUTH_ROOT,
--- applications/packages/core.game-website-common/src/model/response/register.model.ts
+++ applications/packages/core.game-website-common/src/model/response/register.model.ts
@@ -1,11 +1,20 @@
+/**
+ * @deprecated Import it from `core.game-website.types` instead.
+ */
 export interface BaseRegisterResponse {
     success: boolean;
 }
 
+/**
+ * @deprecated Import it from `core.game-website.types` instead.
+ */
 export interface ErrorRegisterResponse extends BaseRegisterResponse {
     errors: Array<string>;
 }
 
+/**
+ * @deprecated Import it from `core.game-website.types` instead.
+ */
 export interface SuccessRegisterResponse extends BaseRegisterResponse {
     
 }
