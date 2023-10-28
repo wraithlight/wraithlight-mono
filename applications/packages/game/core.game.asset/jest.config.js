64eaee865eda11f0456cf885b601b56ed74d914d
kfarkasHU <sohamar@farkask.eu>
2023-10-28 17:10:56 +0200

    chore(*): add `asset` to root level jest config
    Ref: #493

--- applications/packages/game/core.game.asset/jest.config.js
+++ applications/packages/game/core.game.asset/jest.config.js
@@ -2,7 +2,6 @@ const config = require("../../../jest.config.js");
 
 module.exports = config({
     preset: "ts-jest",
-    passWithNoTests: true,
     displayName: "Packages/Game/Core/Asset",
     testMatch: [
       "<rootDir>/src/**/*.spec.ts"
