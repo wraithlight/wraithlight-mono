afb13558010429828f5168b9d98b8f6c221a7d7b
kfarkasHU <sohamar@farkask.eu>
2023-11-09 22:16:57 +0100

    refactor(service): use `createUrl()`
    Ref: #508

--- applications/packages/logs/core.logs.sdk/src/service/logger.config.ts
+++ applications/packages/logs/core.logs.sdk/src/service/logger.config.ts
@@ -1,3 +1,4 @@
+import { createUrl } from "@wraithlight/core.url";
 import { getEnvironmentType } from "@wraithlight/core.env";
 import { LOGGER_API_ENDPOINTS } from "@wraithlight/core.logs.constant";
 import { SharedLogsConfigReader } from "@wraithlight/common.environment-static.shared";
@@ -9,7 +10,7 @@ export class LoggerServiceConfig {
     public getLogUrl(): string {
         const baseApiUrl = this._configReader.get(x => x.server.baseUrl);
         const port = this._configReader.get(x => x.server.port);
-        const host = `${baseApiUrl}:${port}`;
+        const host = createUrl(baseApiUrl, port);
         const path = LOGGER_API_ENDPOINTS.v1.logs.create;
         return `${host}${path}`;
     }
