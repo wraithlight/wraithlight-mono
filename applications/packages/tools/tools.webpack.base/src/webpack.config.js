6cbfa64081dd25273f8159567416dc05ba321a4d
kfarkasHU <sohamar@farkask.eu>
2023-11-14 21:41:04 +0100

    feat(*): add `tools.webpack.base` lib
    Ref: #490

---
+++ applications/packages/tools/tools.webpack.base/src/webpack.config.js
@@ -0,0 +1,31 @@
+const { resolve } = require("path");
+
+module.exports = (env, dirname) => {
+  const isProduction = env.production;
+  const filename = "index.js";
+  const cfg = {
+    mode: isProduction ? "production" : "development",
+    entry: [
+      "./src/index.ts"
+    ],
+    output: {
+      path: resolve(dirname, "./dist"),
+      filename,
+      clean: true,
+      libraryTarget: "commonjs2"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: "ts-loader",
+          exclude: /node_modules/
+        },
+      ]
+    },
+    resolve: {
+      extensions: [".ts", ".js"]
+    }
+  };
+  return cfg;
+};
