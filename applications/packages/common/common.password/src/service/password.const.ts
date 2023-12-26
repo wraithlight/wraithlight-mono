c5e3e06d23aa111ae2e0ee9ef148f8b56f117cb8
kfarkasHU <sohamar@farkask.eu>
2023-12-26 22:01:29 +0100

    feat(*): align to bcrypt
    Ref: #506

--- applications/packages/common/common.password/src/service/password.const.ts
+++ applications/packages/common/common.password/src/service/password.const.ts
@@ -1,12 +1,1 @@
-import {
-    LOWERCASE_ALPHABET,
-    UPPERCASE_ALPHABET,
-    NUMBER_ALPHABET
-} from "@wraithlight/core.random-string";
-
-export const SALT_LENGTH = 10;
-export const SALT_ALPHABET: ReadonlyArray<string> = [
-    LOWERCASE_ALPHABET,
-    UPPERCASE_ALPHABET,
-    NUMBER_ALPHABET
-];
+export const SALT_ROUNDS = 10;
