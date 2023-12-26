24d18f0aa3e4640f46633a3ce9a52236ef604db7
kfarkasHU <sohamar@farkask.eu>
2023-12-26 21:18:04 +0100

    test(bcrypt): add tests

---
+++ applications/packages/facade/facade.bcrypt/src/bcrypt/bcrypt.spec.ts
@@ -0,0 +1,66 @@
+jest.mock("bcrypt", () => {
+    return {
+        genSaltSync: jest.fn(),
+        hashSync: jest.fn(),
+        compareSync: jest.fn()
+    }
+});
+
+import {
+    genSaltSync,
+    hashSync,
+    compareSync
+} from "bcrypt"
+
+import { Bcrypt } from "./bcrypt";
+
+const MOCK_ROUNDS = 15
+const MOCK_PASSWORD = "password"
+const MOCK_SALT = "salt"
+const MOCK_VERIFY_PASSWORD = "password"
+const MOCK_VERIFY_HASH = "hash"
+
+describe("BcryptSpecs", () => {
+
+    describe("given the utility is initialized", () => {
+        describe("when i call", () => {
+            describe("generateHashSync", () => {
+                beforeEach(() => {
+                    Bcrypt.generateHashSync(MOCK_ROUNDS)
+                })
+                it("should call the underlying method", () => {
+                    expect(genSaltSync).toHaveBeenCalled();
+                    expect(genSaltSync).toHaveBeenCalledTimes(1);
+                    expect(genSaltSync).toHaveBeenCalledWith(MOCK_ROUNDS);
+                })
+            });
+
+            describe("encryptPasswordWithSaltSync", () => {
+                beforeEach(() => {
+                    Bcrypt.encryptPasswordWithSaltSync(MOCK_PASSWORD, MOCK_SALT)
+                })
+                it("should call the underlying method", () => {
+                    expect(hashSync).toHaveBeenCalled();
+                    expect(hashSync).toHaveBeenCalledTimes(1);
+                    expect(hashSync).toHaveBeenCalledWith(MOCK_PASSWORD, MOCK_SALT);
+                })
+            });
+
+            describe("verifyPasswordSync", () => {
+                beforeEach(() => {
+                    Bcrypt.verifyPasswordSync(
+                        MOCK_VERIFY_PASSWORD,
+                        MOCK_VERIFY_HASH
+                    )
+                })
+                it("should call the underlying method", () => {
+                    expect(compareSync).toHaveBeenCalled();
+                    expect(compareSync).toHaveBeenCalledTimes(1);
+                    expect(compareSync).toHaveBeenCalledWith(MOCK_VERIFY_PASSWORD, MOCK_VERIFY_HASH);
+                })
+            });
+
+        });
+    });
+
+});
