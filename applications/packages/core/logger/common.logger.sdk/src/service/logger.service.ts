f9aff0fbfa29bc01e5a575fd5aa2b669a67ab323
kfarkasHU <sohamar@farkask.eu>
2023-12-29 20:57:24 +0100

    refactor(service): remove extra `getInstance()` params
    Ref: #475

--- applications/packages/core/logger/common.logger.sdk/src/service/logger.service.ts
+++ applications/packages/core/logger/common.logger.sdk/src/service/logger.service.ts
@@ -7,23 +7,31 @@ import { DEFAULT_CONFIG } from "./logger.const";
 export class LoggerService implements ILogger {
 
     private static _config: Nullable<LoggerConfig>;
+    private static _logger: Nullable<ILogger>;
     private static _instance: Nullable<LoggerService>;
 
     private constructor(
         private readonly _config: LoggerConfig,
-        private readonly _logger: ILogger = console
+        private readonly _logger: ILogger
     ) { }
 
-    public static initialize(config?: LoggerConfig): void {
-        if (this._config) {
+    public static initialize(
+        config?: LoggerConfig,
+        logger?: ILogger
+    ): void {
+        if (this._config || this._logger) {
             throw "LoggerService has been alread initialized!";
         }
         this._config = config;
+        this._logger = logger
     }
 
-    public static getInstance(logger?: ILogger): LoggerService {
+    public static getInstance(): LoggerService {
         if (!this._instance) {
-            this._instance = new LoggerService(this._config ?? DEFAULT_CONFIG, logger);
+            this._instance = new LoggerService(
+                this._config ?? DEFAULT_CONFIG,
+                this._logger ?? console
+            );
         }
         return this._instance;
     }
