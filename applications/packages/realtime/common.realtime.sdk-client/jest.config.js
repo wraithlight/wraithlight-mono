6837ad4aa324c93028f59f69d87fdf9d6bb153be
kfarkasHU <sohamar@farkask.eu>
2024-01-03 00:05:40 +0100

    feat(*): add realtime client pacakage
    Ref: #5

---
+++ applications/packages/realtime/common.realtime.sdk-client/jest.config.js
@@ -0,0 +1,16 @@
+const config = require("../../../jest.config.js");
+
+module.exports = config({
+    preset: "ts-jest",
+    displayName: "Packages/Realtime/Common/Client SDK",
+    testMatch: [
+      "<rootDir>/src/**/*.spec.ts"
+    ],
+    testPathIgnorePatterns: [
+      "dist"
+    ],
+    coveragePathIgnorePatterns: [
+      "dist"
+    ],
+    passWithNoTests: true
+});
