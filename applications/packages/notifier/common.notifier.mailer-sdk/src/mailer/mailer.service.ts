100b33b0c5637bc8f97d7b373103030da29194d5
kfarkasHU <sohamar@farkask.eu>
2024-01-03 13:25:23 +0100

    refactor(*): use nodemailer facade to send mails
    Ref: #596

---
+++ applications/packages/notifier/common.notifier.mailer-sdk/src/mailer/mailer.service.ts
@@ -0,0 +1,60 @@
+import { UNKNOWN_ERROR } from "@wraithlight/core.common-constants";
+import {
+    IMailSender,
+    IMailSenderSendMailResult
+} from "@wraithlight/core.notifier.types";
+import {
+    NodemailerFacadeService
+} from "@wraithlight/facade.nodemailer";
+
+export class MailerService implements IMailSender {
+
+    private readonly _nodemailerFacade = new NodemailerFacadeService(
+        this._host,
+        this._port,
+        this._isSecure,
+        this._username,
+        this._password
+    );
+
+    constructor(
+        private readonly _host: string,
+        private readonly _port: number,
+        private readonly _isSecure: boolean,
+        private readonly _username: string,
+        private readonly _password: string
+    ) {}
+
+    public async sendEmail(
+        toAddress: string,
+        fromAddress: string,
+        subject: string,
+        content: string,
+        isHtml?: boolean,
+        cc?: ReadonlyArray<string>,
+        bcc?: ReadonlyArray<string>
+    ): Promise<IMailSenderSendMailResult> {
+        return this._nodemailerFacade.sendMail(
+            fromAddress,
+            toAddress,
+            subject,
+            undefined,
+            content,
+            bcc,
+            cc
+        )
+        .then(() => {
+            return {
+                success: true,
+                errors: []
+            }
+        })
+        .catch(() => {
+            return {
+                success: false,
+                errors: [UNKNOWN_ERROR]
+            }
+        })
+    }
+
+}
