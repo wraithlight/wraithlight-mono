3a7bb8618f6d77aecdd03a81fd8d86dec93ac3b3
kfarkasHU <sohamar@farkask.eu>
2024-01-07 20:54:20 +0100

    style(*): lint issues
    Ref: #583

--- applications/packages/common/environment-static/common.environment-static.server/src/configuration/server.config.ts
+++ applications/packages/common/environment-static/common.environment-static.server/src/configuration/server.config.ts
@@ -8,11 +8,11 @@ import { SERVER_FORUM_CONFIG } from "./forum";
 import { SERVER_GAME_APPLICATION_CONFIG } from "./game-application";
 import { SERVER_GAME_WEBSITE_CONFIG } from "./game-website";
 import { SERVER_LOGS_CONFIG } from "./logs";
+import { SERVER_LOGS_COLLECTOR_CONFIG } from "./logs-collector";
 import { SERVER_NOTIFIER_CONFIG } from "./notifier";
 import { SERVER_REMOTE_CONFIG_CONFIG } from "./remote-config";
 import { SERVER_USER_MANAGEMENT_CONFIG } from "./user-management";
 import { SERVER_WEBSITE_CONFIG } from "./website";
-import { SERVER_LOGS_COLLECTOR_CONFIG } from "./logs-collector";
 
 export const SERVER_CONFIG: Readonly<EnvironmentStaticServer> = {
     [ApplicationName.Content]: SERVER_CONTENT_CONFIG,
