985e4e425b0a245ccd9f3a8b89f2d024e478d4da
kfarkasHU <sohamar@farkask.eu>
2024-02-09 21:23:14 +0100

    refactor(*): add proper teardown to httpclient
    Ref: #559

--- applications/packages/core/core.http/src/index.ts
+++ applications/packages/core/core.http/src/index.ts
@@ -1,5 +1,2 @@
 export * from "./client";
-
-export * from "./constant/http-code.const";
-export * from "./constant/http-verb.const";
-export * from "./constant/http-header.const";
+export * from "./constant";
