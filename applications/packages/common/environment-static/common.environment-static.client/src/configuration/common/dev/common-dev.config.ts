19c0409337c6518659355efbbaa45cf24e737237
kfarkasHU <sohamar@farkask.eu>
2024-02-09 21:37:52 +0100

    refactor(configuration): move common config under its location
    Ref: #560

---
+++ applications/packages/common/environment-static/common.environment-static.client/src/configuration/common/dev/common-dev.config.ts
@@ -0,0 +1,13 @@
+import { CommonClient } from "@wraithlight/core.environment-static.types";
+import { LogSeverity } from "@wraithlight/core.logger.types";
+
+export const CLIENT_DEV_COMMON_CONFIG: CommonClient = {
+    logging: {
+        enabledLogSeverities: [
+            LogSeverity.DEBUG,
+            LogSeverity.INFO,
+            LogSeverity.WARNING,
+            LogSeverity.ERROR,
+        ]
+    }
+}
