087d05b6fdf151a5403e5b0d5cb35f6bab030c51
kfarkasHU <sohamar@farkask.eu>
2024-02-10 18:12:47 +0100

    feat(user management): add session related configuration
    Ref: #566

--- applications/packages/common/environment-static/core.environment-static.types/src/types/server/apps/user-management/user-management-server.model.ts
+++ applications/packages/common/environment-static/core.environment-static.types/src/types/server/apps/user-management/user-management-server.model.ts
@@ -1,5 +1,8 @@
 import { DatabaseServer } from "../_internal";
 
+import { UserManagementServerSession } from "./session.model";
+
 export interface UserManagementServer {
     database: DatabaseServer;
+    session: UserManagementServerSession;
 }
