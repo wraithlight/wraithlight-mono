c0ea1afe5f6097d2586f8cb0f52770c62e0a9575
kfarkasHU <sohamar@farkask.eu>
2024-02-17 19:04:56 +0100

    feat(*): create `core.environment-static.env` lib
    Ref: #628

---
+++ applications/packages/common/environment-static/core.environment-static.env/src/env/env.spec.ts
@@ -0,0 +1,38 @@
+const getStringSpy = jest.fn();
+const getNumberSpy = jest.fn();
+
+jest.mock("@wraithlight/core.env", () => {
+    return {
+        CoreEnvironment: {
+            getString: getStringSpy,
+            getNumber: getNumberSpy
+        }
+    }
+});
+
+import { EnvironmentStaticEnv } from "./env";
+
+describe("EnvironmentStaticEnvSpecs", () => {
+    describe("given the utility is initialized", () => {
+        describe("when i try to read a numeric value", () => {
+            beforeEach(() => {
+                EnvironmentStaticEnv.userManagementDatabasePort();
+            });
+            it("should call the underlying env reader", () => {
+                expect(getNumberSpy).toHaveBeenCalled();
+                expect(getNumberSpy).toHaveBeenCalledTimes(1);
+                expect(getNumberSpy).toHaveBeenCalledWith("userManagementDbPort", "");
+            });
+        });
+        describe("when i try to read a string value", () => {
+            beforeEach(() => {
+                EnvironmentStaticEnv.userManagementDatabaseName();
+            });
+            it("should call the underlying env reader", () => {
+                expect(getStringSpy).toHaveBeenCalled();
+                expect(getStringSpy).toHaveBeenCalledTimes(1);
+                expect(getStringSpy).toHaveBeenCalledWith("userManagementDbName", "");
+            });
+        });
+    });
+});
---
+++ applications/packages/common/environment-static/core.environment-static.env/src/env/env.ts
@@ -0,0 +1,8 @@
+import { CoreEnvironment } from "@wraithlight/core.env";
+
+export namespace EnvironmentStaticEnv {
+    export const userManagementDatabaseUsername = (): string => CoreEnvironment.getString("userManagementDbUsername", "");
+    export const userManagementDatabasePassword = (): string => CoreEnvironment.getString("userManagementDbPassword", "");
+    export const userManagementDatabaseName = (): string => CoreEnvironment.getString("userManagementDbName", "");
+    export const userManagementDatabasePort = (): number => CoreEnvironment.getNumber("userManagementDbPort", 0);
+}
