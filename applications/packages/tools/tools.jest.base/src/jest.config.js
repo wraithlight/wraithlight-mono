994060ebcebb6fdfc662b0ab276f2b9d4393d996
kfarkasHU <sohamar@farkask.eu>
2024-02-17 19:39:20 +0100

    fix(*): jest mapping

--- applications/packages/tools/tools.jest.base/src/jest.config.js
+++ applications/packages/tools/tools.jest.base/src/jest.config.js
@@ -50,7 +50,7 @@ module.exports = (project, dirname) => {
             `${dirname}/packages/core/$1/src`,
             `${dirname}/packages/common/$1/src`,
             `${dirname}/packages/core/logger/$1/src`,
-            `${dirname}/packages/common/environment-static/$1/src`,
+            `${dirname}/packages/environment-static/$1/src`,
             `${dirname}/packages/auth/$1/src`,
             `${dirname}/packages/game/$1/src`,
             `${dirname}/packages/logs/$1/src`,
