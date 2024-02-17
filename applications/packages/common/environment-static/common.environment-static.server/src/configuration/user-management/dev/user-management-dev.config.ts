ffff59bcecee32c0970a81ab08b9846592d2abf6
kfarkasHU <sohamar@farkask.eu>
2024-02-17 19:07:30 +0100

    feat(dev): consume env vars
    Ref: #628

--- applications/packages/common/environment-static/common.environment-static.server/src/configuration/user-management/dev/user-management-dev.config.ts
+++ applications/packages/common/environment-static/common.environment-static.server/src/configuration/user-management/dev/user-management-dev.config.ts
@@ -1,12 +1,13 @@
+import { EnvironmentStaticEnv } from "@wraithlight/core.environment-static.env";
 import { UserManagementServer } from "@wraithlight/core.environment-static.types";
 
 export const SERVER_DEV_USER_MANAGEMENT_CONFIG: Readonly<UserManagementServer> = {
     database: {
         host: "",
-        port: 0,
-        username: "",
-        password: "",
-        database: "",
+        port: EnvironmentStaticEnv.userManagementDatabasePort(),
+        username: EnvironmentStaticEnv.userManagementDatabaseUsername(),
+        password: EnvironmentStaticEnv.userManagementDatabasePassword(),
+        database: EnvironmentStaticEnv.userManagementDatabaseName(),
     },
     session: {
         iv: "",
