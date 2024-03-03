89f8cce2d066b2318d54d7cd5fe97a8b63977917
kfarkasHU <sohamar@farkask.eu>
2024-03-03 15:50:03 +0100

    chore(*): review `passWithNoTests`
    Ref: #599

--- applications/packages/game-website/core.game-website.constants/jest.config.js
+++ applications/packages/game-website/core.game-website.constants/jest.config.js
@@ -2,7 +2,6 @@ const config = require("../../../jest.config.js");
 
 module.exports = config({
     preset: "ts-jest",
-    passWithNoTests: true,
     displayName: "Packages/Core/Game Website/Constants",
     testMatch: [
       "<rootDir>/src/**/*.spec.ts"
