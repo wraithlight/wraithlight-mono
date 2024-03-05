5f3ff95cf8fc06c14d7516a9193b487987826d53
kfarkasHU <sohamar@farkask.eu>
2024-03-05 18:50:05 +0100

    style(deepmerge): align to `@typescript-eslint/consistent-type-assertions`
    Ref: #633

--- applications/packages/facade/facade.deepmerge/src/deepmerge/deepmerge.fn.ts
+++ applications/packages/facade/facade.deepmerge/src/deepmerge/deepmerge.fn.ts
@@ -2,5 +2,6 @@ import { deepmerge } from "deepmerge-ts";
 
 export function deepmergeFacade<T>(...objects: Array<T>): T {
     const result = deepmerge(...objects);
+    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
     return result as T;
 }
