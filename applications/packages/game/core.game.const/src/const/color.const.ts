0f2ea57c48155868c91e3c3c515e584f3d4341b5
kfarkasHU <sohamar@farkask.eu>
2024-03-05 17:47:41 +0100

    refactor(*): rename to `core.game.constants`
    Ref: #648

--- applications/packages/game/core.game.const/src/const/color.const.ts
+++ applications/packages/game/core.game.const/src/const/color.const.ts
@@ -1,6 +1,7 @@
 export const enum Color {
     /**
      * In memoriam G - never forget!
+     * https://github.com/wraithlight/wraithlight-mono/commit/09bfc23435b10cbbb53126c7ee1819a86e93f627#diff-08d5b4a14934000386b15b1f1ba6fe5f1146a8fa789bad162f8e18172e241342
      */
     MISSING_TEXTURE = "#FFFF8F"
 }
