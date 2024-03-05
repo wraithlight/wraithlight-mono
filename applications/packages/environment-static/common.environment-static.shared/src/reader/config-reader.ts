cb98a00ef054ed4747d4bfb8cad3e4c27a63d507
kfarkasHU <sohamar@farkask.eu>
2024-03-05 18:49:57 +0100

    style(reader): align to `@typescript-eslint/consistent-type-assertions`
    Ref: #633

--- applications/packages/environment-static/common.environment-static.shared/src/reader/config-reader.ts
+++ applications/packages/environment-static/common.environment-static.shared/src/reader/config-reader.ts
@@ -4,13 +4,15 @@ import { CommonShared } from "@wraithlight/core.environment-static.types";
 
 import { SHARED_CONFIG } from "../configuration";
 
-export class SharedConfigurationReader<TApplication> extends ConfigurationReader<TApplication, CommonShared> {
+export class SharedConfigurationReader<TApplication>
+    extends ConfigurationReader<TApplication, CommonShared> {
 
     constructor(
         private readonly _application: ApplicationName,
         private readonly _environment: EnvironmentType
     ) {
         super(
+            // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
             SHARED_CONFIG[_application][_environment] as TApplication,
             SHARED_CONFIG.common[_environment]
         );
