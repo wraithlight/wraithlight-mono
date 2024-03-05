27b038d0400eec595a137ee367a2a7fe34637e09
kfarkasHU <sohamar@farkask.eu>
2024-03-05 17:47:10 +0100

    refactor(*): use `core.game.constants` instead
    Ref: #648

--- applications/packages/game/core.game.layer/src/layer/layer.ts
+++ applications/packages/game/core.game.layer/src/layer/layer.ts
@@ -1,5 +1,5 @@
 import { Renderer } from "@wraithlight/common.game.renderer";
-import { Color } from "@wraithlight/core.game.const";
+import { Color } from "@wraithlight/core.game.constants";
 import { Position, Sprite } from "@wraithlight/core.game.types";
 
 export class Layer {
