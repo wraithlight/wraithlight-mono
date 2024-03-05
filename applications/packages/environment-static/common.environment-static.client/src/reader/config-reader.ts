0cd5edde7dd58debf04ed7b1ee407d8d4a5899ab
kfarkasHU <sohamar@farkask.eu>
2024-03-05 18:49:52 +0100

    style(reader): align to `@typescript-eslint/consistent-type-assertions`
    Ref: #633

--- applications/packages/environment-static/common.environment-static.client/src/reader/config-reader.ts
+++ applications/packages/environment-static/common.environment-static.client/src/reader/config-reader.ts
@@ -4,13 +4,15 @@ import { CommonClient } from "@wraithlight/core.environment-static.types";
 
 import { CLIENT_CONFIG } from "../configuration";
 
-export class ClientConfigurationReader<TApplication> extends ConfigurationReader<TApplication, CommonClient> {
+export class ClientConfigurationReader<TApplication>
+    extends ConfigurationReader<TApplication, CommonClient> {
 
     constructor(
         private readonly _application: ApplicationName,
         private readonly _environment: EnvironmentType
     ) {
         super(
+            // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
             CLIENT_CONFIG[_application][_environment] as TApplication,
             CLIENT_CONFIG.common[_environment]
         );
