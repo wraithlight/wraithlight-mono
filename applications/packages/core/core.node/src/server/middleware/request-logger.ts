2458ca97a5c63f9a820db83cc57b438c52cd662f
kfarkasHU <sohamar@farkask.eu>
2024-03-09 13:55:49 +0100

    feat(server): ability to log each incoming request
    Ref: #659

---
+++ applications/packages/core/core.node/src/server/middleware/request-logger.ts
@@ -0,0 +1,12 @@
+import { LoggerService } from "@wraithlight/common.logger.sdk";
+import { NextFunction, Request, Response } from "express";
+
+export function requestLogger(
+    req: Request,
+    _res: Response,
+    next: NextFunction
+): void {
+    const logger = LoggerService.getInstance();
+    logger.info(`[REQUEST] ${req.method} ${req.path}`);
+    next();
+}
