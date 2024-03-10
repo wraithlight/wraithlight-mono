b3d8e1dadae4b84da26908ec70b034830298c7c1
kfarkasHU <sohamar@farkask.eu>
2024-03-10 15:29:32 +0100

    chore(*): import jest config from the monorepo
    Ref: #545

--- applications/packages/game-website/core.game-website.types/jest.config.js
+++ applications/packages/game-website/core.game-website.types/jest.config.js
@@ -1,6 +1,8 @@
-const config = require("../../../jest.config.js");
+const config = require("@wraithlight/tools.jest.base");
+const { join } = require("path");
 
-module.exports = config({
+module.exports = config(
+  {
     preset: "ts-jest",
     displayName: "Packages/Core/Game Website/Types",
     testMatch: [
@@ -12,4 +14,6 @@ module.exports = config({
     coveragePathIgnorePatterns: [
       "dist"
     ]
-});
+  },
+  join(__dirname, "../../..")
+);
