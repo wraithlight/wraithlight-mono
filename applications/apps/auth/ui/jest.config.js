a167524f11613da3b63df34f318f8c643014529b
kfarkasHU <sohamar@farkask.eu>
2024-03-10 15:27:36 +0100

    chore(*): import jest config from the monorepo
    Ref: #545

--- applications/apps/auth/ui/jest.config.js
+++ applications/apps/auth/ui/jest.config.js
@@ -1,11 +1,12 @@
-import config from "../../../jest.config.js";
+import config from "@wraithlight/tools.jest.base";
 import { dirname, join } from "path";
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-export default config({
+export default config(
+  {
     preset: "ts-jest",
     displayName: "Apps/Auth/UI",
     testMatch: [
@@ -20,4 +21,6 @@ export default config({
     transform: {
       '.ts': ['ts-jest', { tsconfig: join(__dirname, 'tsconfig.jest.json') }]
     }
-});
+  },
+  join(__dirname, "../../..")
+);
