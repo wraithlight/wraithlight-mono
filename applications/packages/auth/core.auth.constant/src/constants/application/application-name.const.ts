8d8581df1860baad92182a91cab4b84ab0b1d455
kfarkasHU <sohamar@farkask.eu>
2024-03-19 23:03:22 +0100

    feat(*): add application related ones to auth constant
    Ref: #697

---
+++ applications/packages/auth/core.auth.constant/src/constants/application/application-name.const.ts
@@ -0,0 +1,12 @@
+export enum ApplicationName {
+    Website = "WEBSITE",
+    GameWebsite = "GAME_WEBSITE",
+    GameApplication = "GAME_APPLICATION",
+    Content = "CONTENT",
+    UserManagement = "USER_MANAGEMENT",
+    Forum = "FORUM",
+    Logs = "LOGS",
+    Editor = "EDITOR",
+    Notifier = "NOTIFIER",
+    RemoteConfig = "REMOTE_CONFIG"
+}
