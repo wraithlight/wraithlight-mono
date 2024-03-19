03adbf86b5c92e5d43d8b971e89d93b715c3000f
kfarkasHU <sohamar@farkask.eu>
2024-03-19 16:57:06 +0100

    refactor(*): use `createNodeServer`
    Ref: #15

--- applications/apps/notifier/node/src/server.spec.ts
+++ applications/apps/notifier/node/src/server.spec.ts
@@ -1,6 +1,6 @@
 jest.mock("@wraithlight/core.server", () => {
     return {
-        createNodeServerV2: jest.fn()
+        createNodeServer: jest.fn()
     }
 });
 jest.mock("./controller", () => {
@@ -39,14 +39,14 @@ jest.mock("@wraithlight/common.environment-static.shared", () => {
     }
 });
 
-import { createNodeServerV2 } from "@wraithlight/core.server";
+import { createNodeServer } from "@wraithlight/core.server";
 
 describe("ServerSpecs", () => {
     import("./server");
     describe("given the server has been started", () => {
-        it("should call `createNodeServerV2`", () => {
-            expect(createNodeServerV2).toHaveBeenCalled();
-            expect(createNodeServerV2).toHaveBeenCalledTimes(1);
+        it("should call `createNodeServer`", () => {
+            expect(createNodeServer).toHaveBeenCalled();
+            expect(createNodeServer).toHaveBeenCalledTimes(1);
         });
     });
 });
