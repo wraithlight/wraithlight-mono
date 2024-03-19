2e3bcd2eed5dc80b9edc7b800ef127b9f5c82eb3
kfarkasHU <sohamar@farkask.eu>
2024-03-19 16:13:05 +0100

    fix(*): mocks mapping
    Ref: #713

--- applications/packages/tools/tools.jest.base/src/jest.config.js
+++ applications/packages/tools/tools.jest.base/src/jest.config.js
@@ -45,6 +45,7 @@ module.exports = (project, dirname) => {
             `${dirname}/packages/realtime/$1/__mocks__`,
             `${dirname}/packages/facade/$1/__mocks__`,
             `${dirname}/packages/core/$1/__mocks__`,
+            `${dirname}/packages/core/mustache/$1/__mocks__`,
             // TODO: create mocks for these libs. (#571)
             `${dirname}/packages/$1/src`,
             `${dirname}/packages/core/$1/src`,
