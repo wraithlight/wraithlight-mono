53282b2feb69b62ffde7bf01353c7e4cae9b72e4
kfarkasHU <sohamar@farkask.eu>
2024-03-19 22:23:48 +0100

    feat(provider): add guard mechanism
    Ref: #682

--- applications/packages/realtime/common.realtime.sdk-server/src/provider/realtime.model.ts
+++ applications/packages/realtime/common.realtime.sdk-server/src/provider/realtime.model.ts
@@ -1,6 +1,13 @@
+import { IncomingMessage } from "http";
+
 import { RealtimeMessage } from "@wraithlight/core.realtime.types";
 
 export interface AsyncRealtimeMessage<T = unknown> {
     id: string;
     message: RealtimeMessage<T>;
 }
+
+
+type InternalNextFunction = (error?: Error) => void;
+
+export type SocketGuard = (request: IncomingMessage, next: InternalNextFunction) => void;
