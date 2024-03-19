9c3dd45a4b69ed7d6c6266535b77c8bc5b85f850
kfarkasHU <sohamar@farkask.eu>
2024-03-19 23:05:24 +0100

    refactor(*): consume application related ones from auth constant
    Ref: #697

--- applications/packages/logs/core.logs.sdk/src/service/logger.service.ts
+++ applications/packages/logs/core.logs.sdk/src/service/logger.service.ts
@@ -1,4 +1,4 @@
-import { ApplicationName } from "@wraithlight/core.common-constants";
+import { ApplicationName } from "@wraithlight/core.auth.constant";
 import { HttpBeaconClient } from "@wraithlight/core.http";
 import { LogSeverity } from "@wraithlight/core.logger.types";
 import { BeaconLogEntry } from "@wraithlight/core.logs.types";
