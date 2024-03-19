6d38e5d97726226e308b42338477f6dca01aff84
kfarkasHU <sohamar@farkask.eu>
2024-03-19 19:10:43 +0100

    chore(*): ignore spec files during build
    Ref: #537

--- batch-committer.sh
+++ batch-committer.sh
@@ -4,5 +4,7 @@ files=$(git status --porcelain | cut -b4-)
 for file in $files; do
     echo $file
     git add $file
-    git commit -m "chore(*): add resolver dependency"
+    git commit -m "chore(*): ignore spec files during build
+
+Ref: #537"
 done
