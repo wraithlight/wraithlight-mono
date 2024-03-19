603635918c3cf907f24b3668a7a4c1d0efcf5d0f
kfarkasHU <sohamar@farkask.eu>
2024-03-19 15:10:07 +0100

    style(const): resolve eslint semi issues
    Ref: #689

--- applications/packages/notifier/core.notifier.const/src/const/endpoints.const.ts
+++ applications/packages/notifier/core.notifier.const/src/const/endpoints.const.ts
@@ -1,6 +1,6 @@
 const API_BASE = "/api";
 const API_V1_BASE = `${API_BASE}/v1`;
-const API_V1_SEND_BASE = `${API_V1_BASE}/send`
+const API_V1_SEND_BASE = `${API_V1_BASE}/send`;
 
 export const NOTIFIER_ENDPOINT_CONST = Object.freeze({
     v1: {
--- applications/packages/notifier/core.notifier.const/src/const/webhook.const.ts
+++ applications/packages/notifier/core.notifier.const/src/const/webhook.const.ts
@@ -1,6 +1,6 @@
 const API_BASE = "/api";
 const API_V1_BASE = `${API_BASE}/v1`;
-const API_V1_MAIL_BASE = `${API_V1_BASE}/mail`
+const API_V1_MAIL_BASE = `${API_V1_BASE}/mail`;
 
 export const NOTIFIER_WEBHOOK_ENDPOINTS = Object.freeze({
     v1: {
