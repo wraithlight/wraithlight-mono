c5d2b5095a0c0f0c503483f1f6f12745619f0016
kfarkasHU <sohamar@farkask.eu>
2024-03-20 22:01:32 +0100

    feat(*): add `common.api-token.sdk-server` lib
    Ref: #734

---
+++ applications/packages/api-token/common.api-token.sdk-server/src/decorator/api-token-header.decorator.ts
@@ -0,0 +1,35 @@
+import { API_TOKEN_HEADER_NAME } from "@wraithlight/core.api-token.constants";
+import { HttpCode } from "@wraithlight/core.http";
+import {
+    FilterDecorator,
+    FilterResult,
+    IDecoratorFactory
+} from "@wraithlight/core.node";
+import { isNil } from "@wraithlight/core.nullable";
+import { Request } from "express";
+
+export const ApiTokenHeader = (
+    tokens: ReadonlyArray<string>
+): IDecoratorFactory<unknown> => FilterDecorator(async (
+    req: Request
+): Promise<FilterResult> => {
+    const token = req.get(API_TOKEN_HEADER_NAME);
+
+    if (isNil(token)) {
+        return Promise.resolve({
+            success: false,
+            errorHttpCode: HttpCode.BadRequest
+        });
+    }
+
+    if (!tokens.includes(token)) {
+        return Promise.resolve({
+            success: false,
+            errorHttpCode: HttpCode.Unauthorized
+        });
+    }
+
+    return Promise.resolve({
+        success: true
+    });
+});
