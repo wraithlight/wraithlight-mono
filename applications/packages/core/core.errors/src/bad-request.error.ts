fcc50b98a33c735ba07f93fc9f103fb47638fa97
kfarkasHU <sohamar@farkask.eu>
2024-03-21 14:49:09 +0100

    feat(*): add badrequest and unauth errors

---
+++ applications/packages/core/core.errors/src/bad-request.error.ts
@@ -0,0 +1,7 @@
+import { WraithlightError } from "./_wraithlight.error";
+
+export class BadRequestError extends WraithlightError {
+    constructor() {
+        super("BadRequestError", "E_BADREQUEST");
+    }
+}
--- applications/packages/core/core.errors/src/index.ts
+++ applications/packages/core/core.errors/src/index.ts
@@ -4,3 +4,5 @@ export * from "./_wraithlight.error";
 export * from "./already-initialized.error";
 export * from "./invalid-argument.error";
 export * from "./not-initalized.error";
+export * from "./bad-request.error";
+export * from "./unauthorized.error";
