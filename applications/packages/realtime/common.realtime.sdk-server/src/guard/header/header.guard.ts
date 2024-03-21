a63fbc919de13e424c3a9325b2a70a40b56d453d
kfarkasHU <sohamar@farkask.eu>
2024-03-21 15:17:17 +0100

    style(header): remove logs

--- applications/packages/realtime/common.realtime.sdk-server/src/guard/header/header.guard.ts
+++ applications/packages/realtime/common.realtime.sdk-server/src/guard/header/header.guard.ts
@@ -11,8 +11,6 @@ import { RTNextFunction, SocketGuard } from "../../provider";
 export const RTHeaderGuard = (
     tokens: ReadonlyArray<string>
 ): SocketGuard => (request: IncomingMessage, next: RTNextFunction) => {
-    console.log(request.headers);
-    console.log(request.headers[RT_AUTH_HEADER_NAME.toLowerCase()]);
     const token = request.headers[RT_AUTH_HEADER_NAME.toLowerCase()];
 
     if (isNil(token)) {
