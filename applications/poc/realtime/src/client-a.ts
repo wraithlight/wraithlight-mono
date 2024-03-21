073f6c49d1c256dddd5e457e86ed7bad2bfcc3d0
kfarkasHU <sohamar@farkask.eu>
2024-03-21 14:50:28 +0100

    feat(*): update POC with guards

--- applications/poc/realtime/src/client-a.ts
+++ applications/poc/realtime/src/client-a.ts
@@ -3,11 +3,17 @@ process.env.DEBUG = "true";
 import { RealtimeService } from "@wraithlight/common.realtime.sdk-client";
 
 import { RT_PATH, SERVER_PORT, TOPIC_A, TOPIC_B, TOPIC_CONNECTED, TOPIC_DISCONNECTED } from "./shared";
+import { RtAuthType } from "@wraithlight/core.realtime.types";
 
 async function run() {
     await new Promise((resolve) => setTimeout(() => resolve(true), 2_000));
     const url = `http://localhost:${SERVER_PORT}`;
-    const service = new RealtimeService(url, RT_PATH);
+    const service = new RealtimeService(
+        url,
+        RT_PATH,
+        RtAuthType.Header,
+        "token-1"
+    );
     service.connectSync();
     service.on("connect", () => {
         console.log("CONNECTED SYNC!");
