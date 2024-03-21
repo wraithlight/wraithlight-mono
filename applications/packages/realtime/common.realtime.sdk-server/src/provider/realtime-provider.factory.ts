b7890bac4ebd80ab72505b601a1602d067d029dd
kfarkasHU <sohamar@farkask.eu>
2024-03-21 14:49:54 +0100

    feat(*): add guard mechanisms
    Ref: #740

--- applications/packages/realtime/common.realtime.sdk-server/src/provider/realtime-provider.factory.ts
+++ applications/packages/realtime/common.realtime.sdk-server/src/provider/realtime-provider.factory.ts
@@ -3,15 +3,20 @@ import { Server } from "http";
 import { InvalidArgumentError } from "@wraithlight/core.errors";
 import { Nullable, isNil } from "@wraithlight/core.nullable";
 
+import { SocketGuard } from "./realtime.model";
 import { RealtimeProvider } from "./realtime.provider";
 
 export class RealtimeProviderFactory {
 
     private _instance: Nullable<RealtimeProvider>;
 
-    public createInstance(server: Server, path: string): void {
+    public createInstance(
+        server: Server,
+        path: string,
+        guards?: ReadonlyArray<SocketGuard>
+    ): void {
         if (isNil(this._instance)) {
-            this._instance = new RealtimeProvider(server, path);
+            this._instance = new RealtimeProvider(server, path, guards);
         }
     }
 
