c8f8dcd84247abb2d4b294e00af192f16b2b43d1
kfarkasHU <sohamar@farkask.eu>
2024-03-22 13:08:15 +0100

    style(*): resolve `max-len` issues
    Ref: #550

--- applications/poc/realtime/src/client-a.ts
+++ applications/poc/realtime/src/client-a.ts
@@ -1,12 +1,13 @@
 process.env.DEBUG = "true";
 
 import { RealtimeService } from "@wraithlight/common.realtime.sdk-client";
-
-import { RT_PATH, SERVER_PORT, TOPIC_A, TOPIC_B, TOPIC_CONNECTED, TOPIC_DISCONNECTED } from "./shared";
 import { RtAuthType } from "@wraithlight/core.realtime.types";
 
-async function run() {
-    await new Promise((resolve) => setTimeout(() => resolve(true), 2_000));
+import { RT_PATH, SERVER_PORT, TOPIC_A, TOPIC_B, TOPIC_CONNECTED, TOPIC_DISCONNECTED } from "./shared";
+
+async function run(): Promise<void> {
+    const timeout = 2_000;
+    await new Promise((resolve) => setTimeout(() => resolve(true), timeout));
     const url = `http://localhost:${SERVER_PORT}`;
     const service = new RealtimeService(
         url,
@@ -16,23 +17,28 @@ async function run() {
     );
     service.connectSync();
     service.on("connect", () => {
+        // eslint-disable-next-line no-console
         console.log("CONNECTED SYNC!");
         service.send(TOPIC_A, "A-A");
         service.send(TOPIC_B, "A-B");
 
         service.on(TOPIC_A, (m) => {
+            // eslint-disable-next-line no-console
             console.log(TOPIC_A, m);
         });
         service.on(TOPIC_B, (m) => {
+            // eslint-disable-next-line no-console
             console.log(TOPIC_B, m);
         });
 
-        new Promise(() => setTimeout(() => service.disconnect(), 2_000));
+        new Promise(() => setTimeout(() => service.disconnect(), timeout));
     });
     service.on(TOPIC_CONNECTED, (m) => {
+        // eslint-disable-next-line no-console
         console.log(TOPIC_CONNECTED, m);
     });
     service.on(TOPIC_DISCONNECTED, (m) => {
+        // eslint-disable-next-line no-console
         console.log(TOPIC_CONNECTED, m);
     });
 }
--- applications/poc/realtime/src/server.ts
+++ applications/poc/realtime/src/server.ts
@@ -1,5 +1,4 @@
-import { ApplicationName } from "@wraithlight/core.auth.constant";
-import { createNodeServer } from "@wraithlight/core.server";
+import { HealthCheckControllerV1 } from "@wraithlight/common.health-checker.sdk-server";
 import {
     AsyncRealtimeMessage,
     ON_CLIENT_CONNECTING_TOKEN,
@@ -7,17 +6,19 @@ import {
     RTHeaderGuard,
     RealtimeProviderFactory
 } from "@wraithlight/common.realtime.sdk-server";
-import { HealthCheckControllerV1 } from "@wraithlight/common.health-checker.sdk-server";
+import { ApplicationName } from "@wraithlight/core.auth.constant";
+import { createNodeServer } from "@wraithlight/core.server";
 
 import { RT_PATH, SERVER_PORT, TOPIC_A, TOPIC_B, TOPIC_CONNECTED, TOPIC_DISCONNECTED } from "./shared";
 
-function run() {
+function run(): void {
     const rtFactory = new RealtimeProviderFactory();
 
     createNodeServer(
+        // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
         "Realtime POC Server" as ApplicationName,
         [new HealthCheckControllerV1("rt-poc", "1.0.0")],           // TODO: Remove this.
-        [(server) => rtFactory.createInstance(
+        [(server): unknown => rtFactory.createInstance(
             server,
             RT_PATH,
             [
@@ -28,25 +29,29 @@ function run() {
         undefined
     );
 
-    const rtProvider = rtFactory.getInstance()
+    const rtProvider = rtFactory.getInstance();
     const mBus = rtProvider.getMessageBus();
     mBus.sub(ON_CLIENT_CONNECTING_TOKEN, (id: string) => {
         rtProvider.sendToAll(TOPIC_CONNECTED, id);
+        // eslint-disable-next-line no-console
         console.log(`Client connected: '${id}'`);
     });
     mBus.sub(ON_CLIENT_DISCONNECTING_TOKEN, (id: string) => {
         rtProvider.sendToAll(TOPIC_DISCONNECTED, id);
+        // eslint-disable-next-line no-console
         console.log(`Client disconnected: '${id}'`);
     });
     mBus.sub(TOPIC_A, (m: AsyncRealtimeMessage) => {
         rtProvider.sendTo(m.id, TOPIC_A, `REPLY:: ${m.message.payload}`);
+        // eslint-disable-next-line no-console
         console.log(`(${TOPIC_A}) '${m.id}' says: ${JSON.stringify(m.message)}`);
     });
     mBus.sub(TOPIC_B, (m: AsyncRealtimeMessage) => {
         rtProvider.sendToAll(TOPIC_B, m.message.payload);
+        // eslint-disable-next-line no-console
         console.log(`(${TOPIC_B}) '${m.id}' says: ${JSON.stringify(m.message)}`);
     });
 }
 
 
-run()
+run();
