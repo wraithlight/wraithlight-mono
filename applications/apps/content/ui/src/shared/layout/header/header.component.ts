e82a17100fd8dd3ce1386cf81373fbc4082ef212
kfarkasHU <sohamar@farkask.eu>
2024-03-26 06:46:28 +0100

    style(*): align to eslint
    Ref: #756

--- applications/apps/content/ui/src/shared/layout/header/header.component.ts
+++ applications/apps/content/ui/src/shared/layout/header/header.component.ts
@@ -6,8 +6,20 @@ export class HeaderComponent implements Component {
     public view(): Children {
         return m(
             "div.header",
-            m("div.title", m("span", "Content")),
-            m("div.settings", m("span", "User info here"))
+            m(
+                "div.title",
+                m(
+                    "span",
+                    "Content"
+                )
+            ),
+            m(
+                "div.settings",
+                m(
+                    "span",
+                    "User info here"
+                )
+            )
         );
     }
 
