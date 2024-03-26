e34dd1cc92b188c5653306548a2c0a042e0940bb
kfarkasHU <sohamar@farkask.eu>
2024-03-26 06:50:27 +0100

    style(mustache): align to eslint
    Ref: #756

--- applications/packages/core/mustache/core.mustache/src/mustache/mustache.fn.ts
+++ applications/packages/core/mustache/core.mustache/src/mustache/mustache.fn.ts
@@ -1,5 +1,8 @@
 import { mustacheFacade } from "@wraithlight/facade.mustache";
 
 export function wlMustache<T>(template: string, data: T): string {
-    return mustacheFacade(template, data).renderedTemplate;
+    return mustacheFacade(
+        template,
+        data
+    ).renderedTemplate;
 }
