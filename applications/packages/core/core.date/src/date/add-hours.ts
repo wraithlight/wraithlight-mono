b05c8d45dcbb93789d48916044fcdac7a1cf65d3
kfarkasHU <sohamar@farkask.eu>
2024-03-26 06:49:50 +0100

    style(date): align to eslint
    Ref: #756

--- applications/packages/core/core.date/src/date/add-hours.ts
+++ applications/packages/core/core.date/src/date/add-hours.ts
@@ -3,5 +3,8 @@ import { hoursToMinutes } from "./operations";
 
 export function addHours(date: Date, hours: number): Date {
     const m = hoursToMinutes(hours);
-    return addMinutes(date, m);
+    return addMinutes(
+        date,
+        m
+    );
 }
