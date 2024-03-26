a5948eb38c9f5fad7936186cf0514c94ee212e74
kfarkasHU <sohamar@farkask.eu>
2024-03-26 06:50:09 +0100

    style(*): align to eslint
    Ref: #756

--- applications/packages/core/core.node/src/crud/decorator/get.decorator.ts
+++ applications/packages/core/core.node/src/crud/decorator/get.decorator.ts
@@ -1,4 +1,7 @@
 import { IDecoratorFactory } from "./decorator.model";
 import { HttpDecorator } from "./http.decorator";
 
-export const HttpGet = (path: string): IDecoratorFactory<any> => HttpDecorator("GET", path);
+export const HttpGet = (path: string): IDecoratorFactory<any> => HttpDecorator(
+    "GET",
+    path
+);
