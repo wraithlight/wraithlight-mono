1f05e27361a2a84a354df77e4adf66dae626a008
kfarkasHU <sohamar@farkask.eu>
2024-03-26 06:52:29 +0100

    style(deepmerge): align to eslint
    Ref: #756

--- applications/packages/core/deepmerge/core.deepmerge/src/deepmerge/deepmerge.fn.ts
+++ applications/packages/core/deepmerge/core.deepmerge/src/deepmerge/deepmerge.fn.ts
@@ -7,5 +7,8 @@ export function wlDeepmerge<T>(
     additional: DeepPartial<T> | Partial<T> | T
 ): T {
     // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-    return deepmergeFacade(target, additional) as T;
+    return deepmergeFacade(
+        target,
+        additional
+    ) as T;
 }
