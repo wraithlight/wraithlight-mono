3527ebe67888155839e9b9c916afceb5655fb4ef
kfarkasHU <sohamar@farkask.eu>
2024-03-26 06:48:03 +0100

    style(env): align to eslint
    Ref: #756

--- applications/packages/environment-static/core.environment-static.env/src/env/env.ts
+++ applications/packages/environment-static/core.environment-static.env/src/env/env.ts
@@ -1,8 +1,24 @@
 import { CoreEnvironment } from "@wraithlight/core.env.sdk";
 
 export namespace EnvironmentStaticEnv {
-    export const userManagementDatabaseUsername = (): string => CoreEnvironment.getString("userManagementDbUsername", "");
-    export const userManagementDatabasePassword = (): string => CoreEnvironment.getString("userManagementDbPassword", "");
-    export const userManagementDatabaseName = (): string => CoreEnvironment.getString("userManagementDbName", "");
-    export const userManagementDatabasePort = (): number => CoreEnvironment.getNumber("userManagementDbPort", 0);
+    export const userManagementDatabaseUsername = (): string => CoreEnvironment
+    .getString(
+        "userManagementDbUsername",
+        ""
+    );
+    export const userManagementDatabasePassword = (): string => CoreEnvironment
+    .getString(
+        "userManagementDbPassword",
+        ""
+    );
+    export const userManagementDatabaseName = (): string => CoreEnvironment
+    .getString(
+        "userManagementDbName",
+        ""
+    );
+    export const userManagementDatabasePort = (): number => CoreEnvironment
+    .getNumber(
+        "userManagementDbPort",
+        0
+    );
 }
