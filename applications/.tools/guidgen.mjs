a82c732b68f3f0526c88b96d08694370035e0231
kfarkasHU <sohamar@farkask.eu>
2024-05-18 22:01:51 +0200

    feat(tools): multiple guids
    Ref: #299

--- applications/.tools/guidgen.mjs
+++ applications/.tools/guidgen.mjs
@@ -1,19 +1,27 @@
 import types from "@wraithlight/core.guid";
 
-const guid = types.newGuid();
+const numberOfGuidsStr = process.argv[2];
+const numberOfGuids = Number(numberOfGuidsStr) || 1;
 
-const banner = "".padStart(guid.length + 4, "#");
-const line = ""
-    .padStart(guid.length + 2, " ")
-    .padStart(1, "#")
-    .padEnd(1, "#")
-;
-const guidLine = `# ${guid} #`
+Array.from({ length: numberOfGuids }).forEach(_ =>generateAndPrintGuid())
 
-console.log(`
-    ${banner}
-    #${line}#
-    ${guidLine}
-    #${line}#
-    ${banner}
-`);
+function generateAndPrintGuid() {
+    const guid = types.newGuid();
+
+    const banner = "".padStart(guid.length + 4, "#");
+    const line = ""
+        .padStart(guid.length + 2, " ")
+        .padStart(1, "#")
+        .padEnd(1, "#")
+    ;
+    const guidLine = `# ${guid} #`
+    
+    console.log(`
+        ${banner}
+        #${line}#
+        ${guidLine}
+        #${line}#
+        ${banner}
+    `);
+    
+}
