702bfc60648679db5595f861e6dcfe785dd62b2b
kfarkasHU <sohamar@farkask.eu>
2024-06-03 23:02:14 +0200

    style(*): remove unused import

--- applications/deplist.mjs
+++ applications/deplist.mjs
@@ -1,6 +1,6 @@
 import { fileURLToPath } from 'url';
 import { dirname, join } from "path";
-import { readFileSync, readdirSync, statSync, writeFileSync } from 'fs';
+import { readFileSync, readdirSync, statSync } from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
