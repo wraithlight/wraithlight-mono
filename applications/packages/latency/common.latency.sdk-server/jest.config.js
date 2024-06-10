5172ab343dd440cbbdc149ba699c71ff5e6546b9
kfarkasHU <sohamar@farkask.eu>
2024-06-11 00:18:00 +0200

    feat(*): add latency server sdk
    Ref: #804

---
+++ applications/packages/latency/common.latency.sdk-server/jest.config.js
@@ -0,0 +1,20 @@
+const config = require("@wraithlight/tools.jest.base");
+const { join } = require("path");
+
+module.exports = config(
+  {
+    preset: "ts-jest",
+    displayName: "Packages/Logs/Common/SDK Client",
+    passWithNoTests: true,  // TODO
+    testMatch: [
+      "<rootDir>/src/**/*.spec.ts"
+    ],
+    testPathIgnorePatterns: [
+      "dist"
+    ],
+    coveragePathIgnorePatterns: [
+      "dist"
+    ]
+  },
+  join(__dirname, "../../..")
+);
