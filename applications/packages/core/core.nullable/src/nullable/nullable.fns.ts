457792fda3a85eecae93d883d4824e43e06b6ef2
kfarkasHU <sohamar@farkask.eu>
2024-07-24 17:30:13 +0200

    feat(nullable): extend the package with
    * `isNan()`
    * `isNanOrNil()`
    * `isEmptyString()`
    * `isEmptyStringOrNil()`
    
    Ref: #828

--- applications/packages/core/core.nullable/src/nullable/nullable.fns.ts
+++ applications/packages/core/core.nullable/src/nullable/nullable.fns.ts
@@ -1,14 +1,43 @@
-/**
- * Checks if the given value is `null` or `undefined`.
- * @param {unknown} valueLike The value to check.
- * @returns {boolean} True if the object is `null` or `undefined` otherwise false.
- */
+import { Nullable } from "./nullable.type";
+
 export function isNil(valueLike: unknown): valueLike is undefined | null {
-    if (valueLike === null) {
-        return true;
-    }
-    if (valueLike === undefined) {
-        return true;
-    }
-    return false;
+  return isNilCore(valueLike);
+}
+
+export function isNan(valueLike: number): valueLike is number {
+  return isNanCore(valueLike);
+}
+
+export function isNanOrNil(
+  valueLike: Nullable<number>
+): valueLike is undefined | null | typeof NaN {
+  return isNilCore(valueLike) || isNanCore(valueLike);
+}
+
+export function isEmptyString(valueLike: string): valueLike is "" {
+  return isEmptyStringCore(valueLike);
+}
+
+export function isEmptyStringOrNil(
+  valueLike: Nullable<string>
+): valueLike is undefined | null | "" {
+  return isNil(valueLike) || isEmptyStringCore(valueLike);
+}
+
+function isEmptyStringCore(valueLike: string): valueLike is "" {
+  return valueLike === "";
+}
+
+function isNanCore(valueLike: number): valueLike is number {
+  return isNaN(valueLike)
+}
+
+function isNilCore(valueLike: unknown): valueLike is undefined | null {
+  if (valueLike === null) {
+    return true;
+  }
+  if (valueLike === undefined) {
+    return true;
+  }
+  return false;
 }
