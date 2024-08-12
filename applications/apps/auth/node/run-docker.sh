06bacf3780f07fe632e05b0ee75990f061bb4d4f
kfarkasHU <sohamar@farkask.eu>
2024-08-12 14:00:00 +0100

    chore(*): DOCKERIZAM
    Ref: #13

---
+++ applications/apps/auth/node/run-docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+CONTAINER_NAME=wraithlight-app-auth
+SQL_ROOT_PASSWORD=wraithlight-auth-app-pw
+SQL_USERNAME=wraithlight-auth-app-user-1
+SQL_PASSWORD=wraithlight-auth-app-user-1-pw
+OUT_PORT=5500
+IN_PORT=4500
+DATABASE_NAME=WL_AUTH
+NETWORK_NAME=WL_Internal
+
+docker stop $CONTAINER_NAME >/dev/null 2>&1 || true
+docker rm $CONTAINER_NAME >/dev/null 2>&1 || true
+docker build -t $CONTAINER_NAME -f ./Dockerfile ../../..
+docker run \
+    -d \
+    -p $OUT_PORT:$IN_PORT \
+    --name $CONTAINER_NAME \
+    --network $NETWORK_NAME \
+    $CONTAINER_NAME
