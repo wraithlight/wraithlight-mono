0f9db0d6f1a367c5fdb504f7306d1508cfbf2f5c
kfarkasHU <sohamar@farkask.eu>
2024-08-27 14:00:00 +0100

    feat(*): add HC and init logger
    Ref: #13

--- applications/apps/remote-config/node/src/server.ts
+++ applications/apps/remote-config/node/src/server.ts
@@ -1,14 +1,24 @@
 import { SharedRemoteConfigConfigReader } from "@wraithlight/common.environment-static.shared";
+import { HealthCheckControllerV1 } from "@wraithlight/common.health-checker.sdk-server";
+import { LoggerService } from "@wraithlight/common.logger.sdk";
 import { ApplicationName } from "@wraithlight/core.auth.constant";
 import { CoreEnvironment } from "@wraithlight/core.env.sdk";
 import { BaseController } from "@wraithlight/core.node";
 import { createNodeServer } from "@wraithlight/core.server";
+
+LoggerService.initialize({
+    applicationName: ApplicationName.RemoteConfig
+});
 
 const sharedCfg = SharedRemoteConfigConfigReader
     .getInstance(CoreEnvironment.getEnvironmentType())
 ;
 
 const CONTROLLERS: ReadonlyArray<BaseController> = [
+    new HealthCheckControllerV1(
+        ApplicationName.RemoteConfig,
+        "1.0.0" // TODO: package.json reader
+    )
 ];
 
 createNodeServer(
