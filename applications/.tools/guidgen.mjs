2a3a81102dd71f395685a6623ba46aa88c06f24b
kfarkasHU <sohamar@farkask.eu>
2024-09-06 22:02:22 +0200

    refactor(.tools): use logger
    Ref: #861

--- applications/.tools/guidgen.mjs
+++ applications/.tools/guidgen.mjs
@@ -1,4 +1,16 @@
 import types from "@wraithlight/core.guid";
+import logger from "@wraithlight/common.logger.sdk";
+
+const { LoggerService } = logger;
+
+LoggerService.initialize({
+    applicationName: "guidgen",
+    enabledLogSeverities: [
+        "INF" // TODO: since its a const enum it cannot be imported in mjs files
+    ]
+});
+
+const loggerService = LoggerService.getInstance();
 
 const numberOfGuidsStr = process.argv[2];
 const numberOfGuids = Number(numberOfGuidsStr) || 1;
@@ -16,7 +28,7 @@ function generateAndPrintGuid() {
     ;
     const guidLine = `# ${guid} #`
     
-    console.log(`
+    loggerService.info(`
         ${banner}
         #${line}#
         ${guidLine}
