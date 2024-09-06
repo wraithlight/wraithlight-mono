390aab6eadc7ef613ed3abc6211219e6f9481b88
kfarkasHU <sohamar@farkask.eu>
2024-09-06 21:29:42 +0200

    feat(*): add HC and init logger
    Ref: #13

--- applications/apps/website/node/src/server.ts
+++ applications/apps/website/node/src/server.ts
@@ -3,11 +3,16 @@ import { join } from "path";
 import { ServerAccountControllerV1, ServerAuthControllerV1 } from "@wraithlight/common.auth-sdk.server";
 import { ServerWebsiteConfigReader } from "@wraithlight/common.environment-static.server";
 import { SharedWebsiteConfigReader } from "@wraithlight/common.environment-static.shared";
+import { HealthCheckControllerV1 } from "@wraithlight/common.health-checker.sdk-server";
+import { LoggerService } from "@wraithlight/common.logger.sdk";
 import { ApplicationName } from "@wraithlight/core.auth.constant";
 import { LoginScope } from "@wraithlight/core.auth.types";
 import { CoreEnvironment } from "@wraithlight/core.env.sdk";
 import { createNodeServer } from "@wraithlight/core.server";
 
+LoggerService.initialize({
+    applicationName: ApplicationName.Website
+});
 
 const serverCfg = ServerWebsiteConfigReader
     .getInstance(CoreEnvironment.getEnvironmentType())
@@ -19,7 +24,11 @@ const sharedCfg = SharedWebsiteConfigReader
 
 const CONTROLLERS = [
     new ServerAuthControllerV1(LoginScope.Website),
-    new ServerAccountControllerV1()
+    new ServerAccountControllerV1(),
+    new HealthCheckControllerV1(
+        ApplicationName.Website,
+        "1.0.0" // TODO package.json reader
+    )
 ];
 
 const frontendPath = serverCfg.getCommon(x => x.files.frontend.static);
