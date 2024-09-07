733534c70193615efbd4469607b6131387f40fe2
kfarkasHU <sohamar@farkask.eu>
2024-09-07 13:33:30 +0200

    feat(enum): add new languages
    Ref: #876

--- applications/packages/content/core.content.types/src/enum/language.enum.ts
+++ applications/packages/content/core.content.types/src/enum/language.enum.ts
@@ -4,4 +4,8 @@ export enum Language {
     German = "014f9988-ff88-44c6-8e60-fbc52ea0e93d",
     Latin = "7655c0c5-c14b-4ebe-bf4a-d67efa2fe4b1",
     Croatian = "61022a82-027d-4cee-af6a-10e0116d55ad",
+    Slovenian = "affdccf2-8794-404e-8f30-3b6880b17aa6",
+    Serbian = "94a7aab7-c7df-38d0-9d87-279f6a5c0ac5",
+    Bosnian = "3c392ece-4dd8-21ba-8b1b-e16a347aba71",
+    Russian = "e3f4f29d-e228-9de7-a46f-ec853c831542",
 }
