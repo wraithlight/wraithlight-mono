30f0612735eadd4099c9c435a03eb7db4d4247d9
kfarkasHU <sohamar@farkask.eu>
2024-09-07 14:24:50 +0200

    feat(*): add character model
    Ref: #858

---
+++ applications/packages/game/core.game.types/src/enum/character/index.ts
@@ -0,0 +1,2 @@
+export * from "./character-headline-color.enum";
+export * from "./character-title-position.enum";
---
+++ applications/packages/game/core.game.types/src/enum/color.enum.ts
@@ -0,0 +1,12 @@
+export const enum Color {
+    /**
+     * In memoriam G - never forget!
+     * https://github.com/wraithlight/wraithlight-mono/commit/09bfc23435b10cbbb53126c7ee1819a86e93f627#diff-08d5b4a14934000386b15b1f1ba6fe5f1146a8fa789bad162f8e18172e241342
+     */
+    MISSING_TEXTURE = "#FFFF8F",
+    CHARACTER_HEADLINE_SELF = "",
+    CHARACTER_HEADLINE_ALLY = "",
+    CHARACTER_HEADLINE_NEUTRAL = "",
+    CHARACTER_HEADLINE_ENEMY = "",
+    CHARACTER_HEADLINE_UNALIVE = "",
+}
