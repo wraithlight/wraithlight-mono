d9c3c1a481bce959903ac9d5bbbf5aa0da5ba828
kfarkasHU <sohamar@farkask.eu>
2024-09-08 13:53:00 +0200

    chore(*): remove packages from pinfo
    Ref: #879

--- applications/packages/tools/tools.package-json-copy.base/src/package-json-copy.js
+++ applications/packages/tools/tools.package-json-copy.base/src/package-json-copy.js
@@ -1,6 +1,17 @@
-const { copyFileSync } = require("fs");
+const {
+  copyFileSync,
+  readFileSync,
+  writeFileSync
+} = require("fs");
 
 module.exports = (srcLocation, dstLocation) => {
   console.log(`Copy from '${srcLocation}' to '${dstLocation}'`);
   copyFileSync(srcLocation, dstLocation);
+  const content = readFileSync(dstLocation).toString();
+  const data = JSON.parse(content);
+  data.dependencies = {};
+  data.devDependencies = {};
+  data.peerDependencies = {};
+  const newContent = JSON.stringify(data, undefined, 2);
+  writeFileSync(dstLocation, newContent);
 }
