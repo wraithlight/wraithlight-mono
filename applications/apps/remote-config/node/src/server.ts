03b204438b14d8802bf1ff4dd74ac0c776ca1c0a
kfarkasHU <sohamar@farkask.eu>
2024-09-09 22:27:10 +0200

    feat(*): wire in packagejson reader
    Ref: #903

--- applications/apps/remote-config/node/src/server.ts
+++ applications/apps/remote-config/node/src/server.ts
@@ -1,6 +1,10 @@
+import { join } from "path";
+
+import { ServerRemoteConfigConfigReader } from "@wraithlight/common.environment-static.server";
 import { SharedRemoteConfigConfigReader } from "@wraithlight/common.environment-static.shared";
 import { HealthCheckControllerV1 } from "@wraithlight/common.health-checker.sdk-server";
 import { LoggerService } from "@wraithlight/common.logger.sdk";
+import { PackageJsonReader } from "@wraithlight/common.package-info.sdk-server";
 import { ApplicationName } from "@wraithlight/core.auth.constant";
 import { CoreEnvironment } from "@wraithlight/core.env.sdk";
 import { BaseController } from "@wraithlight/core.node";
@@ -10,14 +14,25 @@ LoggerService.initialize({
     applicationName: ApplicationName.RemoteConfig
 });
 
+const serverCfg = ServerRemoteConfigConfigReader
+  .getInstance(CoreEnvironment.getEnvironmentType())
+;
+
 const sharedCfg = SharedRemoteConfigConfigReader
     .getInstance(CoreEnvironment.getEnvironmentType())
 ;
 
+const packageInfoReader = new PackageJsonReader(
+  join(__dirname, serverCfg.getCommon(m => m.files.packageJson.path)),
+  LoggerService.getInstance(),
+  ApplicationName.RemoteConfig,
+  "0.0.1"   // TODO: From domain constants.
+);
+
 const CONTROLLERS: ReadonlyArray<BaseController> = [
     new HealthCheckControllerV1(
         ApplicationName.RemoteConfig,
-        "1.0.0" // TODO: package.json reader
+        packageInfoReader.getPackageJsonInfo().version
     )
 ];
 
