3096812d7714fc3f2d5219ce12a73e4cf153b07c
kfarkasHU <sohamar@farkask.eu>
2024-09-09 22:26:58 +0200

    feat(*): wire in packagejson reader
    Ref: #903

--- applications/apps/editor/node/src/server.ts
+++ applications/apps/editor/node/src/server.ts
@@ -1,7 +1,11 @@
+import { join } from "path";
+
 import { ServerAuthControllerV1 } from "@wraithlight/common.auth-sdk.server";
+import { ServerEditorConfigReader } from "@wraithlight/common.environment-static.server";
 import { SharedEditorConfigReader } from "@wraithlight/common.environment-static.shared";
 import { HealthCheckControllerV1 } from "@wraithlight/common.health-checker.sdk-server";
 import { LoggerService } from "@wraithlight/common.logger.sdk";
+import { PackageJsonReader } from "@wraithlight/common.package-info.sdk-server";
 import { ApplicationName } from "@wraithlight/core.auth.constant";
 import { LoginScope } from "@wraithlight/core.auth.types";
 import { CoreEnvironment } from "@wraithlight/core.env.sdk";
@@ -11,15 +15,26 @@ LoggerService.initialize({
     applicationName: ApplicationName.Editor
 });
 
+const serverCfg = ServerEditorConfigReader
+    .getInstance(CoreEnvironment.getEnvironmentType())
+;
+
 const sharedCfg = SharedEditorConfigReader
     .getInstance(CoreEnvironment.getEnvironmentType())
 ;
+
+const packageInfoReader = new PackageJsonReader(
+  join(__dirname, serverCfg.getCommon(m => m.files.packageJson.path)),
+  LoggerService.getInstance(),
+  ApplicationName.Editor,
+  "0.0.1"   // TODO: From domain constants.
+);
 
 const CONTROLLERS = [
     new ServerAuthControllerV1(LoginScope.Editor),
     new HealthCheckControllerV1(
         ApplicationName.Editor,
-        "1.0.0" // TODO: Package JSON reader
+        packageInfoReader.getPackageJsonInfo().version
     )
 ];
 
