f8ad375a57a11b949f55198e6db523b6e9641139
kfarkasHU <sohamar@farkask.eu>
2024-09-09 22:27:04 +0200

    feat(*): wire in packagejson reader
    Ref: #903

--- applications/apps/logs/node/src/server.ts
+++ applications/apps/logs/node/src/server.ts
@@ -1,6 +1,10 @@
+import { join } from "path";
+
+import { ServerLogsConfigReader } from "@wraithlight/common.environment-static.server";
 import { SharedLogsConfigReader } from "@wraithlight/common.environment-static.shared";
 import { HealthCheckControllerV1 } from "@wraithlight/common.health-checker.sdk-server";
 import { LoggerService } from "@wraithlight/common.logger.sdk";
+import { PackageJsonReader } from "@wraithlight/common.package-info.sdk-server";
 import { ApplicationName } from "@wraithlight/core.auth.constant";
 import { CoreEnvironment } from "@wraithlight/core.env.sdk";
 import { createNodeServer } from "@wraithlight/core.server";
@@ -11,15 +15,26 @@ LoggerService.initialize({
     applicationName: ApplicationName.Logs
 });
 
+const serverCfg = ServerLogsConfigReader
+  .getInstance(CoreEnvironment.getEnvironmentType())
+;
+
 const sharedCfg = SharedLogsConfigReader
     .getInstance(CoreEnvironment.getEnvironmentType())
 ;
+
+const packageInfoReader = new PackageJsonReader(
+  join(__dirname, serverCfg.getCommon(m => m.files.packageJson.path)),
+  LoggerService.getInstance(),
+  ApplicationName.Editor,
+  "0.0.1"   // TODO: From domain constants.
+);
 
 const CONTROLLERS = [
     new LogsEntryController(),
     new HealthCheckControllerV1(
         ApplicationName.Logs,
-        "1.0.0" // TODO: Package JSON reader
+        packageInfoReader.getPackageJsonInfo().version
     )
 ];
 
