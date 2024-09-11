4681cf8c3b5ba20113850c43cd3876932749e350
kfarkasHU <sohamar@farkask.eu>
2024-09-11 18:58:29 +0200

    refactor(tables): add `LanguageCode` column
    Ref: #892

--- database/content/src/tables/language.sql
+++ database/content/src/tables/language.sql
@@ -3,5 +3,6 @@ CREATE TABLE `Language` (
     `DisplayName` VARCHAR(64) NOT NULL,
     `IsoCode` VARCHAR(5) NOT NULL,
     `IsActive` BIT NOT NULL,
+    `LanguageCode` VARCHAR(36) NOT NULL,
     PRIMARY KEY (`Id`)
 ) ENGINE = MyISAM DEFAULT CHARSET=utf8;
