b3d3672c33f4ccb5d17f5a4bb6eee4a816e5f7bb
kfarkasHU <sohamar@farkask.eu>
2024-09-11 19:40:30 +0200

    feat(*): extend mocks with `isEmptyString`
    Ref: #889

--- applications/packages/core/core.nullable/__mocks__/nullable/nullable.fns.ts
+++ applications/packages/core/core.nullable/__mocks__/nullable/nullable.fns.ts
@@ -2,3 +2,8 @@ export const isNil = jest
     .fn()
     .mockImplementation((m) => m === undefined || m === null)
 ;
+
+export const isEmptyString = jest
+  .fn()
+  .mockImplementation(m => m === "")
+;
