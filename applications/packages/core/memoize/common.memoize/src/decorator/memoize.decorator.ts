b797889c7bb8fcf41e1673dc83b97fdb24148d57
kfarkasHU <sohamar@farkask.eu>
2024-09-13 23:51:13 +0200

    feat(*): create memoize package
    Ref: #832

---
+++ applications/packages/core/memoize/common.memoize/src/decorator/memoize.decorator.ts
@@ -0,0 +1,41 @@
+import { toBase64String } from "@wraithlight/core.base64";
+import { Dictionary } from "@wraithlight/core.dictionary";
+
+import { MemoizeDictionaryFactory } from "../dictionary";
+
+const memoizeDictionary = MemoizeDictionaryFactory.getDictionary();
+
+export const MemoizeDecorator = (token: string): MethodDecorator => (
+  _target: object,
+  _propertyKey: string | symbol,
+  descriptor: PropertyDescriptor
+): void => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const originalMethod = descriptor.value;
+
+  if (!memoizeDictionary.has(token)) {
+    memoizeDictionary.set(token, new Dictionary<unknown>());
+  }
+  const tokenDictionary = memoizeDictionary.get(token);
+
+  const lifted = (...args: Array<unknown>): unknown => {
+    const argsHash = args.map(m => JSON.stringify(m)).join("");
+    const argsB64 = toBase64String(argsHash);
+
+    if (!argsB64.isSuccess) {
+      return originalMethod(...args);
+    }
+
+    if (tokenDictionary.has(argsB64.payload)) {
+      return tokenDictionary.get(argsB64.payload);
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const result = originalMethod(...args);
+    tokenDictionary.set(argsB64.payload, result);
+    return tokenDictionary.get(argsB64.payload);
+  };
+
+  descriptor.value = lifted;
+
+};
---
+++ applications/packages/core/memoize/common.memoize/src/service/memoize.service.spec.ts
@@ -0,0 +1,52 @@
+const hasSpy = jest.fn();
+const getSpy = jest.fn();
+const resetSpy = jest.fn();
+
+jest.mock("@wraithlight/core.dictionary", () => {
+  return {
+    Dictionary: jest.fn().mockImplementation(() => {
+      return {
+        has: hasSpy.mockImplementation(() => true),
+        get: getSpy.mockImplementation(() => {
+          return {
+            reset: resetSpy
+          }
+        }),
+      }
+    })
+  }
+});
+
+import { MemoizeService } from "./memoize.service";
+
+describe("MemoizeServiceSpecs", () => {
+
+  const MOCK_TOKEN = "iamatoken";
+  let service: MemoizeService;
+
+  describe("given the service is initialized", () => {
+
+    service = new MemoizeService();
+
+    describe("when i call `reset`", () => {
+      beforeAll(() => {
+        service.reset(MOCK_TOKEN);
+      });
+      it("should check if the given key exists", () => {
+        expect(hasSpy).toHaveBeenCalled();
+        expect(hasSpy).toHaveBeenCalledTimes(1);
+        expect(hasSpy).toHaveBeenCalledWith(MOCK_TOKEN);
+      });
+      it("should read the given key", () => {
+        expect(getSpy).toHaveBeenCalled();
+        expect(getSpy).toHaveBeenCalledTimes(1);
+        expect(getSpy).toHaveBeenCalledWith(MOCK_TOKEN);
+      });
+      it("should call the underlying dictionary", () => {
+        expect(resetSpy).toHaveBeenCalled();
+        expect(resetSpy).toHaveBeenCalledTimes(1);
+        expect(resetSpy).toHaveBeenCalledWith();
+      });
+    });
+  });
+});
