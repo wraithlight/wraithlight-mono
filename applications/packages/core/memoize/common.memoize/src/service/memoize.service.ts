436af2e4ba3945e765fc8039548ebbf1c3f04cac
kfarkasHU <sohamar@farkask.eu>
2024-09-14 00:01:48 +0200

    improvement(service): make it singleton
    Ref: #832

--- applications/packages/core/memoize/common.memoize/src/service/memoize.service.ts
+++ applications/packages/core/memoize/common.memoize/src/service/memoize.service.ts
@@ -2,7 +2,15 @@ import { MemoizeDictionaryFactory } from "../dictionary";
 
 export class MemoizeService {
 
+  private static readonly _instance = new MemoizeService();
   private readonly _dictionary = MemoizeDictionaryFactory.getDictionary();
+
+  private constructor() {
+  }
+
+  public static getInstance(): MemoizeService {
+    return this._instance;
+  }
 
   public reset(token: string): boolean {
     if (!this._dictionary.has(token)) {
