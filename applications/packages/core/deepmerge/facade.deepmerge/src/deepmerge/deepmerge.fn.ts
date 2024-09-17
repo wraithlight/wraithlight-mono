99b4b576e7b637c5be5d8180dc346d5f0f28dcb2
kfarkasHU <sohamar@farkask.eu>
2024-09-17 19:38:42 +0200

    refactor(*): remove `core` level `deepmerge`
    Ref: #918

--- applications/packages/core/deepmerge/facade.deepmerge/src/deepmerge/deepmerge.fn.ts
+++
@@ -1,7 +0,0 @@
-import { deepmerge } from "deepmerge-ts";
-
-export function deepmergeFacade<T>(...objects: Array<T>): T {
-    const result = deepmerge(...objects);
-    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-    return result as T;
-}
