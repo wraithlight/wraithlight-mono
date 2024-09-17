7ca9c9edd752e0852dfad5f7cf9ff706b0bde3b4
kfarkasHU <sohamar@farkask.eu>
2024-09-17 19:38:02 +0200

    refactor(*): move `deepmerge` to fw level
    Ref: #918

---
+++ applications/packages/framework/deepmerge/framework.deepmerge/src/deepmerge/deepmerge.fn.spec.ts
@@ -0,0 +1,28 @@
+import { deepmergeFacade } from "@wraithlight/facade.deepmerge";
+
+import { wlDeepmerge } from "./deepmerge.fn"
+
+jest.mock("@wraithlight/facade.deepmerge", () => {
+    return {
+        deepmergeFacade: jest.fn().mockImplementation(() => ({}))
+    }
+})
+
+describe("wlDeepmergeSpecs", () => {
+    const MOCK_TARGET = {};
+    const MOCK_ADDITIONAL = {};
+
+    describe("given the utility is initialized", () => {
+        describe("when i call it", () => {
+            beforeEach(() => {
+                wlDeepmerge(MOCK_TARGET, MOCK_ADDITIONAL)
+            })
+            it("should call the underlying method", () => {
+                expect(deepmergeFacade).toHaveBeenCalled();
+                expect(deepmergeFacade).toHaveBeenCalledTimes(1);
+                expect(deepmergeFacade)
+                    .toHaveBeenCalledWith(MOCK_TARGET, MOCK_ADDITIONAL);
+            })
+        })
+    })
+})
