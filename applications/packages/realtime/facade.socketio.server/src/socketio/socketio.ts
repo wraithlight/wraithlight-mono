e309ec153ab4fc53e8952434abe3986f59a3bb69
kfarkasHU <sohamar@farkask.eu>
2024-09-28 17:58:18 +0200

    refactor(socketio): object construction
    Ref: #946

--- applications/packages/realtime/facade.socketio.server/src/socketio/socketio.ts
+++ applications/packages/realtime/facade.socketio.server/src/socketio/socketio.ts
@@ -5,77 +5,78 @@ import { Server } from "socket.io";
 import { EVT_CONNECTION, EVT_DISCONNECT } from "./socketio.const";
 import { Socket, SocketGuard } from "./socketio.model";
 import {
-    OnConnectCallback,
-    OnDisconnectCallback,
-    OnEventCallback
+  OnConnectCallback,
+  OnDisconnectCallback,
+  OnEventCallback
 } from "./socketio.type";
 
 export class SocketIOFacade {
 
-    private readonly _server = new Server(
-        this._app,
-        {
-            path: this._path
-        },
+  private readonly _server: Server;
+
+  constructor(
+    readonly app: HTTPServer,
+    readonly path: string,
+    private readonly _connectCallback: OnConnectCallback,
+    private readonly _disconnectCallback: OnDisconnectCallback,
+    private readonly _onMessageCallback: OnEventCallback,
+    guards?: ReadonlyArray<SocketGuard>
+  ) {
+    this._server = new Server(
+      app,
+      {
+        path: path
+      },
     );
-
-    constructor(
-        private readonly _app: HTTPServer,
-        private readonly _path: string,
-        private readonly _connectCallback: OnConnectCallback,
-        private readonly _disconnectCallback: OnDisconnectCallback,
-        private readonly _onMessageCallback: OnEventCallback,
-        guards?: ReadonlyArray<SocketGuard>
-    ) {
-        guards?.forEach(m => this._server.use(
-            (socket, next) => m(
-                socket.request,
-                next
-            )
-        )
+    guards?.forEach(m => this._server.use(
+      (socket, next) => m(
+        socket.request,
+        next
+      )
+    )
+    );
+    this._server.on(
+      EVT_CONNECTION,
+      (socket: Socket) => {
+        socket.onAny((m, o) => this._onMessageCallback(
+          m,
+          socket.id,
+          o
+        ));
+        this._connectCallback(socket.id);
+        socket.on(
+          EVT_DISCONNECT,
+          () => {
+            this._disconnectCallback(socket.id);
+          }
         );
-        this._server.on(
-            EVT_CONNECTION,
-            (socket: Socket) => {
-                socket.onAny((m, o) => this._onMessageCallback(
-                    m,
-                    socket.id,
-                    o
-                ));
-            this._connectCallback(socket.id);
-            socket.on(
-                EVT_DISCONNECT,
-                () => {
-                    this._disconnectCallback(socket.id);
-                }
-            );
-        });
-    }
+      });
+  }
 
-    public broadcastToAll(
-        topic: string,
-        message: string
-    ): void {
-        this._server.emit(
-            topic,
-            message
-        );
-    }
+  public broadcastToAll(
+    topic: string,
+    message: string
+  ): void {
+    this._server.emit(
+      topic,
+      message
+    );
+  }
 
-    public send(
-        id: string,
-        topic: string,
-        message: string
-    ): void {
-        this._server.sockets.to(id).emit(
-            topic,
-            message
-        );
-    }
+  public send(
+    id: string,
+    topic: string,
+    message: string
+  ): void {
+    this._server.sockets.to(id).emit(
+      topic,
+      message
+    );
+  }
 
-    public close(): void {
-        this._server.removeAllListeners();
-        this._server.close();
-    }
+  public close(): void {
+    this._server.removeAllListeners();
+    this._server.close();
+  }
 
 }
