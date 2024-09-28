ecde2855b65dda795395a4ea37afd34406e8c560
kfarkasHU <sohamar@farkask.eu>
2024-09-28 13:19:07 +0200

    feat(session): export error codes
    Ref: #944

--- applications/packages/user-management/common.user-management.dal/src/service/session/session.service.ts
+++ applications/packages/user-management/common.user-management.dal/src/service/session/session.service.ts
@@ -1,10 +1,18 @@
 import { addHours, addMinutes, utcNow } from "@wraithlight/core.date";
 import { Guid, newGuid } from "@wraithlight/core.guid";
-import { Nullable } from "@wraithlight/core.nullable";
+import { isNil } from "@wraithlight/core.nullable";
+import {
+  OperationResult,
+  OperationResultFactory
+} from "@wraithlight/framework.operation-result";
 
 import { SessionDbo, UserManagementDbContextFactory } from "../../db-context";
 
-import { REFRESH_VALID_FOR_HOURS, SESSION_VALID_FOR_MINUTES } from "./session.const";
+import {
+  ERROR_CODES,
+  REFRESH_VALID_FOR_HOURS,
+  SESSION_VALID_FOR_MINUTES
+} from "./session.const";
 
 export class SessionService {
 
@@ -18,94 +26,116 @@ export class SessionService {
     applicationId: Guid,
     token: string,
     refreshToken: string,
-  ): Promise<Nullable<SessionDbo>> {
+  ): Promise<OperationResult<SessionDbo>> {
     const id = newGuid();
     const now = utcNow();
     const sessionValidUntil = addMinutes(now, SESSION_VALID_FOR_MINUTES);
     const refreshValidHours = addHours(now, REFRESH_VALID_FOR_HOURS);
-    await this._context.Sessions
-      .insert({
-        id: id,
-        userId: userId,
-        applicationId: applicationId,
-        token: token,
-        refreshToken: refreshToken,
-        tokenValidFromUtc: now,
-        tokenValidUntilUtc: sessionValidUntil,
-        refreshTokenValidFromUtc: now,
-        refreshTokenValidUntilUtc: refreshValidHours,
-        createdAtUtc: now,
-        createdByUserId: userId,
-        isDeleted: false
-      })
-      .run()
-    ;
-
-    return this._context.Sessions
-      .select()
-      .where("id", id)
-      .first()
-    ;
+    try {
+      await this._context.Sessions
+        .insert({
+          id: id,
+          userId: userId,
+          applicationId: applicationId,
+          token: token,
+          refreshToken: refreshToken,
+          tokenValidFromUtc: now,
+          tokenValidUntilUtc: sessionValidUntil,
+          refreshTokenValidFromUtc: now,
+          refreshTokenValidUntilUtc: refreshValidHours,
+          createdAtUtc: now,
+          createdByUserId: userId,
+          isDeleted: false
+        })
+        .run()
+        ;
+    } catch {
+      return OperationResultFactory.error(ERROR_CODES.CREATE);
+    }
+    return this.findByIdInternal(id);
   }
 
   public async deleteSessionById(
     id: Guid
-  ): Promise<Nullable<SessionDbo>> {
+  ): Promise<OperationResult<SessionDbo>> {
     await this._context.Sessions
       .update("id", id, { isDeleted: true })
       .run()
-    ;
+      ;
     return this.findByIdInternal(id);
   }
 
   public async findByToken(
     token: Guid
-  ): Promise<Nullable<SessionDbo>> {
+  ): Promise<OperationResult<SessionDbo>> {
     return this.findByTokenInternal(token);
   }
 
   public async renewToken(
     sessionToken: string
-  ): Promise<Nullable<SessionDbo>> {
+  ): Promise<OperationResult<SessionDbo>> {
     const session = await this.findByTokenInternal(sessionToken);
-    if (!session) {
-      return undefined;
+    if (!session.isSuccess) {
+      return session;
     }
     const now = utcNow();
     const sessionValidUntilUtc = addMinutes(now, SESSION_VALID_FOR_MINUTES);
     const refreshValidUntilUtc = addHours(now, REFRESH_VALID_FOR_HOURS);
-    this._context.Sessions
+
+    try {
+      await this._context.Sessions
       .update(
         "id",
-        session.id,
+        session.payload.id,
         {
           updatedAtUtc: now,
-          updatedByUserId: session.userId,
+          updatedByUserId: session.payload.userId,
           tokenValidUntilUtc: sessionValidUntilUtc,
           refreshTokenValidUntilUtc: refreshValidUntilUtc
         }
-      );
+      )
+      .run();
+    } catch {
+      return OperationResultFactory.error(ERROR_CODES.UPDATE_BY_ID);
+    }
+    return this.findByIdInternal(session.payload.id);
   }
 
   private async findByTokenInternal(
     token: Guid
-  ): Promise<Nullable<SessionDbo>> {
-    return this._context.Sessions
-      .select()
-      .where("token", token)
-      .where("isDeleted", false)
-      .first()
-    ;
+  ): Promise<OperationResult<SessionDbo>> {
+    try {
+      const result = await this._context.Sessions
+        .select()
+        .where("token", token)
+        .where("isDeleted", false)
+        .first()
+        ;
+      if (isNil(result)) {
+        return OperationResultFactory.error(ERROR_CODES.NOT_FOUND_BY_TOKEN);
+      }
+      return OperationResultFactory.success(result);
+    } catch {
+      return OperationResultFactory.error(ERROR_CODES.FIND_BY_TOKEN);
+    }
   }
 
   private async findByIdInternal(
     id: Guid
-  ): Promise<Nullable<SessionDbo>> {
-    return this._context.Sessions
-      .select()
-      .where("id", id)
-      .first()
-    ;
+  ): Promise<OperationResult<SessionDbo>> {
+    try {
+      const result = await this._context.Sessions
+        .select()
+        .where("id", id)
+        .first()
+        ;
+      if (isNil(result)) {
+        return OperationResultFactory.error(ERROR_CODES.NOT_FOUND_BY_ID);
+      }
+      return OperationResultFactory.success(result);
+    } catch {
+      return OperationResultFactory.error(ERROR_CODES.FIND_BY_ID);
+    }
   }
 
 }
