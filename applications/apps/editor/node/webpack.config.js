e8f11f96a2955b26a00d8d25b8b2ef7563e628da
kfarkasHU <sohamar@farkask.eu>
2024-09-28 17:21:53 +0200

    chore(*): build the app only
    Ref: #946

--- applications/apps/editor/node/webpack.config.js
+++ applications/apps/editor/node/webpack.config.js
@@ -4,7 +4,7 @@ const config = require("@wraithlight/tools.webpack.base").beConfig;
 module.exports = (env) => {
   const filename = "index.js";
   return {
-    ...config(env, __dirname),
+    ...config(env, __dirname, "tsconfig.build.json"),
     target: "node",
     output: {
       path: resolve(__dirname, "../dist/node"),
