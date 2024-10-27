797865c4b77bd34500573c6c282dd9a53688e199
kfarkasHU <sohamar@farkask.eu>
2024-10-27 11:26:22 +0100

    style(param): param decorator
    Ref: #983

--- applications/packages/core/core.node/src/crud/decorator/param/param.decorator.ts
+++ applications/packages/core/core.node/src/crud/decorator/param/param.decorator.ts
@@ -1,5 +1,6 @@
 import { IArgumentDecoratorFactory } from "@wraithlight/core.decorator.types";
 import { Predicate } from "@wraithlight/core.linq";
+import { isNil } from "@wraithlight/core.nullable";
 import { Request } from "express";
 
 import { BaseController } from "../../controller";
@@ -14,12 +15,25 @@ export const ParamDecorator = <T extends BaseController>(
   propertyKey: string,
   parameterIndex: number
 ) => {
-  target[PARAM_PROPERTY_KEY] = target[PARAM_PROPERTY_KEY] ?? {};
-  // eslint-disable-next-line max-len
-  target[PARAM_PROPERTY_KEY][propertyKey] = target[PARAM_PROPERTY_KEY][propertyKey] ?? { contextExecutor: contextExecutor, parameters: [] };
+  setPropertyContext(target, propertyKey, contextExecutor);
 
   // eslint-disable-next-line max-len, @typescript-eslint/no-unsafe-member-access
   const paramExecutor: Predicate<any, unknown> = (m => parameterName ? m[parameterName] : m);
-  // eslint-disable-next-line max-len
-  target[PARAM_PROPERTY_KEY][propertyKey].parameters[parameterIndex] = paramExecutor;
+  const paramContext = target[PARAM_PROPERTY_KEY] ?? {};
+  paramContext[propertyKey].parameters[parameterIndex] = paramExecutor;
 };
+
+function setPropertyContext<T extends BaseController>(
+  target: T,
+  propertyKey: string,
+  contextExecutor: Predicate<Request, any>
+): void {
+  if (isNil(target[PARAM_PROPERTY_KEY])) {
+    target[PARAM_PROPERTY_KEY] = {};
+  }
+
+  if (isNil(target[PARAM_PROPERTY_KEY][propertyKey])) {
+    const context = { contextExecutor: contextExecutor, parameters: [] };
+    target[PARAM_PROPERTY_KEY][propertyKey] = context;
+  }
+}
