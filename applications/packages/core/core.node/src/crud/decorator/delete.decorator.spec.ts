a287729a170af262e60d25d85c7c4f17a997e8aa
kfarkasHU <sohamar@farkask.eu>
2024-11-14 18:00:00 +0100

    test(decorator): delete decorator spec
    Ref: #1001

---
+++ applications/packages/core/core.node/src/crud/decorator/delete.decorator.spec.ts
@@ -0,0 +1,28 @@
+jest.mock("./http.decorator", () => {
+    return {
+        HttpDecorator: jest.fn()
+    }
+})
+
+import { HttpDecorator } from "./http.decorator";
+import { HttpDelete } from "./delete.decorator";
+
+describe("HttpDeleteSpecs", () => {
+
+    const MOCK_PATH = "wraithlight/test";
+    const decorator = HttpDelete;
+
+    describe("given the decorator is initalized", () => {
+
+        const _result = decorator(MOCK_PATH);
+
+        it("should call the underlying HttpDecorator function", () => {
+            expect(HttpDecorator).toHaveBeenCalled();
+            expect(HttpDecorator).toHaveBeenCalledTimes(1);
+            expect(HttpDecorator).toHaveBeenCalledWith("DELETE", MOCK_PATH);
+        });
+
+
+    });
+
+});
