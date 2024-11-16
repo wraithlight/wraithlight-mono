dcea5bf334a1650c10e2b401817c75a272311ec1
kfarkasHU <sohamar@farkask.eu>
2024-11-16 22:08:44 +0100

    feat(*): domain http constants lib
    Ref: #998

---
+++ applications/packages/domain/domain.http.constants/src/header-name/header-name.const.ts
@@ -0,0 +1,8 @@
+export namespace HeaderName {
+  export const SessionToken = "X-WL-SESSION-TOKEN";
+  export const ApiToken = "X-WL-API-TOKEN";
+  export const IsBot = "X-WL-IS-BOT";
+  export const IsSsrEnabled = "X-WL-SSR-ENABLED";
+  export const CorrelationId = "X-WL-CORRELATION-ID";
+  export const RealtimeToken = "X-WL-RT-TOKEN";
+};
