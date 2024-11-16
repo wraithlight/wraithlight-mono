72d3dcc76834a803e9586b54cae6e84526391680
kfarkasHU <sohamar@farkask.eu>
2024-11-16 22:02:53 +0100

    feat(param): add `FromBodyV2()`
    Ref: #1016

--- applications/packages/core/core.node/src/crud/decorator/param/from-body.decorator.ts
+++ applications/packages/core/core.node/src/crud/decorator/param/from-body.decorator.ts
@@ -7,6 +7,9 @@ import { BaseController } from "../../controller";
 
 import { ParamDecorator } from "./param.decorator";
 
+/**
+ * @deprecated Use `FromBodyV2()` instead.
+ */
 export const FromBody = <T extends BaseController>(
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   propertyName?: string | Predicate<any, any>
@@ -23,4 +26,17 @@ export const FromBody = <T extends BaseController>(
   return ParamDecorator<T>(
     propertyName((m: Request) => m.body)
   );
+};
+
+export const FromBodyV2 = <TBody>(
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  selector?: Predicate<TBody, any>
+): IArgumentDecoratorFactory<BaseController> => {
+  if (isNil(selector)) {
+    return ParamDecorator(m => m.body);
+  }
+  const extractor = (m: Request) => {
+    return selector(m.body);
+  };
+  return ParamDecorator(extractor);
 };
