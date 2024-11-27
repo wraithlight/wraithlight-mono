778f1176c3e9c4b1a6f6de47ad61fb76c9b10480
kfarkasHU <sohamar@farkask.eu>
2024-11-28 00:19:39 +0100

    feat(const): add injection name
    Ref: #509

--- applications/packages/core/core.node.evo/src/model/base-controller.model.ts
+++ applications/packages/core/core.node.evo/src/model/base-controller.model.ts
@@ -1,6 +1,7 @@
-import { CONTROLLER_METADATA_KEY } from "../const";
+import { CONTROLLER_METADATA_KEY, INJECTION_NAME } from "../const";
 import { ControllerHttpMetadata } from "./controller-http-metadata.model";
 
 export interface IBaseController {
-  [CONTROLLER_METADATA_KEY]: ControllerHttpMetadata
+  [CONTROLLER_METADATA_KEY]: ControllerHttpMetadata;
+  [INJECTION_NAME]: string;
 }
