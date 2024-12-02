92b3d44fb7f2c1c1a218b295396edd36735a7f2b
kfarkasHU <sohamar@farkask.eu>
2024-12-02 15:43:03 +0100

    feat(request handler): add `RequestHandler`
    Ref: #509

---
+++ applications/packages/core/core.node.evo/src/request-handler/request-handler.ts
@@ -0,0 +1,195 @@
+import { Application, Request, Response } from "express";
+import { HttpCode, HttpVerb } from "@wraithlight/core.http";
+import { Guid, newGuid } from "@wraithlight/core.guid";
+import { Timer } from "@wraithlight/framework.timer";
+
+import { isBaseControllerResult } from "../base";
+import { Injector } from "../injector";
+
+import {
+  HandleControllerModel,
+  HandlerControllerEndpointFilterModel
+} from "./request-handler.model";
+import { EventBus } from "../events";
+
+// TODO: Wire in `EventBus`
+// TODO: Wire in filters properly
+// TODO: Wire in param decoratorss
+// TODO: Add proper headers (isbot, correlationid)
+export class RequestHandler {
+
+  private static controllers: Array<HandleControllerModel> = [];
+
+  public static addBluepirnt(
+    context: HandleControllerModel
+  ): void {
+    this.controllers.push(context);
+  }
+
+  public static defineBlueprints(
+    application: Application
+  ): void {
+    for(const controller of this.controllers) {
+      for(const endpoint of controller.endpoints) {
+        const handler = this.getHandler(application, endpoint.verb);
+
+        handler(endpoint.fullPath, async (
+          req: Request,
+          res: Response
+        ) => {
+          const correlation = newGuid();
+          const timer = new Timer();
+          timer.start();
+          EventBus.emitRequestStart(correlation);
+          const filterResult = await this.processFilters(
+            correlation,
+            endpoint.filters,
+            req
+          );
+
+          if (filterResult) {
+            const end = timer.stop();
+            EventBus.emitRequestEnd(
+              correlation,
+              end
+            )
+            this.processErrorResponse(
+              res,
+              correlation,
+              filterResult.errorCode,
+              filterResult.message
+            );
+            return;
+          }
+
+          const controllerInstance = Injector.getInstance<any>(controller.injectionToken);
+
+          const method = controllerInstance[endpoint.methodName];
+
+          try {
+            const methodResult = await method();
+
+            if (isBaseControllerResult(methodResult)) {
+              this.processSuccessResponse(
+                res,
+                correlation,
+                methodResult.code,
+                methodResult.payload
+              );
+            } else {
+              this.processSuccessResponse(
+                res,
+                correlation,
+                HttpCode.Ok,
+                methodResult
+              );
+            }
+
+          } catch {
+            // TODO: Error handling here.
+            this.processErrorResponse(
+              res,
+              correlation,
+              HttpCode.InternalError,
+              "E_METHOD"
+            )
+          }
+        });
+      }
+    }
+  }
+
+  private static async processFilters(
+    correlation: Guid,
+    filters: Array<HandlerControllerEndpointFilterModel>,
+    req: Request
+  ): Promise<{ errorCode: HttpCode, message: string } | undefined> {
+    for (const filter of filters) {
+      try {
+        const result = await filter.guardFn(req);
+        if (result.isError) {
+          EventBus.emitFilterFail(
+            correlation,
+            result.errorMessage,
+            result.errorHttpCode,
+          );
+          return {
+            errorCode: result.errorHttpCode,
+            message: result.errorMessage
+          }
+        }
+      } catch {
+        EventBus.emitFilterFatal(
+          correlation
+        );
+        return {
+          errorCode: HttpCode.InternalError,
+          message: "E_FILTER"
+        }
+      }
+    }
+  }
+
+  private static processErrorResponse(
+    res: Response,
+    correlationId: Guid,
+    errorCode: HttpCode,
+    error: string
+  ): void {
+    const response = {
+      correlationId: correlationId,
+      payload: undefined,
+      error: {
+        code: error
+      }
+    };
+
+    this.processResponse(
+      res,
+      errorCode,
+      response
+    );
+  }
+
+  private static processSuccessResponse<T>(
+    res: Response,
+    correlationId: Guid,
+    statusCode: HttpCode,
+    payload: T
+  ): void {
+    const response = {
+      correlationId: correlationId,
+      payload: payload,
+      error: {
+        code: ""
+      }
+    };
+
+    this.processResponse(
+      res,
+      statusCode,
+      response
+    );
+  }
+
+  private static processResponse<T>(
+    res: Response,
+    code: HttpCode,
+    response: T
+  ): void {
+    res.status(code).send(response);
+  }
+
+  private static getHandler(
+    application: Application,
+    verb: HttpVerb
+  ) {
+    switch(verb) {
+      case HttpVerb.GET: return application.get.bind(application);
+      case HttpVerb.DELETE: return application.delete.bind(application);
+      case HttpVerb.PUT: return application.put.bind(application);
+      case HttpVerb.POST: return application.post.bind(application);
+    }
+  }
+
+}
