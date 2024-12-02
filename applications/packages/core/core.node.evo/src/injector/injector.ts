85faedf5edef1e7f5f71c0ec8876627d875da373
kfarkasHU <sohamar@farkask.eu>
2024-12-02 15:22:30 +0100

    feat(injector): add `Injector`
    Ref: #509

---
+++ applications/packages/core/core.node.evo/src/injector/injector.ts
@@ -0,0 +1,30 @@
+export class Injector {
+
+  private static cache = new Map<string, Function>();
+
+  public static registerSingleton<TController>(
+    token: string,
+    factory: () => TController
+  ): void {
+    const instance = factory();
+    this.cache.set(token, () => instance);
+  }
+
+  public static registerMultiton<TController>(
+    token: string,
+    factory: () => TController
+  ): void {
+    this.cache.set(token, factory);
+  }
+
+  public static getInstance<TController>(
+    token: string
+  ): TController {
+    if (!this.cache.has(token)) {
+      throw `No registered object was found for token '${token}'!`;
+    }
+    const factory = this.cache.get(token)!;
+    const instance = factory();
+    return instance;
+  }
+}
