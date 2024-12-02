8f32287f1b6bc5137c55d146a9e40209473e735f
kfarkasHU <sohamar@farkask.eu>
2024-12-02 16:01:45 +0100

    feat(*): add server and main entry
    Ref: #509

---
+++ applications/packages/core/core.node.evo/src/index.ts
@@ -0,0 +1,5 @@
+export * from "./base";
+export * from "./enum";
+export * from "./events";
+export * from "./decorator";
+export * from "./server";
---
+++ applications/packages/core/core.node.evo/src/server/server.const.ts
@@ -0,0 +1,2 @@
+export const DEFAULT_PAYLOAD_SIZE = 1;
+export const DEFAULT_USE_CORS = false;
