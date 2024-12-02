d97769863e3dba5753fbf590b3ba83e0708d2895
kfarkasHU <sohamar@farkask.eu>
2024-12-02 15:56:50 +0100

    feat(controller): add `HttpController` decorator
    Ref: #509

---
+++ applications/packages/core/core.node.evo/src/decorator/controller/controller.decorator.ts
@@ -0,0 +1,22 @@
+import { Creatable } from "@wraithlight/framework.creatable";
+
+import { BaseController } from "../../base";
+import { InjectionScope } from "../../enum";
+import { HandlerContext } from "../../handler-context";
+import { RequestHandler } from "../../request-handler";
+
+export const HttpController = <T extends Creatable<BaseController>>(
+  path: string = "",
+  injectionScope = InjectionScope.Multiton
+) => (
+  target: T
+) => {
+  HandlerContext.addClass(
+    path,
+    injectionScope,
+    target
+  );
+  const controllerContext = HandlerContext.compile();
+  RequestHandler.addBluepirnt(controllerContext);
+  HandlerContext.reset();
+}
