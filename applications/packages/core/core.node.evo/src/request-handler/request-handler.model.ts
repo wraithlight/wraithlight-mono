7fd94ef9457229937fbe19484210ddf492db64eb
kfarkasHU <sohamar@farkask.eu>
2024-12-02 15:43:03 +0100

    feat(request handler): add `RequestHandler`
    Ref: #509

---
+++ applications/packages/core/core.node.evo/src/request-handler/request-handler.model.ts
@@ -0,0 +1,27 @@
+import { Request } from "express";
+import { HttpVerb } from "@wraithlight/core.http";
+
+import { FilterResult } from "../model";
+
+export interface HandleControllerModel {
+  injectionToken: string;
+  endpoints: Array<HandlerControllerEndpointModel>;
+}
+
+export interface HandlerControllerEndpointModel {
+  verb: HttpVerb;
+  fullPath: string;
+  methodName: string;
+  params: Array<HandlerControllerParamModel>;
+  filters: Array<HandlerControllerEndpointFilterModel>;
+}
+
+export interface HandlerControllerEndpointFilterModel {
+  guardFn: (req: Request) => Promise<FilterResult> | FilterResult;
+}
+
+export interface HandlerControllerParamModel {
+  methodName: string;
+  propertyIndex: number;
+  extractorFn: <T>(req: Request) => T;
+}
