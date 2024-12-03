26e0caf93133c7a515a371257cc043e163aee3f4
kfarkasHU <sohamar@farkask.eu>
2024-12-03 06:48:38 +0100

    style(base): add missing semicolons
    Ref: #509

--- applications/packages/core/core.node.evo/src/base/base.controller.ts
+++ applications/packages/core/core.node.evo/src/base/base.controller.ts
@@ -8,13 +8,13 @@ export abstract class BaseController {
       code: HttpCode.Ok,
       payload: payload,
       __brand: "BaseControllerResult<T>"
-    }
+    };
   }
 
   protected accepted(): BaseControllerResult<undefined> {
     return {
       code: HttpCode.Accepted,
       __brand: "BaseControllerResult<T>"
-    }
+    };
   }
 }
