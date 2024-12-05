3eb77d0f4f7e6bca0217ab99a5679b6c269f8a41
kfarkasHU <sohamar@farkask.eu>
2024-12-05 18:00:00 +0100

    style(creatable): ignore eslint rules
    Ref: #509

--- applications/packages/framework/framework.creatable/src/creatable/creatable.type.ts
+++ applications/packages/framework/framework.creatable/src/creatable/creatable.type.ts
@@ -1,1 +1,2 @@
-export type Creatable<T> = { new(...args: Array<any>): T }
+// eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/prefer-function-type
+export type Creatable<T> = { new(...args: Array<any>): T };
