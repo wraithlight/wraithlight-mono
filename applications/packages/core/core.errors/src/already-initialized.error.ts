058d64e2f4ed6092320538dfa65d07558d99fcfe
kfarkasHU <sohamar@farkask.eu>
2024-12-07 23:00:35 +0100

    refactor(*): http support
    Ref: #509

--- applications/packages/core/core.errors/src/already-initialized.error.ts
+++ applications/packages/core/core.errors/src/already-initialized.error.ts
@@ -1,6 +1,11 @@
+import { HttpCode } from "@wraithlight/core.http";
+
 import { WraithlightError } from "./_wraithlight.error";
 
 export class AlreadyInitializedError extends WraithlightError {
+    public statusCode = HttpCode.InternalError;
+    public statusMessage = "E_ALREADY_INITIALIZED";
+
     constructor(
         objectName: string
     ) {
