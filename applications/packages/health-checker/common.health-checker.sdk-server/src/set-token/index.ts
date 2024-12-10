0f151fc96ea854626e3e79275c05668c71dc415f
kfarkasHU <sohamar@farkask.eu>
2024-12-10 18:51:31 +0100

    feat(*): add support for appinfo in HC level
    Ref: #1026

--- applications/packages/health-checker/common.health-checker.sdk-server/src/set-token/index.ts
+++ applications/packages/health-checker/common.health-checker.sdk-server/src/set-token/index.ts
@@ -1,3 +1,5 @@
 export * from "./set-token.fn";
 export * from "./token.const";
 export * from "./token.dictionary";
+export * from "./appinfo.const";
+export * from "./appinfo.dictionary";
--- applications/packages/health-checker/common.health-checker.sdk-server/src/set-token/set-token.fn.ts
+++ applications/packages/health-checker/common.health-checker.sdk-server/src/set-token/set-token.fn.ts
@@ -1,3 +1,5 @@
+import { HC_APPNAME_KEY, HC_APPVERSION_KEY } from "./appinfo.const";
+import { AppInfoDictionary } from "./appinfo.dictionary";
 import { HC_HEALTH_TOKENS_KEY, HC_METRICS_TOKENS_KEY } from "./token.const";
 import { TokenDictionary } from "./token.dictionary";
 
@@ -13,4 +15,13 @@ export function setHcMetricsTokens(
 ): void {
   const dictionary = TokenDictionary.getInstance();
   dictionary.set(HC_METRICS_TOKENS_KEY, tokens);
+}
+
+export function setAppinfo(
+  appName: string,
+  appVersion: string
+): void {
+  const dictionary = AppInfoDictionary.getInstance();
+  dictionary.set(HC_APPNAME_KEY, appName);
+  dictionary.set(HC_APPVERSION_KEY, appVersion);
 }
