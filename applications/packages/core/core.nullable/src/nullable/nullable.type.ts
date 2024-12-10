0181b41de3d32228f775bd4a35d32204eac3c9fa
kfarkasHU <sohamar@farkask.eu>
2024-12-10 19:49:39 +0100

    style(nullable): mark the package as deprecated
    Ref: #1035

--- applications/packages/core/core.nullable/src/nullable/nullable.type.ts
+++ applications/packages/core/core.nullable/src/nullable/nullable.type.ts
@@ -1,4 +1,5 @@
 /**
  * Represents any nullable type that can be `T`, `null` or `undefined`.
+ * @deprecated Use the same type from `"@wraithlight/framework.nullable"` instead.
  */
 export type Nullable<T> = T | undefined | null;
