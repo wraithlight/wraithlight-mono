eed0ab338a2ac8b19e29a95ad8fe76460119fdb1
kfarkasHU <sohamar@farkask.eu>
2024-12-12 23:21:23 +0100

    fix(full url): ignore empty paths

--- applications/packages/core/core.url/src/full-url/url.fns.ts
+++ applications/packages/core/core.url/src/full-url/url.fns.ts
@@ -1,7 +1,12 @@
+import { isEmptyString } from "@wraithlight/core.nullable";
+
 export function createFullUrl(
     baseUrl: string,
     ...path: Array<string>
 ): string {
-    const paths = path.map(m => m.startsWith("/") ? m : `/${m}`);
-    return baseUrl.concat(paths.join(""));
+  const paths = path.map(m => (isEmptyString(m) || m.startsWith("/"))
+    ? m
+    : `/${m}`)
+  ;
+  return baseUrl.concat(paths.join(""));
 }
