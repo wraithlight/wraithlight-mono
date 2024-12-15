31d226973ed07c6c81f0030bb10b41fdda28445e
kfarkasHU <sohamar@farkask.eu>
2024-12-15 18:10:54 +0100

    style(tools): autoformat
    Ref: #1025

--- applications/.tools/json-schema.mjs
+++ applications/.tools/json-schema.mjs
@@ -7,12 +7,12 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const libFolders = [
-    "apps",
-    "packages"
+  "apps",
+  "packages"
 ];
 const ignoreFolders = [
-    "node_modules",
-    "tools"
+  "node_modules",
+  "tools"
 ]
 
 const filesSwitch = "--files=";
@@ -29,66 +29,66 @@ if (!hasFilesParam) {
 const targetFiles = filesParam.map(m => m.replace(filesSwitch, ""));
 
 function getAllPackageJsonFiles() {
-    const result = libFolders.map(m => {
-        const path = join(__dirname, "..", m);
-        const files = getPackageJsonFiles(path);
-        return files;
-    });
-    return result.flat();
+  const result = libFolders.map(m => {
+    const path = join(__dirname, "..", m);
+    const files = getPackageJsonFiles(path);
+    return files;
+  });
+  return result.flat();
 }
 
 function getPackageJsonFiles(path) {
-    const result = [];
-    const content = readdirSync(path);
-    for (const item of content) {
-        if (ignoreFolders.includes(item)) continue;
-        const itemPath = join(path, item);
-        const stat = statSync(itemPath);
-        if(stat.isDirectory()) {
-            result.push(...getPackageJsonFiles(itemPath));
-        } else if (stat.isFile() && targetFiles.includes(item)) {
-            const content = readFileSync(itemPath).toString();
-            const jsonContent = JSON.parse(content);
-            result.push({ content: JSON.parse(content), path: itemPath, $schema: jsonContent.$schema });
-        }
+  const result = [];
+  const content = readdirSync(path);
+  for (const item of content) {
+    if (ignoreFolders.includes(item)) continue;
+    const itemPath = join(path, item);
+    const stat = statSync(itemPath);
+    if (stat.isDirectory()) {
+      result.push(...getPackageJsonFiles(itemPath));
+    } else if (stat.isFile() && targetFiles.includes(item)) {
+      const content = readFileSync(itemPath).toString();
+      const jsonContent = JSON.parse(content);
+      result.push({ content: JSON.parse(content), path: itemPath, $schema: jsonContent.$schema });
     }
-    return result;
+  }
+  return result;
 }
 
 function getSchemaFile({ path, $schema }) {
-    const schema = readFileSync(join(path, "..", $schema)).toString();
-    return JSON.parse(schema);
+  const schema = readFileSync(join(path, "..", $schema)).toString();
+  return JSON.parse(schema);
 }
 
 function validate(content, schema) {
-    const validator = new Validator();
-    const result = validator.validate(content, schema);
-    if (Array.isArray(result.errors)) {
-        return result.errors;
-    } else if (typeof result.errors === "object") {
-        return [result.errors];
-    } else {
-        return [];
-    }
+  const validator = new Validator();
+  const result = validator.validate(content, schema);
+  if (Array.isArray(result.errors)) {
+    return result.errors;
+  } else if (typeof result.errors === "object") {
+    return [result.errors];
+  } else {
+    return [];
+  }
 }
 
 const result = getAllPackageJsonFiles().map(m => {
-    const schema = getSchemaFile(m);
-    return {
-        result: validate(m.content, schema),
-        path: m.path
-    };
+  const schema = getSchemaFile(m);
+  return {
+    result: validate(m.content, schema),
+    path: m.path
+  };
 });
 if (result.length > 0) {
-    const data = result.map(m => ({
-        path: m.path,
-        errors: m.result.map(o => o.property + " " + o.message)
-    }));
-    const errors = data.filter(m => m.errors.length > 0);
-    if (errors.length > 0) {
-        console.warn(errors);
-        if (!isSilentRun) {
-          throw `Found ${errors.length} errors!`;
-        }
+  const data = result.map(m => ({
+    path: m.path,
+    errors: m.result.map(o => o.property + " " + o.message)
+  }));
+  const errors = data.filter(m => m.errors.length > 0);
+  if (errors.length > 0) {
+    console.warn(errors);
+    if (!isSilentRun) {
+      throw `Found ${errors.length} errors!`;
     }
+  }
 }
