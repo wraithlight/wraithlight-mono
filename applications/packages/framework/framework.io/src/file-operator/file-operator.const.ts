0288bb636e780c30dc9ead3c8c81407fff412f63
kfarkasHU <sohamar@farkask.eu>
2024-12-15 15:16:42 +0100

    refactor(file operator): use const file

---
+++ applications/packages/framework/framework.io/src/file-operator/file-operator.const.ts
@@ -0,0 +1,21 @@
+export const NATIVE_ENOENT = "ENOENT";
+export const NATIVE_EACCES = "EACCES";
+export const NATIVE_EPERM = "EPERM";
+export const NATIVE_EISDIR = "EISDIR";
+export const NATIVE_EMFILE = "EMFILE";
+export const NATIVE_ENAMETOOLONG = "ENAMETOOLONG";
+export const NATIVE_ENOTDIR = "ENOTDIR";
+export const NATIVE_EBADF = "EBADF";
+
+export const E_NO_CODE = "E_NO_CODE";
+
+export const ERROR_IO_NOT_EXIST = "E_IO_NOT_EXIST";
+export const ERROR_IO_ACCESS = "E_IO_ACCESS";
+export const ERROR_IO_PERMISSION = "E_IO_PERMISSION";
+export const ERROR_IO_IS_DIR = "E_IO_IS_DIR";
+export const ERROR_IO_TOO_MANY_OPEN = "E_IO_TOO_MANY_OPEN";
+export const ERROR_IO_NAME_TOO_LONG = "E_IO_NAME_TOO_LONG";
+export const ERROR_IO_NOT_DIR = "E_IO_NOT_DIR";
+export const ERROR_IO_BAD_DESCRIPTOR = "E_IO_BAD_DESCRIPTOR";
+export const ERROR_IO_UNKNOWN = "E_IO_UNKNOWN";
+export const ERROR_IO_JSON_PARSE = "E_IO_JSON_PARSE";
--- applications/packages/framework/framework.io/src/file-operator/file-operator.ts
+++ applications/packages/framework/framework.io/src/file-operator/file-operator.ts
@@ -1,11 +1,35 @@
 import { readFileSync } from "fs";
 
 import {
+  isEmptyStringOrNil
+} from "@wraithlight/framework.nullable";
+import {
   OperationResult,
   OperationResultFactory,
   isErrorResult
 } from "@wraithlight/framework.operation-result";
 import { cast } from "@wraithlight/framework.type-utils";
+
+import {
+  ERROR_IO_ACCESS,
+  ERROR_IO_BAD_DESCRIPTOR,
+  ERROR_IO_IS_DIR,
+  ERROR_IO_NAME_TOO_LONG,
+  ERROR_IO_NOT_DIR,
+  ERROR_IO_NOT_EXIST,
+  ERROR_IO_PERMISSION,
+  ERROR_IO_TOO_MANY_OPEN,
+  ERROR_IO_UNKNOWN,
+  E_NO_CODE,
+  NATIVE_EACCES,
+  NATIVE_EBADF,
+  NATIVE_EISDIR,
+  NATIVE_EMFILE,
+  NATIVE_ENAMETOOLONG,
+  NATIVE_ENOENT,
+  NATIVE_ENOTDIR,
+  NATIVE_EPERM
+} from "./file-operator.const";
 
 export class FileOperator {
 
@@ -37,17 +61,23 @@ export class FileOperator {
       const content = readFileSync(path).toString();
       return OperationResultFactory.success(content);
     } catch (e) {
-      const code = cast<{ code: string }>(e).code;
+      const error = cast<NodeJS.ErrnoException>(e);
+      const code = isEmptyStringOrNil(error.code)
+        ? E_NO_CODE
+        : error.code
+      ;
       switch (code) {
-        case "ENOENT": return OperationResultFactory.error("E_IO_NOT_EXIST");
-        case "EACCES": return OperationResultFactory.error("E_IO_ACCESS");
-        case "EPERM": return OperationResultFactory.error("E_IO_PERMISSION");
-        case "EISDIR": return OperationResultFactory.error("E_IO_IS_DIR");
-        case "EMFILE": return OperationResultFactory.error("E_IO_TOO_MANY_OPEN");
-        case "ENAMETOOLONG": return OperationResultFactory.error("E_IO_NAME_TOO_LONG");
-        case "ENOTDIR": return OperationResultFactory.error("E_IO_NOT_DIR");
-        case "EBADF": return OperationResultFactory.error("E_IO_BAD_DESCRIPTOR");
-        default: return OperationResultFactory.error("E_IO_UNKNOWN", code);
+        /* eslint-disable max-len */
+        case NATIVE_ENOENT: return OperationResultFactory.error(ERROR_IO_NOT_EXIST);
+        case NATIVE_EACCES: return OperationResultFactory.error(ERROR_IO_ACCESS);
+        case NATIVE_EPERM: return OperationResultFactory.error(ERROR_IO_PERMISSION);
+        case NATIVE_EISDIR: return OperationResultFactory.error(ERROR_IO_IS_DIR);
+        case NATIVE_EMFILE: return OperationResultFactory.error(ERROR_IO_TOO_MANY_OPEN);
+        case NATIVE_ENAMETOOLONG: return OperationResultFactory.error(ERROR_IO_NAME_TOO_LONG);
+        case NATIVE_ENOTDIR: return OperationResultFactory.error(ERROR_IO_NOT_DIR);
+        case NATIVE_EBADF: return OperationResultFactory.error(ERROR_IO_BAD_DESCRIPTOR);
+        default: return OperationResultFactory.error(ERROR_IO_UNKNOWN, code);
+        /* eslint-enable max-len */
       }
     }
   }
