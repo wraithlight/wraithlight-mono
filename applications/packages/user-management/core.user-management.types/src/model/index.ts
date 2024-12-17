8192f3e9660d376201b9a3cedff9e6bce2875996
kfarkasHU <sohamar@farkask.eu>
2024-12-17 22:04:16 +0100

    feat(model): external user DTOs
    Ref: #364

--- applications/packages/user-management/core.user-management.types/src/model/index.ts
+++ applications/packages/user-management/core.user-management.types/src/model/index.ts
@@ -1,6 +1,7 @@
 export * from "./http-response.model";
 
 export * from "./external-session.model";
+export * from "./external-user.model";
 
 export * from "./internal-session.model";
 export * from "./internal-user.model";
