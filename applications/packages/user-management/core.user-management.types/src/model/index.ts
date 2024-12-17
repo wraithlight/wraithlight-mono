1b8a8d82ba02c75460439509b14fca20e3954f24
kfarkasHU <sohamar@farkask.eu>
2024-12-17 21:25:08 +0100

    feat(model): internal user DTOs
    Ref: #364

--- applications/packages/user-management/core.user-management.types/src/model/index.ts
+++ applications/packages/user-management/core.user-management.types/src/model/index.ts
@@ -1,2 +1,3 @@
 export * from "./http-response.model";
 export * from "./internal-session.model";
+export * from "./internal-user.model";
---
+++ applications/packages/user-management/core.user-management.types/src/model/internal-user.model.ts
@@ -0,0 +1,76 @@
+import { Guid } from "@wraithlight/core.guid";
+
+import { UserStatus } from "../enum";
+
+interface UserResponse {
+  id: Guid;
+  username: string;
+  emailAddress: string
+  status: UserStatus;
+  createdAtUTC: string;
+  isLoggedIn: boolean;
+  lastLoginAtUTC: string
+  numberOfContexts: number
+}
+
+export interface InternalUsersGetResponse extends ReadonlyArray<UserResponse> {
+}
+
+export interface InternalUserGetResponse extends UserResponse {
+}
+
+export interface InternalUserPostRequest {
+  username: string;
+  emailAddress: string;
+  password: string;
+  passwordConfirmation: string;
+}
+
+export interface InternalUserPostResponse extends UserResponse {
+}
+
+export interface InternalCheckEmailRequest {
+  emailAddress: string;
+}
+
+export interface InternalCheckEmailResponse {
+}
+
+interface UserPatch {
+  name: string;
+  emailAddress: string;
+  password: string;
+  passwordConfirmation: string;
+}
+
+export interface InternalUserPatchRequest {
+  update: Partial<UserPatch>;
+}
+
+export interface InternalUserPatchResponse extends UserResponse {
+}
+
+export interface InternalUserDeleteResponse {
+}
+
+export interface InternalCheckUsernameRequest {
+  username: string;
+}
+
+export interface InternalCheckUsernameResponse {
+}
+
+export interface InternalUserContextPostRequest {
+}
+
+export interface InternalUserContextPostResponse {
+  relationId: Guid;
+  userId: Guid;
+  contextId: Guid;
+  createdById: Guid;
+  createdByUsername: string;
+  createdAtUTC: string;
+}
+
+export interface InternalUserContextDeleteResponse {
+}
