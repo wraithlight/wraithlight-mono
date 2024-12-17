9b9eae7811c5b8c4a2e37af9faf4c584d4da3819
kfarkasHU <sohamar@farkask.eu>
2024-12-17 22:32:01 +0100

    feat(model): internal context DTOs
    Ref: #364

---
+++ applications/packages/user-management/core.user-management.types/src/model/internal-context.model.ts
@@ -0,0 +1,58 @@
+import { Guid } from "@wraithlight/core.guid";
+
+interface ContextResponse {
+  id: Guid;
+  label: string;
+  description: string;
+  isDeleted: false
+  createdAtUTC: string;
+  createdById: Guid;
+  createdByUsername: string;
+  lastUpdatedAtUTC: string;
+  lastUpdatedById: Guid;
+  lastUpdatedByUsername: string;
+  numberOfUsers: number;
+}
+
+export interface InternalContextsGetResponse {
+  items: ReadonlyArray<ContextResponse>;
+  count: number;
+}
+
+export interface InternalContextGetResponse extends ContextResponse {
+}
+
+export interface InternalContextPostRequest {
+  label: string;
+  description: string;
+}
+
+export interface InternalContextPostResponse {
+  items: ReadonlyArray<ContextResponse>;
+  count: number;
+}
+
+export interface InternalContextPatchRequest {
+  label: string;
+  description: string;
+}
+
+export interface InternalContextPatchResponse {
+  items: ReadonlyArray<ContextResponse>;
+  count: number;
+}
+
+export interface InternalContextDeleteResponse {
+}
+
+export interface InternalContextUsersDeleteResponse {
+}
+
+export interface InternalContextUserPostRequest {
+}
+
+export interface InternalContextUserPostResponse {
+}
+
+export interface InternalContextUsersDeleteResponse {
+}
