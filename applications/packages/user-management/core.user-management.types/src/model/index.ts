7545aafb372c2841049494a24a790c795d87b441
kfarkasHU <sohamar@farkask.eu>
2024-12-17 22:32:01 +0100

    feat(model): internal context DTOs
    Ref: #364

--- applications/packages/user-management/core.user-management.types/src/model/index.ts
+++ applications/packages/user-management/core.user-management.types/src/model/index.ts
@@ -3,5 +3,6 @@ export * from "./http-response.model";
 export * from "./external-session.model";
 export * from "./external-user.model";
 
+export * from "./internal-context.model";
 export * from "./internal-session.model";
 export * from "./internal-user.model";
