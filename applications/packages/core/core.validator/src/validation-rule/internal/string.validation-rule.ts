46d1bf6cd15be1af2e05286677f1a58fcc924817
kfarkasHU <sohamar@farkask.eu>
2025-01-04 19:41:14 +0100

    feat(internal): add `toBeInArray()`
    Ref: #364

--- applications/packages/core/core.validator/src/validation-rule/internal/string.validation-rule.ts
+++ applications/packages/core/core.validator/src/validation-rule/internal/string.validation-rule.ts
@@ -2,28 +2,36 @@ import { IterableValidationRule } from "./iterable.validation-rule";
 
 export class StringValidationRule extends IterableValidationRule<string> {
 
-    public testType(): StringValidationRule {
-        this.addRuleCase(
-            (item: string) => typeof item === "string",
-            "E_STRING_TESTTYPE"
-        );
-        return this;
-    }
+  public testType(): StringValidationRule {
+    this.addRuleCase(
+      (item: string) => typeof item === "string",
+      "E_STRING_TESTTYPE"
+    );
+    return this;
+  }
 
-    public toMatchRegex(regex: RegExp): StringValidationRule {
-        this.addRuleCase(
-            (item: string) => regex.test(item),
-            "E_STRING_TOMATCHREGEX"
-        );
-        return this;
-    }
+  public toMatchRegex(regex: RegExp): StringValidationRule {
+    this.addRuleCase(
+      (item: string) => regex.test(item),
+      "E_STRING_TOMATCHREGEX"
+    );
+    return this;
+  }
 
-    public notToMatchRegex(regex: RegExp): StringValidationRule {
-        this.addRuleCase(
-            (item: string) => !regex.test(item),
-            "E_STRING_TOMATCHREGEX"
-        );
-        return this;
-    }
+  public notToMatchRegex(regex: RegExp): StringValidationRule {
+    this.addRuleCase(
+      (item: string) => !regex.test(item),
+      "E_STRING_TOMATCHREGEX"
+    );
+    return this;
+  }
+
+  public toBeInArray(array: ReadonlyArray<string>): StringValidationRule {
+    this.addRuleCase(
+      (item: string) => array.includes(item),
+      "E_STRING_TOBEINARRAY"
+    );
+    return this;
+  }
 
 }
