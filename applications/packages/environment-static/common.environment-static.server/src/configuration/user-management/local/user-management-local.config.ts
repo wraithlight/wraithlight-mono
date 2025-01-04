40f088633298c80e1a11759b8ff312423665b64a
kfarkasHU <sohamar@farkask.eu>
2025-01-04 19:41:36 +0100

    feat(user management): add apitokens
    Ref: #364

--- applications/packages/environment-static/common.environment-static.server/src/configuration/user-management/local/user-management-local.config.ts
+++ applications/packages/environment-static/common.environment-static.server/src/configuration/user-management/local/user-management-local.config.ts
@@ -13,5 +13,8 @@ export const SERVER_LOCAL_USER_MANAGEMENT_CONFIG: Readonly<
         iv: "",
         secret: "",
         key: ""
-    }
+    },
+    apiTokens: [
+      "bb3a7a80-44ec-4b74-9b7b-c8cd58f777b3"
+    ]
 };
