159541acc6512c69ac27f5d1997b6ce8fb5a0770
kfarkasHU <sohamar@farkask.eu>
2025-01-04 17:04:09 +0100

    style(*): mark the lib as deprecated
    Ref: #1097

--- applications/packages/core/logger/core.logger.types/src/model/logger-config.model.ts
+++ applications/packages/core/logger/core.logger.types/src/model/logger-config.model.ts
@@ -1,5 +1,8 @@
 import { LogSeverity } from "../enum";
 
+/**
+ * @deprecated - Do not use this.
+ */
 export interface LoggerConfig {
     enabledLogSeverities: ReadonlyArray<LogSeverity>;
     applicationName: string;
