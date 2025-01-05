2f41b1a190a07ba746ce507addc066f86b560a41
kfarkasHU <sohamar@farkask.eu>
2025-01-05 13:08:52 +0100

    feat(data): proper user data
    Ref: #364

--- database/auth/src/data/user.sql
+++ database/auth/src/data/user.sql
@@ -4,71 +4,80 @@ INSERT INTO `User` (
     `Username`,
     `EmailAddress`,
     `PasswordHash`,
-    -- `LanguageId`,
+    `PasswordSalt`,
     `FailedLoginAttempts`,
     `UserStatus`,
     `CreatedAtUtc`,
     `CreatedByUserId`,
+    `LanguageId`,
     `IsDeleted`
 ) VALUES (
     "7777ba27-da56-4ddd-ae97-f38a1e48effa",
     "root",
     "root@wraithlight.ai",
-    -- password: root
-    -- hash: QwErT123Qw
-    -- pwToHash: QwErT123QwrootwQ321TrEwQ
-    "ac3b6887099db1166429578c3abbac9fe582b2b6b3ca83bc470e36776b0eeb02c21bbea150b7b49849111a0bc84ac0384f952bfa724f05a6881418a741ccfd79",
-    0,
+    -- Password1
+    "$2a$12$JB/VIMi29z/s4IcGgEbwc.tvP8ynlxvq0jwIEYwBkPtolnTsDx1oS",
     "",
+    0,
+    "ACTIVE",
     NOW(),
     "7777ba27-da56-4ddd-ae97-f38a1e48effa",
+    "7655c0c5-c14b-4ebe-bf4a-d67efa2fe4b1",
     0
 );
 
--- -- Platform deployer svc user
--- INSERT INTO `User` (
---     `Id`,
---     `Username`,
---     `EmailAddress`,
---     `PasswordHash`,
---     `LanguageId`,
---     `FailedLoginAttempts`,
---     `Status`,
---     `IsDeleted`
--- ) VALUES (
---     "3f0e995e-6bcc-71f9-9d17-5e82c7c84301",
---     "platform_deployer_svc",
---     "platform_deployer_svc@wraithlight.ai",
---     -- password: P4ssw0rd_1
---     -- hash: QwErT123Qw
---     -- pwToHash: QwErT123QwP4ssw0rd_1wQ321TrEwQ
---     "$2a$12$OIbOt.PpDKdNgt4NdHAa5Op5InVxfide0T8ZHbVTHzX1A/li33sq6",
---     "b6d2118f-1fca-4485-b61f-82a6538e8144", -- english
---     0,
---     1,
---     1
--- );
+-- Platform deployer svc user
+INSERT INTO `User` (
+    `Id`,
+    `Username`,
+    `EmailAddress`,
+    `PasswordHash`,
+    `PasswordSalt`,
+    `FailedLoginAttempts`,
+    `UserStatus`,
+    `CreatedAtUtc`,
+    `CreatedByUserId`,
+    `LanguageId`,
+    `IsDeleted`
+) VALUES (
+    "3f0e995e-6bcc-71f9-9d17-5e82c7c84301",
+    "platform_deployer_svc",
+    "platform_deployer_svc@wraithlight.ai",
+    -- Password2
+    "$2a$12$9Wg1tYiXthR0r4C/GvtZQeOFebdS4cUXUCfVNzNwV29B7..bt/5oa",
+    "",
+    0,
+    "INACTIVE",
+    NOW(),
+    "7777ba27-da56-4ddd-ae97-f38a1e48effa",
+    "b6d2118f-1fca-4485-b61f-82a6538e8144",
+    1
+);
 
--- -- Platform onboarding svc user
--- INSERT INTO `User` (
---     `Id`,
---     `Username`,
---     `EmailAddress`,
---     `PasswordHash`,
---     `LanguageId`,
---     `FailedLoginAttempts`,
---     `Status`,
---     `IsDeleted`
--- ) VALUES (
---     "ebe313ba-47e9-68df-978e-0d6a0ef34c6b",
---     "platform_onboarding_svc",
---     "platform_onboarding_svc@wraithlight.ai",
---     -- password: P4ssw0rd_1
---     -- hash: QwErT123Qw
---     -- pwToHash: QwErT123QwP4ssw0rd_1wQ321TrEwQ
---     "$2a$12$OIbOt.PpDKdNgt4NdHAa5Op5InVxfide0T8ZHbVTHzX1A/li33sq6",
---     "b6d2118f-1fca-4485-b61f-82a6538e8144", -- english
---     0,
---     1,
---     1
--- );
+-- Platform onboarding svc user
+INSERT INTO `User` (
+    `Id`,
+    `Username`,
+    `EmailAddress`,
+    `PasswordHash`,
+    `PasswordSalt`,
+    `FailedLoginAttempts`,
+    `UserStatus`,
+    `CreatedAtUtc`,
+    `CreatedByUserId`,
+    `LanguageId`,
+    `IsDeleted`
+) VALUES (
+  "ebe313ba-47e9-68df-978e-0d6a0ef34c6b",
+  "platform_onboarding_svc",
+  "platform_onboarding_svc@wraithlight.ai",
+  -- Password3
+  "$2a$12$ltewtJti8BlGfXg.sb5eFOCJdWSpcY4TzvMXWThDEPXOBZayCrANa",
+  "",
+  0,
+  "INACTIVE",
+  NOW(),
+  "7777ba27-da56-4ddd-ae97-f38a1e48effa",
+  "3c392ece-4dd8-21ba-8b1b-e16a347aba71",
+  1
+);
