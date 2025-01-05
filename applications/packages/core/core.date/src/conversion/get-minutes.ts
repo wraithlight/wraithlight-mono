057de56df83938b5680a5b37ea950129db5bea65
kfarkasHU <sohamar@farkask.eu>
2025-01-05 17:33:51 +0100

    style(*): mark core date as deprecated
    Ref: #1101

--- applications/packages/core/core.date/src/conversion/get-minutes.ts
+++ applications/packages/core/core.date/src/conversion/get-minutes.ts
@@ -1,6 +1,9 @@
 import { SEC_IN_MINUTE } from "./get-minutes.const";
 import { getSeconds } from "./get-seconds";
 
+/**
+ * @deprecated Import this from `@wraithlight/framework.date` instead.
+ */
 export function getMinutes(minutes: number): number {
     return minutes * getSeconds(SEC_IN_MINUTE);
 }
