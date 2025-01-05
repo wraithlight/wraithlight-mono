47a5dfcafe0e3c0c2a3126d0abc318cf5e621c63
kfarkasHU <sohamar@farkask.eu>
2025-01-05 17:33:33 +0100

    feat(*): add fw date lib
    Ref: #1101

---
+++ applications/packages/framework/framework.date/src/conversion/get-hours.const.ts
@@ -0,0 +1,1 @@
+export const MINUTE_IN_HOUR = 60;
---
+++ applications/packages/framework/framework.date/src/date/add-minutes.ts
@@ -0,0 +1,10 @@
+import { addSeconds } from "./add-seconds";
+import { minutesToSeconds } from "./operations";
+
+export function addMinutes(date: Date, minutes: number): Date {
+    const s = minutesToSeconds(minutes);
+    return addSeconds(
+        date,
+        s
+    );
+}
