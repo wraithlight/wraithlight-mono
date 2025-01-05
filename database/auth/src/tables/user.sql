d305f8bac88132b171999d8fdfe3893c38b0ecde
kfarkasHU <sohamar@farkask.eu>
2025-01-05 13:08:26 +0100

    feat(tables): add languageid and salt to user
    Ref: #364

--- database/auth/src/tables/user.sql
+++ database/auth/src/tables/user.sql
@@ -4,11 +4,13 @@ CREATE TABLE `User` (
     `Username` VARCHAR(100) NOT NULL,
     `UserStatus` VARCHAR(32) NOT NULL,
     `PasswordHash` VARCHAR(128) NOT NULL,
+    `PasswordSalt` VARCHAR(128) NOT NULL,
     `FailedLoginAttempts` TINYINT(3) UNSIGNED NOT NULL,
     `CreatedAtUtc` DATETIME NOT NULL,
     `CreatedByUserId` VARCHAR(36) NOT NULL,
     `UpdatedAtUtc` DATETIME NULL,
     `UpdatedByUserId` VARCHAR(36) NULL,
-    `IsDeleted` BIT NOT NULL,
+    `LanguageId` VARCHAR(36) NULL,
+    `IsDeleted` BOOLEAN NOT NULL,
     PRIMARY KEY (`Id`)
 ) ENGINE = MyISAM DEFAULT CHARSET=utf8;
