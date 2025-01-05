1f2e50780e8d76f896112c7afccc43ea516c3556
kfarkasHU <sohamar@farkask.eu>
2025-01-05 13:12:43 +0100

    feat(*): add forbidden and notfound errors
    Ref: #364

--- applications/packages/core/core.errors/src/index.ts
+++ applications/packages/core/core.errors/src/index.ts
@@ -8,5 +8,7 @@ export * from "./bad-request.error";
 export * from "./unauthorized.error";
 export * from "./internal-server-error.error";
 export * from "./conflict.error";
+export * from "./not-found.error";
+export * from "./forbidden.error";
 
 export * from "./_wraithlight.error.guard";
