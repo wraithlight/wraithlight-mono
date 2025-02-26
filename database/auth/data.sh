107a777da7292ba9c89070a593f83777b81b97a4
kfarkasHU <sohamar@farkask.eu>
2025-02-26 22:20:09 +0100

    refactor(auth): rename `auth` to `user-management`
    Ref: #264

--- database/auth/data.sh
+++ database/auth/data.sh
@@ -1,8 +1,8 @@
-CONTAINER_NAME=wraithlight-db-auth
-SQL_ROOT_PASSWORD=wraithlight-auth-pw
-SQL_USERNAME=wraithlight-auth-user-1
-SQL_PASSWORD=wraithlight-auth-user-1-pw
+CONTAINER_NAME=wraithlight-db-um
+SQL_ROOT_PASSWORD=wraithlight-um-pw
+SQL_USERNAME=wraithlight-um-user-1
+SQL_PASSWORD=wraithlight-um-user-1-pw
 OUT_PORT=9000
 IN_PORT=3306
-DATABASE_NAME=WL_AUTH
+DATABASE_NAME=WL_UM
 NETWORK_NAME=WL_Internal
