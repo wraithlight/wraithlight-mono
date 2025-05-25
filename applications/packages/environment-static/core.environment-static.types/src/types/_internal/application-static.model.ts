500eecf81f430900d68374b875c92285a6823bde
kfarkasHU <sohamar@farkask.eu>
2025-05-25 16:42:43 +0200

    style(internal): autoformat

--- applications/packages/environment-static/core.environment-static.types/src/types/_internal/application-static.model.ts
+++ applications/packages/environment-static/core.environment-static.types/src/types/_internal/application-static.model.ts
@@ -1,25 +1,33 @@
 import { ApplicationName } from "@wraithlight/core.auth.constant";
 
 export interface ApplicationStatic<
-    TContent,
-    TEditor,
-    TForum,
-    TGameApplication,
-    TGameWebsite,
-    TLogs,
-    TUserManagement,
-    TWebsite,
-    TNotifier,
-    TRemoteConfig
+  TContent,
+  TEditor,
+  TForum,
+  TGameApplication,
+  TGameWebsite,
+  TLogs,
+  TUserManagement,
+  TWebsite,
+  TNotifier,
+  TRemoteConfig,
+  TCommsESS,
+  TCommsNPS,
+  TCommsPSS,
+  TCommsSSS
 > {
-    [ApplicationName.Content]: TContent,
-    [ApplicationName.Editor]: TEditor,
-    [ApplicationName.Forum]: TForum,
-    [ApplicationName.GameApplication]: TGameApplication,
-    [ApplicationName.GameWebsite]: TGameWebsite,
-    [ApplicationName.Logs]: TLogs,
-    [ApplicationName.UserManagement]: TUserManagement,
-    [ApplicationName.Website]: TWebsite,
-    [ApplicationName.Notifier]: TNotifier,
-    [ApplicationName.RemoteConfig]: TRemoteConfig
+  [ApplicationName.Content]: TContent,
+  [ApplicationName.Editor]: TEditor,
+  [ApplicationName.Forum]: TForum,
+  [ApplicationName.GameApplication]: TGameApplication,
+  [ApplicationName.GameWebsite]: TGameWebsite,
+  [ApplicationName.Logs]: TLogs,
+  [ApplicationName.UserManagement]: TUserManagement,
+  [ApplicationName.Website]: TWebsite,
+  [ApplicationName.Notifier]: TNotifier,
+  [ApplicationName.RemoteConfig]: TRemoteConfig,
+  [ApplicationName.CommsESS]: TCommsESS
+  [ApplicationName.CommsNPS]: TCommsNPS
+  [ApplicationName.CommsPSS]: TCommsPSS
+  [ApplicationName.CommsSSS]: TCommsSSS
 }
