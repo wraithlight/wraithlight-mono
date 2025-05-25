88ede2cb25dc3bf289f947c27fd25c47df465161
kfarkasHU <sohamar@farkask.eu>
2025-05-25 16:49:14 +0200

    feat(server): export comms domain

--- applications/packages/environment-static/core.environment-static.types/src/types/server/server.model.ts
+++ applications/packages/environment-static/core.environment-static.types/src/types/server/server.model.ts
@@ -1,30 +1,38 @@
 import { ApplicationStatic, EnvironmentStatic } from "../_internal";
 
 import {
-    ContentServer,
-    EditorServer,
-    ForumServer,
-    GameApplicationServer,
-    GameWebsiteServer,
-    LogsServer,
-    NotifierServer,
-    RemoteConfigServer,
-    UserManagementServer,
-    WebsiteServer
+  CommsESSServer,
+  CommsNPSServer,
+  CommsPSSServer,
+  CommsSSSServer,
+  ContentServer,
+  EditorServer,
+  ForumServer,
+  GameApplicationServer,
+  GameWebsiteServer,
+  LogsServer,
+  NotifierServer,
+  RemoteConfigServer,
+  UserManagementServer,
+  WebsiteServer
 } from "./apps";
 import { CommonServer } from "./common";
 
 export interface EnvironmentStaticServer extends ApplicationStatic<
-    EnvironmentStatic<ContentServer>,
-    EnvironmentStatic<EditorServer>,
-    EnvironmentStatic<ForumServer>,
-    EnvironmentStatic<GameApplicationServer>,
-    EnvironmentStatic<GameWebsiteServer>,
-    EnvironmentStatic<LogsServer>,
-    EnvironmentStatic<UserManagementServer>,
-    EnvironmentStatic<WebsiteServer>,
-    EnvironmentStatic<NotifierServer>,
-    EnvironmentStatic<RemoteConfigServer>
+  EnvironmentStatic<ContentServer>,
+  EnvironmentStatic<EditorServer>,
+  EnvironmentStatic<ForumServer>,
+  EnvironmentStatic<GameApplicationServer>,
+  EnvironmentStatic<GameWebsiteServer>,
+  EnvironmentStatic<LogsServer>,
+  EnvironmentStatic<UserManagementServer>,
+  EnvironmentStatic<WebsiteServer>,
+  EnvironmentStatic<NotifierServer>,
+  EnvironmentStatic<RemoteConfigServer>,
+  EnvironmentStatic<CommsESSServer>,
+  EnvironmentStatic<CommsNPSServer>,
+  EnvironmentStatic<CommsPSSServer>,
+  EnvironmentStatic<CommsSSSServer>
 > {
-    common: EnvironmentStatic<CommonServer>
+  common: EnvironmentStatic<CommonServer>
 }
