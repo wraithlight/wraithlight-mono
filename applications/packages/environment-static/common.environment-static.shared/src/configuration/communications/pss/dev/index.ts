1e3d6c74a4854c9b2f5e66e5d6159fe9fd9adb0d
kfarkasHU <sohamar@farkask.eu>
2025-05-26 21:18:52 +0200

    feat(*): shared config and reader for PSS
    Ref: #1319

---
+++ applications/packages/environment-static/common.environment-static.shared/src/configuration/communications/pss/dev/index.ts
@@ -0,0 +1,1 @@
+export * from "./pss-dev.config";
---
+++ applications/packages/environment-static/common.environment-static.shared/src/configuration/communications/pss/local/pss-local.config.ts
@@ -0,0 +1,8 @@
+import { CommsPSSShared } from "@wraithlight/core.environment-static.types";
+
+export const SHARED_LOCAL_COMMS_PSS_CONFIG: Readonly<CommsPSSShared> = {
+  server: {
+    port: 4513,
+    baseUrl: ""
+  }
+};
