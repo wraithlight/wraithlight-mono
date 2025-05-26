a82277fcac2be983701f4e3f2c7112b7fc9a0831
kfarkasHU <sohamar@farkask.eu>
2025-05-26 21:22:17 +0200

    feat(*): server config and reader for ESS
    Ref: #1313

---
+++ applications/packages/environment-static/common.environment-static.server/src/configuration/communications/ess/production/ess-production.config.ts
@@ -0,0 +1,11 @@
+import { CommsESSServer } from "@wraithlight/core.environment-static.types";
+
+export const SERVER_PRODUCTION_COMMS_ESS_CONFIG: Readonly<CommsESSServer> = {
+  database: {
+    host: "",
+    port: 0,
+    username: "",
+    password: "",
+    database: ""
+  }
+};
