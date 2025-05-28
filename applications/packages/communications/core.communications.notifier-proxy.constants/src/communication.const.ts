eeea3c0059672eba0900bce2a68fb609ca48af96
kfarkasHU <sohamar@farkask.eu>
2025-05-28 23:29:27 +0200

    feat(*): NPS const lib
    Ref: #1228

---
+++ applications/packages/communications/core.communications.notifier-proxy.constants/src/communication.const.ts
@@ -0,0 +1,42 @@
+const API_BASE = (version: number): string => `/api/v${version}`;
+const RESOURCE_BASE = (resource: string): string => `${API_BASE(1)}/${resource}`;
+
+const COMMUNICATION_RES = RESOURCE_BASE("communication");
+
+export const API_ENDPOINTS = {
+  v1: {
+    communication: {
+      forServer: (): string => COMMUNICATION_RES,
+      get: {
+        forServer: (): string => ``,
+        forClient: (): string => COMMUNICATION_RES
+      },
+      post: {
+        forServer: (): string => ``,
+        forClient: (): string => COMMUNICATION_RES
+      },
+      id: {
+        get: {
+          forServer: (): string => `/:id`,
+          forClient: (id: string): string => `${COMMUNICATION_RES}/${id}`
+        },
+        success: {
+          patch: {
+            forServer: (): string => `/:id/success`,
+            forClient: (
+              id: string
+            ): string => `${COMMUNICATION_RES}/${id}/success`
+          }
+        },
+        fail: {
+          patch: {
+            forServer: (): string => `/:id/fail`,
+            forClient: (
+              id: string
+            ): string => `${COMMUNICATION_RES}/${id}/fail`
+          }
+        }
+      }
+    }
+  }
+};
