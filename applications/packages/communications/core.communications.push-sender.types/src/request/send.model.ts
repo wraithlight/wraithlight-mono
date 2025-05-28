6b9ae65a883b1ced1fc149aacf69530b461e0c06
kfarkasHU <sohamar@farkask.eu>
2025-05-28 23:51:39 +0200

    refactor(request): move applink to SSS to PSS

--- applications/packages/communications/core.communications.push-sender.types/src/request/send.model.ts
+++ applications/packages/communications/core.communications.push-sender.types/src/request/send.model.ts
@@ -3,6 +3,7 @@ import { SendRequest } from "@wraithlight/core.communications.types";
 export interface SendPushNotificationAddtionalPayloadRequest {
   subject: string;
   logoUrl: string;
+  applicationLink: string;
 }
 
 export interface SendPushNotificationRequest
