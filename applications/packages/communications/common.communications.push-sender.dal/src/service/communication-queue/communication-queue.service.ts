40fcb9efc06c0863b7ad23504d6dce2391e5357a
kfarkasHU <sohamar@farkask.eu>
2025-05-29 22:07:15 +0200

    feat(*): add PSS DAL lib
    Ref: #1292

---
+++ applications/packages/communications/common.communications.push-sender.dal/src/service/communication-queue/communication-queue.service.ts
@@ -0,0 +1,115 @@
+import { Guid } from "@wraithlight/core.guid";
+import { utcNow } from "@wraithlight/framework.date";
+import { isNil } from "@wraithlight/framework.nullable";
+import {
+  OperationResult,
+  OperationResultFactory
+} from "@wraithlight/framework.operation-result";
+
+import {
+  CommunicationQueueDbo,
+  PushSenderDbContextFactory
+} from "../../db-context";
+
+import { ERROR_CODES } from "./communication-queue.const";
+
+export class NotificationQueueService {
+
+  private readonly _context = PushSenderDbContextFactory
+    .getInstance()
+    .getDbContext()
+  ;
+
+  public async create(
+    id: Guid,
+    proxyId: Guid,
+    recipientEmailAddress: string,
+    subject: string,
+    content: string,
+    applicationLink: string,
+    logoUrl: string,
+    receviedAtUtc: Date,
+    lastUpdatedAtUtc: Date
+  ): Promise<OperationResult<CommunicationQueueDbo>> {
+    const model: CommunicationQueueDbo = {
+      id: id,
+      proxyId: proxyId,
+      recipientEmailAddress: recipientEmailAddress,
+      subject: subject,
+      content: content,
+      applicationLink: applicationLink,
+      logoUrl: logoUrl,
+      status: "NOTIFICATION_IN_QUEUE",
+      receviedAtUtc: receviedAtUtc,
+      lastUpdatedAtUtc: lastUpdatedAtUtc
+    };
+    await this._context.CommunicationQueue
+      .insert(model)
+      .run()
+    ;
+    return this.getByIdCore(id);
+  }
+
+  public async getById(
+    id: Guid
+  ): Promise<OperationResult<CommunicationQueueDbo>> {
+    return this.getByIdCore(id);
+  }
+
+  public async markAsSucceed(
+    id: Guid,
+    sentAtUtc: Date
+  ): Promise<OperationResult<CommunicationQueueDbo>> {
+    return this.updateCore(
+      id,
+      {
+        status: "NOTIFICATION_SENT",
+        sentAtUtc: sentAtUtc,
+        lastUpdatedAtUtc: utcNow()
+      }
+    );
+  }
+
+  public async markAsFail(
+    id: Guid,
+    errorMessage: string
+  ): Promise<OperationResult<CommunicationQueueDbo>> {
+    return this.updateCore(
+      id,
+      {
+        status: "NOTIFICATION_ERROR",
+        errorMessage: errorMessage,
+        lastUpdatedAtUtc: utcNow()
+      }
+    );
+  }
+
+  private async updateCore(
+    id: Guid,
+    model: Partial<CommunicationQueueDbo>
+  ): Promise<OperationResult<CommunicationQueueDbo>> {
+    await this._context.CommunicationQueue
+      .update("id", id, model)
+      .run()
+    ;
+
+    return this.getByIdCore(id);
+  }
+
+  private async getByIdCore(
+    id: Guid
+  ): Promise<OperationResult<CommunicationQueueDbo>> {
+    const result = await this._context.CommunicationQueue
+      .select()
+      .where("id", id)
+      .first()
+    ;
+
+    if (isNil(result)) {
+      return OperationResultFactory.error(ERROR_CODES.NOT_FOUND);
+    }
+
+    return OperationResultFactory.success(result);
+  }
+
+}
