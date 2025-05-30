6d30cd096d50a9d991d174c3d3a978d21227646b
kfarkasHU <sohamar@farkask.eu>
2025-05-30 19:12:22 +0200

    fix(*): test mock
    Ref: #1292

--- applications/packages/communications/common.communications.push-sender.dal/src/initialize.fn.spec.ts
+++ applications/packages/communications/common.communications.push-sender.dal/src/initialize.fn.spec.ts
@@ -1,7 +1,7 @@
 const initializeSpy = jest.fn();
 jest.mock("./db-context/dbcontext.factory", () => {
   return {
-    EmailSenderDbContextFactory: {
+    PushSenderDbContextFactory: {
       initialize: initializeSpy
     }
   }
