1e6e4a0ff55ca58dfb8db685807f881787e60459
kfarkasHU <sohamar@farkask.eu>
2025-06-08 11:17:29 +0200

    chore(*): undefined consolidation
    Ref: #1372

--- applications/packages/auth/common.auth-sdk.client/src/sdk/auth/auth.reducer.ts
+++ applications/packages/auth/common.auth-sdk.client/src/sdk/auth/auth.reducer.ts
@@ -1,4 +1,5 @@
 import { Action, ActionWithPayload, Store } from "@wraithlight/core.redux";
+import { GLOBAL_UNDEFINED } from "@wraithlight/core.undefined";
 
 import { INITIAL_AUTH_STATE } from "../state.const";
 import { IAuthContainerStore } from "../state.model";
@@ -6,178 +7,178 @@ import { IAuthContainerStore } from "../state.model";
 import { AuthAction } from "./auth.action";
 
 export function initializeReducers(
-    store: Store<IAuthContainerStore>
+  store: Store<IAuthContainerStore>
 ): Store<IAuthContainerStore> {
-    store.addReducer(
-        [AuthAction.login],
-        (
-            state
-        ) => {
-            return {
-                ...state,
-                auth: {
-                    ...state.auth ?? INITIAL_AUTH_STATE,
-                    session: {
-                        ...state.auth?.session ?? INITIAL_AUTH_STATE.session,
-                        isBusy: true
-                    }
-                }
-            };
+  store.addReducer(
+    [AuthAction.login],
+    (
+      state
+    ) => {
+      return {
+        ...state,
+        auth: {
+          ...state.auth ?? INITIAL_AUTH_STATE,
+          session: {
+            ...state.auth?.session ?? INITIAL_AUTH_STATE.session,
+            isBusy: true
+          }
         }
-    );
-    store.addReducer(
-        [AuthAction.loginFail],
-        (
-            state,
-            action: ActionWithPayload<Array<string>>
-        ) => {
-            return {
-                ...state,
-                auth: {
-                    ...state.auth ?? INITIAL_AUTH_STATE,
-                    session: {
-                        ...state.auth?.session ?? INITIAL_AUTH_STATE.session,
-                        isBusy: false,
-                        errors: action.payload
-                    }
-                }
-            };
+      };
+    }
+  );
+  store.addReducer(
+    [AuthAction.loginFail],
+    (
+      state,
+      action: ActionWithPayload<Array<string>>
+    ) => {
+      return {
+        ...state,
+        auth: {
+          ...state.auth ?? INITIAL_AUTH_STATE,
+          session: {
+            ...state.auth?.session ?? INITIAL_AUTH_STATE.session,
+            isBusy: false,
+            errors: action.payload
+          }
         }
-    );
-    store.addReducer(
-        [AuthAction.loginSuccess],
-        (
-            state,
-            action: ActionWithPayload<{ token: string, validUntil: Date }>
-        ) => {
-            return {
-                ...state,
-                auth: {
-                    ...state.auth ?? INITIAL_AUTH_STATE,
-                    session: {
-                        ...state.auth?.session ?? INITIAL_AUTH_STATE.session,
-                        isBusy: false,
-                        isLoggedIn: true,
-                        token: action.payload.token,
-                        tokenValidUntil: action.payload.validUntil
-                    }
-                }
-            };
+      };
+    }
+  );
+  store.addReducer(
+    [AuthAction.loginSuccess],
+    (
+      state,
+      action: ActionWithPayload<{ token: string, validUntil: Date }>
+    ) => {
+      return {
+        ...state,
+        auth: {
+          ...state.auth ?? INITIAL_AUTH_STATE,
+          session: {
+            ...state.auth?.session ?? INITIAL_AUTH_STATE.session,
+            isBusy: false,
+            isLoggedIn: true,
+            token: action.payload.token,
+            tokenValidUntil: action.payload.validUntil
+          }
         }
-    );
-    store.addReducer(
-        [AuthAction.logout],
-        (
-            state,
-            _action: ActionWithPayload<{ token: string}>
-        ) => {
-            return {
-                ...state,
-                auth: {
-                    ...state.auth ?? INITIAL_AUTH_STATE,
-                    session: {
-                        ...state.auth?.session ?? INITIAL_AUTH_STATE.session,
-                        isBusy: true
-                    }
-                }
-            };
+      };
+    }
+  );
+  store.addReducer(
+    [AuthAction.logout],
+    (
+      state,
+      _action: ActionWithPayload<{ token: string }>
+    ) => {
+      return {
+        ...state,
+        auth: {
+          ...state.auth ?? INITIAL_AUTH_STATE,
+          session: {
+            ...state.auth?.session ?? INITIAL_AUTH_STATE.session,
+            isBusy: true
+          }
         }
-    );
-    store.addReducer(
-        [AuthAction.logoutSuccess]
-        , (
-            state,
-            _action: Action
-        ) => {
-            return {
-                ...store,
-                auth: {
-                    ...state.auth ?? INITIAL_AUTH_STATE,
-                    session: {
-                        ...state.auth?.session ?? INITIAL_AUTH_STATE.session,
-                        isBusy: false,
-                        isLoggedIn: false,
-                        token: undefined,
-                        tokenValidUntil: undefined
-                    }
-                }
-            };
+      };
+    }
+  );
+  store.addReducer(
+    [AuthAction.logoutSuccess]
+    , (
+      state,
+      _action: Action
+    ) => {
+      return {
+        ...store,
+        auth: {
+          ...state.auth ?? INITIAL_AUTH_STATE,
+          session: {
+            ...state.auth?.session ?? INITIAL_AUTH_STATE.session,
+            isBusy: false,
+            isLoggedIn: false,
+            token: GLOBAL_UNDEFINED,
+            tokenValidUntil: GLOBAL_UNDEFINED
+          }
         }
-    );
-    store.addReducer(
-        [AuthAction.loginFail],
-        (
-            state,
-            action: ActionWithPayload<Array<string>>
-        ) => {
-            return {
-                ...store,
-                auth: {
-                    ...state.auth ?? INITIAL_AUTH_STATE,
-                    session: {
-                        ...state.auth?.session ?? INITIAL_AUTH_STATE.session,
-                        isBusy: false,
-                        errors: action.payload
-                    }
-                }
-            };
+      };
+    }
+  );
+  store.addReducer(
+    [AuthAction.loginFail],
+    (
+      state,
+      action: ActionWithPayload<Array<string>>
+    ) => {
+      return {
+        ...store,
+        auth: {
+          ...state.auth ?? INITIAL_AUTH_STATE,
+          session: {
+            ...state.auth?.session ?? INITIAL_AUTH_STATE.session,
+            isBusy: false,
+            errors: action.payload
+          }
         }
-    );
-    store.addReducer(
-        [AuthAction.keepAlive],
-        (
-            state
-        ) => {
-            return {
-                ...state,
-                auth: {
-                    ...state.auth ?? INITIAL_AUTH_STATE,
-                    session: {
-                        ...state.auth?.session ?? INITIAL_AUTH_STATE.session,
-                        isBusy: true
-                    }
-                }
-            };
+      };
+    }
+  );
+  store.addReducer(
+    [AuthAction.keepAlive],
+    (
+      state
+    ) => {
+      return {
+        ...state,
+        auth: {
+          ...state.auth ?? INITIAL_AUTH_STATE,
+          session: {
+            ...state.auth?.session ?? INITIAL_AUTH_STATE.session,
+            isBusy: true
+          }
         }
-    );
-    store.addReducer(
-        [AuthAction.keepAliveFail],
-        (
-            state,
-            action: ActionWithPayload<Array<string>>
-        ) => {
-            return {
-                ...state,
-                auth: {
-                    ...state.auth ?? INITIAL_AUTH_STATE,
-                    session: {
-                        ...state.auth?.session ?? INITIAL_AUTH_STATE.session,
-                        isBusy: false,
-                        errors: action.payload
-                    }
-                }
-            };
+      };
+    }
+  );
+  store.addReducer(
+    [AuthAction.keepAliveFail],
+    (
+      state,
+      action: ActionWithPayload<Array<string>>
+    ) => {
+      return {
+        ...state,
+        auth: {
+          ...state.auth ?? INITIAL_AUTH_STATE,
+          session: {
+            ...state.auth?.session ?? INITIAL_AUTH_STATE.session,
+            isBusy: false,
+            errors: action.payload
+          }
         }
-    );
-    store.addReducer(
-        [AuthAction.keepAliveSuccess],
-        (
-            state,
-            action: ActionWithPayload<{ token: string, validUntil: Date }>
-        ) => {
-            return {
-                ...state,
-                auth: {
-                    ...state.auth ?? INITIAL_AUTH_STATE,
-                    session: {
-                        ...state.auth?.session ?? INITIAL_AUTH_STATE.session,
-                        isBusy: false,
-                        token: action.payload.token,
-                        tokenValidUntil: action.payload.validUntil
-                    }
-                }
-            };
+      };
+    }
+  );
+  store.addReducer(
+    [AuthAction.keepAliveSuccess],
+    (
+      state,
+      action: ActionWithPayload<{ token: string, validUntil: Date }>
+    ) => {
+      return {
+        ...state,
+        auth: {
+          ...state.auth ?? INITIAL_AUTH_STATE,
+          session: {
+            ...state.auth?.session ?? INITIAL_AUTH_STATE.session,
+            isBusy: false,
+            token: action.payload.token,
+            tokenValidUntil: action.payload.validUntil
+          }
         }
-    );
-    return store;
+      };
+    }
+  );
+  return store;
 }
