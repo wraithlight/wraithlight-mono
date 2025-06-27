dcb8fb9318b8421f8ee97304c4c38380c0478286
kfarkasHU <sohamar@farkask.eu>
2025-06-27 16:19:00 +0100

    feat(*): add kernel undefined lib
    Ref: #1376

---
+++ applications/packages/kernel/kernel.undefined/src/undefined/undefined.const.spec.ts
@@ -0,0 +1,11 @@
+import { GLOBAL_UNDEFINED } from "./undefined.const";
+
+describe("GLOBAL_UNDEFINEDSPecs", () => {
+    describe("given the const is initialized", () => {
+        it("should be undefined", () => {
+            expect(GLOBAL_UNDEFINED).toBeFalsy();
+            expect(GLOBAL_UNDEFINED).toBeUndefined();
+            expect(GLOBAL_UNDEFINED).toBe(undefined);
+        });
+    });
+});
