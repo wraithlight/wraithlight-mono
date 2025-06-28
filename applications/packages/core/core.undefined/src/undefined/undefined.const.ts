e687b2f8690b47305aca2876f44bfdb75fed9c48
kfarkasHU <sohamar@farkask.eu>
2025-06-28 15:39:17 +0200

    style(*): mark core undefined as deprecated
    Ref: #1376

--- applications/packages/core/core.undefined/src/undefined/undefined.const.ts
+++ applications/packages/core/core.undefined/src/undefined/undefined.const.ts
@@ -1,2 +1,5 @@
+/**
+ * @deprecated Import from `@wraithlight/kernel.undefined`.
+ */
 // eslint-disable-next-line no-undefined, @typescript-eslint/no-inferrable-types
 export const GLOBAL_UNDEFINED: undefined = undefined;
--- applications/packages/core/core.undefined/src/undefined/undefined.type.ts
+++ applications/packages/core/core.undefined/src/undefined/undefined.type.ts
@@ -1,3 +1,6 @@
 import { GLOBAL_UNDEFINED } from "./undefined.const";
 
+/**
+ * @deprecated Import from `@wraithlight/kernel.undefined`.
+ */
 export type T_GLOBAL_UNDEFINED = typeof GLOBAL_UNDEFINED;
