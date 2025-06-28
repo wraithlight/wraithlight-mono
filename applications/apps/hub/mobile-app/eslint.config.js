f35ef805b728d7459bb5466c140a82d67d468f4a
kfarkasHU <sohamar@farkask.eu>
2025-06-28 09:26:34 +0200

    chore(*): use plain eslint config object instead
    Ref: #840

--- applications/apps/hub/mobile-app/eslint.config.js
+++ applications/apps/hub/mobile-app/eslint.config.js
@@ -1,10 +1,7 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
 
-module.exports = defineConfig([
-  expoConfig,
-  {
-    ignores: ['dist/*'],
-  },
-]);
+module.exports = {
+  ...expoConfig,
+  ignores: ['dist/*'],
+};
