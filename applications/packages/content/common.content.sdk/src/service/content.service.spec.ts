538bc567e8132ee79bec245b016f062615cec719
kfarkasHU <sohamar@farkask.eu>
2025-07-01 16:19:00 +0100

    style(*): autoformat

--- applications/packages/content/common.content.sdk/src/service/content.service.spec.ts
+++ applications/packages/content/common.content.sdk/src/service/content.service.spec.ts
@@ -1,27 +1,27 @@
 const clientCtorSpy = jest.fn();
 
 jest.mock("../client", () => {
-    return {
-        ContentClient: clientCtorSpy
-    }
+  return {
+    ContentClient: clientCtorSpy
+  }
 });
 
 import { ContentService } from "./content.service";
 
 describe("ContentServiceSpecs", () => {
 
-    let service: ContentService;
-    const MOCK_BASE_URL = "base-url";
-    const MOCK_KEY = "key";
+  let service: ContentService;
+  const MOCK_BASE_URL = "base-url";
+  const MOCK_KEY = "key";
 
-    describe("given the service is initialized", () => {
+  describe("given the service is initialized", () => {
 
-        service = new ContentService(MOCK_BASE_URL, MOCK_KEY);
+    service = new ContentService(MOCK_BASE_URL, MOCK_KEY);
 
-        it("should create a client", () => {
-            expect(clientCtorSpy).toHaveBeenCalled();
-            expect(clientCtorSpy).toHaveBeenCalledTimes(1);
-            expect(clientCtorSpy).toHaveBeenCalledWith(MOCK_BASE_URL, MOCK_KEY);
-        });
+    it("should create a client", () => {
+      expect(clientCtorSpy).toHaveBeenCalled();
+      expect(clientCtorSpy).toHaveBeenCalledTimes(1);
+      expect(clientCtorSpy).toHaveBeenCalledWith(MOCK_BASE_URL, MOCK_KEY);
     });
+  });
 });
