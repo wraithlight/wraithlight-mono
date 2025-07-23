18c7e82ad8a13d05751c0e351e207f4114c2a2f2
kfarkasHU <sohamar@farkask.eu>
2025-07-23 18:02:00 +0200

    style(*): autoformat

--- applications/packages/core/mustache/core.mustache/src/mustache/mustache.fn.spec.ts
+++ applications/packages/core/mustache/core.mustache/src/mustache/mustache.fn.spec.ts
@@ -4,19 +4,19 @@ import { mustacheFacade } from "@wraithlight/facade.mustache";
 
 describe("wlMustacheSpecs", () => {
 
-    const MOCK_TEMPLATE = "template";
-    const MOCK_DATA = {};
+  const MOCK_TEMPLATE = "template";
+  const MOCK_DATA = {};
 
-    describe("given the utility is initialized", () => {
-        describe("when i call it", () => {
-            beforeEach(() => {
-                wlMustache(MOCK_TEMPLATE, MOCK_DATA);
-            });
-            it("should call the underlying service with the proper params", () => {
-                expect(mustacheFacade).toBeCalled();
-                expect(mustacheFacade).toBeCalledTimes(1);
-                expect(mustacheFacade).toBeCalledWith(MOCK_TEMPLATE, MOCK_DATA);
-            });
-        });
+  describe("given the utility is initialized", () => {
+    describe("when i call it", () => {
+      beforeEach(() => {
+        wlMustache(MOCK_TEMPLATE, MOCK_DATA);
+      });
+      it("should call the underlying service with the proper params", () => {
+        expect(mustacheFacade).toBeCalled();
+        expect(mustacheFacade).toBeCalledTimes(1);
+        expect(mustacheFacade).toBeCalledWith(MOCK_TEMPLATE, MOCK_DATA);
+      });
     });
+  });
 });
