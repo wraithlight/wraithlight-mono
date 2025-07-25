d6402eb8e90002c8ea08a5fa0c08846ea9394a31
kfarkasHU <sohamar@farkask.eu>
2025-07-25 22:03:00 +0200

    style(*): autoformat

--- applications/packages/core/core.middleware/src/middleware/bot-matcher/bot-matcher.middleware.ts
+++ applications/packages/core/core.middleware/src/middleware/bot-matcher/bot-matcher.middleware.ts
@@ -3,21 +3,21 @@ import { NextFunction, Request, Response } from "express";
 
 import { isBot } from "./_internal";
 import {
-    IS_BOT_HEADER_NAME,
-    USER_AGENT_HEADER_NAME
+  IS_BOT_HEADER_NAME,
+  USER_AGENT_HEADER_NAME
 } from "./bot-matcher.const";
 
 export function botMatcherMiddleware(
-    req: Request,
-    _res: Response,
-    next: NextFunction
+  req: Request,
+  _res: Response,
+  next: NextFunction
 ): void {
-    const userAgent = req.get(USER_AGENT_HEADER_NAME);
-    const isVisitorABot = hasNoUserAgent(userAgent) || isBot(userAgent ?? "");
-    req.headers[IS_BOT_HEADER_NAME] = isVisitorABot.toString();
-    next();
+  const userAgent = req.get(USER_AGENT_HEADER_NAME);
+  const isVisitorABot = hasNoUserAgent(userAgent) || isBot(userAgent ?? "");
+  req.headers[IS_BOT_HEADER_NAME] = isVisitorABot.toString();
+  next();
 }
 
 function hasNoUserAgent(userAgent: Nullable<string>): boolean {
-    return isNil(userAgent) || userAgent === "";
+  return isNil(userAgent) || userAgent === "";
 }
