ac95dd0b194991e3aa663a1f77c3eec5311d7267
kfarkasHU <sohamar@farkask.eu>
2025-07-25 05:47:00 +0200

    style(*): autoformat

--- applications/packages/core/core.primitive/src/primitive/primitive.fn.ts
+++ applications/packages/core/core.primitive/src/primitive/primitive.fn.ts
@@ -4,6 +4,6 @@ import { Primitive } from "./primitive.type";
  * @deprecated Use the same utility from `framework.primitive` instead.
  */
 export function isPrimitive(primitiveLike: any): primitiveLike is Primitive {
-    const primitiveTypes = ["boolean", "string", "number"];
-    return primitiveTypes.includes(typeof primitiveLike);
+  const primitiveTypes = ["boolean", "string", "number"];
+  return primitiveTypes.includes(typeof primitiveLike);
 }
