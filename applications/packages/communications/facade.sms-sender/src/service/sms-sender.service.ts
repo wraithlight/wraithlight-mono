1d3fd0b9d4f4351bd6d8fd011730fd98a31d2565
kfarkasHU <sohamar@farkask.eu>
2025-08-31 13:12:51 +0200

    feat(*): create facade smssender lib
    Ref: #1282

---
+++ applications/packages/communications/facade.sms-sender/src/service/sms-sender.service.ts
@@ -0,0 +1,15 @@
+import { OperationResultFactory, OperationResult } from "@wraithlight/framework.operation-result";
+
+import { ERROR_CODES } from "./sms-sender.const";
+
+export class SmsSenderFacadeService {
+
+  public async sendSms(
+    _recipientAddress: string,
+    _senderAddress: string,
+    _htmlContent: string
+  ): Promise<OperationResult<void>> {
+    return OperationResultFactory.error(ERROR_CODES.GENERAL_ERROR);
+  }
+
+}
