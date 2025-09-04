6e95f4af8ca626004d4c3b1114732466a8b5e697
kfarkasHU <sohamar@farkask.eu>
2025-09-04 03:12:00 +0200

    feat(non empty array): add non empty array

---
+++ applications/packages/framework/framework.type-utils/src/non-empty-array/non-empty-array.typeguard.spec.ts
@@ -0,0 +1,148 @@
+import { isNonEmptyArray } from "./non-empty-array.typeguard";
+
+describe("NonEmptyArrayTypeguardSpecs", () => {
+  describe("given the typeguard is initialized", () => {
+    describe("when i call it", () => {
+      describe("with an empty array", () => {
+        let result: boolean;
+        const MOCK: Array<unknown> = [];
+        beforeEach(() => {
+          result = isNonEmptyArray(MOCK);
+        });
+        it("should return false", () => {
+          expect(result).toBe(false);
+        });
+      });
+      describe("with a non-empty array", () => {
+        let result: boolean;
+        const MOCK: Array<unknown> = [1, 2, 3];
+        beforeEach(() => {
+          result = isNonEmptyArray(MOCK);
+        });
+        it("should return true", () => {
+          expect(result).toBe(true);
+        });
+      });
+      describe("with an empty string", () => {
+        let result: boolean;
+        const MOCK = "";
+        beforeEach(() => {
+          result = isNonEmptyArray(MOCK);
+        });
+        it("should return false", () => {
+          expect(result).toBe(false);
+        });
+      });
+      describe("with a non empty string", () => {
+        let result: boolean;
+        const MOCK = "hrvatska dusu ozdravi";
+        beforeEach(() => {
+          result = isNonEmptyArray(MOCK);
+        });
+        it("should return false", () => {
+          expect(result).toBe(false);
+        });
+      });
+      describe("with NaN", () => {
+        let result: boolean;
+        const MOCK = NaN;
+        beforeEach(() => {
+          result = isNonEmptyArray(MOCK);
+        });
+        it("should return false", () => {
+          expect(result).toBe(false);
+        });
+      });
+      describe("with -Infinity", () => {
+        let result: boolean;
+        const MOCK = -Infinity;
+        beforeEach(() => {
+          result = isNonEmptyArray(MOCK);
+        });
+        it("should return false", () => {
+          expect(result).toBe(false);
+        });
+      });
+      describe("with -1", () => {
+        let result: boolean;
+        const MOCK = -1;
+        beforeEach(() => {
+          result = isNonEmptyArray(MOCK);
+        });
+        it("should return false", () => {
+          expect(result).toBe(false);
+        });
+      });
+      describe("with zero", () => {
+        let result: boolean;
+        const MOCK = 0;
+        beforeEach(() => {
+          result = isNonEmptyArray(MOCK);
+        });
+        it("should return false", () => {
+          expect(result).toBe(false);
+        });
+      });
+      describe("with 1", () => {
+        let result: boolean;
+        const MOCK = 1;
+        beforeEach(() => {
+          result = isNonEmptyArray(MOCK);
+        });
+        it("should return false", () => {
+          expect(result).toBe(false);
+        });
+      });
+      describe("with +Infinity", () => {
+        let result: boolean;
+        const MOCK = Infinity;
+        beforeEach(() => {
+          result = isNonEmptyArray(MOCK);
+        });
+        it("should return false", () => {
+          expect(result).toBe(false);
+        });
+      });
+      describe("with false", () => {
+        let result: boolean;
+        const MOCK = false;
+        beforeEach(() => {
+          result = isNonEmptyArray(MOCK);
+        });
+        it("should return false", () => {
+          expect(result).toBe(false);
+        });
+      });
+      describe("with true", () => {
+        let result: boolean;
+        const MOCK = true;
+        beforeEach(() => {
+          result = isNonEmptyArray(MOCK);
+        });
+        it("should return false", () => {
+          expect(result).toBe(false);
+        });
+      });
+      describe("with a Date", () => {
+        let result: boolean;
+        const MOCK = new Date();
+        beforeEach(() => {
+          result = isNonEmptyArray(MOCK);
+        });
+        it("should return false", () => {
+          expect(result).toBe(false);
+        });
+      });
+      describe("with a {}", () => {
+        let result: boolean;
+        const MOCK = {};
+        beforeEach(() => {
+          result = isNonEmptyArray(MOCK);
+        });
+        it("should return false", () => {
+          expect(result).toBe(false);
+        });
+      });
+    });
+  });
+});
