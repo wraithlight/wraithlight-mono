e4629326661de42d8c5c548d99e1b3e4bf454b29
kfarkasHU <sohamar@farkask.eu>
2025-09-10 21:37:00 +0200

    style(non empty readonly array): align to linter

--- applications/packages/framework/framework.type-utils/src/non-empty-readonly-array/non-empty-readonly-array.type.ts
+++ applications/packages/framework/framework.type-utils/src/non-empty-readonly-array/non-empty-readonly-array.type.ts
@@ -1,1 +1,2 @@
-export type NonEmptyReadonlyArray<T> = ReadonlyArray<T> & [T, ...T[]];
+// eslint-disable-next-line max-len
+export type NonEmptyReadonlyArray<T> = ReadonlyArray<T> & [T, ...ReadonlyArray<T>];
