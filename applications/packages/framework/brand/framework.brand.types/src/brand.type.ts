82225b748cf8d141683b1928aeb8ed3348955e21
kfarkasHU <sohamar@farkask.eu>
2025-09-19 22:32:36 +0200

    chore(*): knip and sherif alignment
    Ref: #977

--- applications/packages/framework/brand/framework.brand.types/src/brand.type.ts
+++ applications/packages/framework/brand/framework.brand.types/src/brand.type.ts
@@ -1,3 +1,3 @@
-import { BRAND_PROPERTY_NAME } from "@wraithlight/framework.brand.types";
+import { BRAND_PROPERTY_NAME } from "@wraithlight/framework.brand.constants";
 
 export type WlBrand<TType, TKey extends string> = TType & { [BRAND_PROPERTY_NAME]: TKey };
