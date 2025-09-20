ef25941ee5878fedf16fd6e11e2859f0c57d38c5
kfarkasHU <sohamar@farkask.eu>
2025-09-21 00:14:30 +0200

    style(*): align to linter
    Ref: #1346

--- applications/packages/communications/common.communications.validators/src/validators.ts
+++ applications/packages/communications/common.communications.validators/src/validators.ts
@@ -3,7 +3,7 @@ import { SmsAdditionalPayloadSendRequestValidator } from "./sss";
 
 export namespace CommonValidators {
   // eslint-disable-next-line max-len
-  export const SendRequestPayloadValidator = <T extends object>() => new SendRequestValidator<T>();
+  export const SendRequestPayloadValidator = <T extends object>(): SendRequestValidator<T> => new SendRequestValidator<T>();
 }
 
 export namespace SSSValidators {
