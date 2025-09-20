e0a15693e39560158e637a80dbb5b05b10e7ac32
kfarkasHU <sohamar@farkask.eu>
2025-09-20 13:18:10 +0200

    style(*): align to linter
    Ref: #977

--- applications/packages/framework/brand/framework.brand.types/src/brand.type.ts
+++ applications/packages/framework/brand/framework.brand.types/src/brand.type.ts
@@ -1,6 +1,6 @@
 import { BRAND_PROPERTY_NAME } from "@wraithlight/framework.brand.constants";
 
-export type WlBrand<TType, TKey extends string> = TType & 
-{ 
+export type WlBrand<TType, TKey extends string> = TType &
+{
   [BRAND_PROPERTY_NAME]: TKey
 };
