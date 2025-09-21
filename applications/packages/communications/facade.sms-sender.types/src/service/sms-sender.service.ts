f20dd2fd8e9a290b15bcc2f5d30fe436b33c6a98
kfarkasHU <sohamar@farkask.eu>
2025-09-21 13:30:57 +0200

    refactor(*): align to BL manager
    Ref: #1325

--- applications/packages/communications/facade.sms-sender.types/src/service/sms-sender.service.ts
+++ applications/packages/communications/facade.sms-sender.types/src/service/sms-sender.service.ts
@@ -1,13 +1,18 @@
 import { OperationResult } from "@wraithlight/framework.operation-result";
 
+import { ISMSSenderConfig } from "../config";
+
 import { SmsSenderResponse } from "./sms-sender.model";
 
-export interface ISmsSenderFacadeService<T> {
+export interface ISmsSenderFacadeService<T extends ISMSSenderConfig> {
 
   initialize(config: T): OperationResult<void>;
 
   sendSms(
     recipientPhoneNumber: string,
+    /**
+     * @deprecated - Will be handled internally
+     */
     senderPhoneNumber: string,
     content: string,
   ): Promise<OperationResult<SmsSenderResponse>>;
