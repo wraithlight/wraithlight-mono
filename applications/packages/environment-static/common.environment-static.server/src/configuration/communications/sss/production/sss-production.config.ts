3da38bf161172824c9441da19d2e4ff73b64a97e
kfarkasHU <sohamar@farkask.eu>
2025-09-21 15:03:36 +0200

    feat(sss): add allowed nps tokens
    Ref: #1346

--- applications/packages/environment-static/common.environment-static.server/src/configuration/communications/sss/production/sss-production.config.ts
+++ applications/packages/environment-static/common.environment-static.server/src/configuration/communications/sss/production/sss-production.config.ts
@@ -7,5 +7,8 @@ export const SERVER_PRODUCTION_COMMS_SSS_CONFIG: Readonly<CommsSSSServer> = {
     username: "",
     password: "",
     database: ""
+  },
+  apiTokens: {
+    npsTokens: []
   }
 };
