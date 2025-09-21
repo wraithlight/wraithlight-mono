04a3628ea7dc3b105ef60c124adad0cd112e32be
kfarkasHU <sohamar@farkask.eu>
2025-09-21 14:30:39 +0200

    feat(data): add `Provider` initial data
    Ref: #1532

---
+++ database/communications-sss/src/data/provider.sql
@@ -0,0 +1,12 @@
+-- DUMMY
+INSERT INTO `Provider` (
+    `Id`,
+    `Label`,
+    `Config`,
+    `IsActive`
+) VALUES (
+    "a9c31d83-2f3c-4aec-841d-6b27967367cf",
+    "Dummy SMS Provider",
+    "{}",
+    0
+);
