5234123b99eb188d1e6795cf5c4d26ecd7f48492
kfarkasHU <sohamar@farkask.eu>
2025-09-23 20:47:22 +0200

    feat(headers): add `WithValidApiToken` guard
    Ref: #1537

---
+++ applications/packages/common/common.node.evo-utils/src/headers/with-valid-api-token.header.ts
@@ -0,0 +1,21 @@
+import { HttpCode } from "@wraithlight/core.http";
+import {
+  createFailResponse,
+  createFilterAttribute,
+  createSuccessResponse
+} from "@wraithlight/core.node.evo";
+import { HeaderName } from "@wraithlight/domain.http.constants";
+import { Request } from "express";
+
+export const WithValidApiToken = (tokens: ReadonlyArray<string>) => createFilterAttribute(
+  (req: Request) => {
+    const token = req.headers[HeaderName.ApiToken];
+    if (typeof token !== "string") {
+      return createFailResponse(HttpCode.BadRequest, "E_APITOKEN_TYPE");
+    }
+    if (!tokens.includes(token)) {
+      return createFailResponse(HttpCode.Unauthorized, "E_APITOKEN");
+    }
+    return createSuccessResponse();
+  }
+);
