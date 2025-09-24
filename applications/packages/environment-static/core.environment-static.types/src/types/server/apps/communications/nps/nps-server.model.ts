fa2c24a47e9f5fed047b06a7f10ce6ef04d8aea3
kfarkasHU <sohamar@farkask.eu>
2025-09-24 06:58:00 +0200

    feat(nps): add apitokens property
    Ref: #1344

--- applications/packages/environment-static/core.environment-static.types/src/types/server/apps/communications/nps/nps-server.model.ts
+++ applications/packages/environment-static/core.environment-static.types/src/types/server/apps/communications/nps/nps-server.model.ts
@@ -2,4 +2,9 @@ import { DatabaseServer } from "../../_internal";
 
 export interface CommsNPSServer {
   database: DatabaseServer;
+  apiTokens: {
+    emailSender: string,
+    pushSender: string,
+    smsSender: string
+  }
 }
