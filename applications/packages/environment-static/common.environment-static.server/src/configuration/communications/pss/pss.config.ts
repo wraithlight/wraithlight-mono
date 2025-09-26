9333b86ff0502fc1ff01c3c75f84ed3f0a80206c
kfarkasHU <sohamar@farkask.eu>
2025-09-27 00:51:16 +0200

    fix(pss): config type
    Ref: #1344

--- applications/packages/environment-static/common.environment-static.server/src/configuration/communications/pss/pss.config.ts
+++ applications/packages/environment-static/common.environment-static.server/src/configuration/communications/pss/pss.config.ts
@@ -1,5 +1,5 @@
 import { EnvironmentType } from "@wraithlight/core.env.types";
-import { CommsNPSServer, EnvironmentStatic } from "@wraithlight/core.environment-static.types";
+import { CommsPSSServer, EnvironmentStatic } from "@wraithlight/core.environment-static.types";
 
 import { SERVER_DEV_COMMS_PSS_CONFIG } from "./dev";
 import { SERVER_LOCAL_COMMS_PSS_CONFIG } from "./local";
@@ -8,7 +8,7 @@ import { SERVER_STAGING_COMMS_PSS_CONFIG } from "./staging";
 import { SERVER_TEST_COMMS_PSS_CONFIG } from "./test";
 
 export const SERVER_COMMS_PSS_CONFIG: Readonly<
-    EnvironmentStatic<CommsNPSServer>
+    EnvironmentStatic<CommsPSSServer>
     > = {
     [EnvironmentType.Local]: SERVER_LOCAL_COMMS_PSS_CONFIG,
     [EnvironmentType.Dev]: SERVER_DEV_COMMS_PSS_CONFIG,
