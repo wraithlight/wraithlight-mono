31b791e5073072f5760a1f6ab507a898b24f6034
kfarkasHU <sohamar@farkask.eu>
2025-09-27 12:18:06 +0200

    refactor(noop): move code to its folder and align specfile
    Ref: #1541

--- applications/packages/framework/framework.noop/src/noop/noop.spec.ts
+++ applications/packages/framework/framework.noop/src/noop/noop.spec.ts
@@ -4,7 +4,7 @@ describe("NoopSpecs", () => {
   describe("given the utility is initialized", () => {
     describe("when i call it", () => {
       it("should not to throw an error", () => {
-        expect(noop()).not.toThrow();
+        expect(() => noop()).not.toThrow();
       });
       it("should return undefined", () => {
       expect(noop()).toBeUndefined();
