851cddf0073198f4b41acd1f9bf74e55e6f0d88c
kfarkasHU <sohamar@farkask.eu>
2025-09-27 12:04:02 +0200

    test(*): noop spec
    Ref: #1541

---
+++ applications/packages/framework/framework.noop/src/noop.spec.ts
@@ -0,0 +1,14 @@
+import { noop } from "./noop";
+
+describe("NoopSpecs", () => {
+  describe("given the utility is initialized", () => {
+    describe("when i call it", () => {
+      it("should not to throw an error", () => {
+        expect(noop()).not.toThrow();
+      });
+      it("should return undefined", () => {
+      expect(noop()).toBeUndefined();
+      });
+    });
+  });
+});
