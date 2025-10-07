59d57734dcad271ce2f80c069acf3cc1ada839ff
kfarkasHU <sohamar@farkask.eu>
2025-10-07 18:13:00 +0100

    style(*): autoformat

--- applications/packages/auth/common.auth-sdk.client/src/sdk/account/account.effect.ts
+++ applications/packages/auth/common.auth-sdk.client/src/sdk/account/account.effect.ts
@@ -9,32 +9,32 @@ import { AccountService } from "./account.service";
 import { RegisterModel } from "./model";
 
 export function initializeEffects(
-    store: Store<IAuthContainerStore>,
-    apiBaseUrl: string
+  store: Store<IAuthContainerStore>,
+  apiBaseUrl: string
 ): Store<IAuthContainerStore> {
 
-    const service = new AccountService(apiBaseUrl);
-    const logger = LoggerService.getInstance();
+  const service = new AccountService(apiBaseUrl);
+  const logger = LoggerService.getInstance();
 
-    store.addEffect(
-        [AccountAction.register],
-        (
-            action: ActionWithPayload<RegisterModel>
-        ) => {
-            service
-                .register(action.payload)
-                .then(m => {
-                    const action = m.success
-                        ? AccountAction.registerSuccess()
-                        : AccountAction.registerFail(m.errors ?? []);
-                    store.dispatch(action);
-                })
-                .catch(m => {
-                    logger.warn(m);
-                    store.dispatch(AccountAction.registerFail([UNKNOWN_ERROR]));
-                })
-            ;
-        }
-    );
-    return store;
+  store.addEffect(
+    [AccountAction.register],
+    (
+      action: ActionWithPayload<RegisterModel>
+    ) => {
+      service
+        .register(action.payload)
+        .then(m => {
+          const action = m.success
+            ? AccountAction.registerSuccess()
+            : AccountAction.registerFail(m.errors ?? []);
+          store.dispatch(action);
+        })
+        .catch(m => {
+          logger.warn(m);
+          store.dispatch(AccountAction.registerFail([UNKNOWN_ERROR]));
+        })
+        ;
+    }
+  );
+  return store;
 }
--- applications/packages/auth/common.auth-sdk.client/src/sdk/account/model/register.model.ts
+++ applications/packages/auth/common.auth-sdk.client/src/sdk/account/model/register.model.ts
@@ -1,6 +1,6 @@
 export interface RegisterModel {
-    username: string;
-    password: string;
-    passwordVerify: string;
-    emailAddress: string;
+  username: string;
+  password: string;
+  passwordVerify: string;
+  emailAddress: string;
 }
