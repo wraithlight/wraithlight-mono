576c0043cad90594e3c17552e365f79c3a68e2fe
kfarkasHU <sohamar@farkask.eu>
2025-10-08 20:23:00 +0100

    style(*): autoformat

--- applications/packages/auth/core.auth.constant/src/constants/application/application-name.const.ts
+++ applications/packages/auth/core.auth.constant/src/constants/application/application-name.const.ts
@@ -2,18 +2,18 @@
  * @deprecated Import it from `domain.application-info.constants`
  */
 export enum ApplicationName {
-    Website = "WEBSITE",
-    GameWebsite = "GAME_WEBSITE",
-    GameApplication = "GAME_APPLICATION",
-    Content = "CONTENT",
-    UserManagement = "USER_MANAGEMENT",
-    Forum = "FORUM",
-    Logs = "LOGS",
-    Editor = "EDITOR",
-    Notifier = "NOTIFIER",
-    RemoteConfig = "REMOTE_CONFIG",
-    CommsNPS = "COMMUNICATIONS_NPS",
-    CommsSSS = "COMMUNICATIONS_SSS",
-    CommsESS = "COMMUNICATIONS_ESS",
-    CommsPSS = "COMMUNICATIONS_PSS"
+  Website = "WEBSITE",
+  GameWebsite = "GAME_WEBSITE",
+  GameApplication = "GAME_APPLICATION",
+  Content = "CONTENT",
+  UserManagement = "USER_MANAGEMENT",
+  Forum = "FORUM",
+  Logs = "LOGS",
+  Editor = "EDITOR",
+  Notifier = "NOTIFIER",
+  RemoteConfig = "REMOTE_CONFIG",
+  CommsNPS = "COMMUNICATIONS_NPS",
+  CommsSSS = "COMMUNICATIONS_SSS",
+  CommsESS = "COMMUNICATIONS_ESS",
+  CommsPSS = "COMMUNICATIONS_PSS"
 }
