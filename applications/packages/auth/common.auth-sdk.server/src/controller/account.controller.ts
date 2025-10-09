5762dc4b2c296375118e748c63b5a999b5e2aaf6
kfarkasHU <sohamar@farkask.eu>
2025-10-09 05:13:00 +0100

    style(*): autoformat

--- applications/packages/auth/common.auth-sdk.server/src/controller/account.controller.ts
+++ applications/packages/auth/common.auth-sdk.server/src/controller/account.controller.ts
@@ -1,14 +1,14 @@
 import { API_ROUTES } from "@wraithlight/core.auth.constant";
 import {
-    ApiRegisterErrorResponse,
-    RegisterErrorResponse,
-    RegisterRequest,
-    RegisterSuccessResponse
+  ApiRegisterErrorResponse,
+  RegisterErrorResponse,
+  RegisterRequest,
+  RegisterSuccessResponse
 } from "@wraithlight/core.auth.types";
 import {
-    BaseController,
-    HttpController,
-    HttpPost
+  BaseController,
+  HttpController,
+  HttpPost
 } from "@wraithlight/core.node";
 import { isNil } from "@wraithlight/core.nullable";
 
@@ -17,30 +17,30 @@ import { ServerAccountService } from "../service";
 @HttpController(API_ROUTES.v1.account.root)
 export class ServerAccountControllerV1 extends BaseController {
 
-    private readonly _accountService = new ServerAccountService();
+  private readonly _accountService = new ServerAccountService();
 
-    @HttpPost(API_ROUTES.v1.account.register)
-    public async register(model: RegisterRequest): Promise<void> {
-        const result = await this._accountService.register(
-            model.username,
-            model.password,
-            model.passwordVerify,
-            model.emailAddress
-        );
-        if (isNil(result) || !result.success) {
-            const data: RegisterErrorResponse = {
-                success: false,
-                // TODO: OperationResult
-                // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-                errors: (result as ApiRegisterErrorResponse).errors
-            };
-            return super.badRequest(data);
-        }
-
-        const data: RegisterSuccessResponse = {
-            success: true
-        };
-        return super.ok(data);
+  @HttpPost(API_ROUTES.v1.account.register)
+  public async register(model: RegisterRequest): Promise<void> {
+    const result = await this._accountService.register(
+      model.username,
+      model.password,
+      model.passwordVerify,
+      model.emailAddress
+    );
+    if (isNil(result) || !result.success) {
+      const data: RegisterErrorResponse = {
+        success: false,
+        // TODO: OperationResult
+        // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+        errors: (result as ApiRegisterErrorResponse).errors
+      };
+      return super.badRequest(data);
     }
+
+    const data: RegisterSuccessResponse = {
+      success: true
+    };
+    return super.ok(data);
+  }
 
 }
--- applications/packages/auth/common.auth-sdk.server/src/service/auth/auth.config.ts
+++ applications/packages/auth/common.auth-sdk.server/src/service/auth/auth.config.ts
@@ -5,49 +5,49 @@ import { createUrl } from "@wraithlight/core.url";
 
 export class ServerAuthServiceConfig {
 
-    private readonly _reader = SharedUserManagementConfigReader
-        .getInstance(CoreEnvironment.getEnvironmentType());
+  private readonly _reader = SharedUserManagementConfigReader
+    .getInstance(CoreEnvironment.getEnvironmentType());
 
-    public getLoginUrl(): string {
-        return this.concatSegments(
-            this.getApiUrl(),
-            API_ENDPOINTS.external.v2.auth.root,
-            API_ENDPOINTS.external.v2.auth.login
-        );
-    }
+  public getLoginUrl(): string {
+    return this.concatSegments(
+      this.getApiUrl(),
+      API_ENDPOINTS.external.v2.auth.root,
+      API_ENDPOINTS.external.v2.auth.login
+    );
+  }
 
-    public getLogoutUrl(): string {
-        return this.concatSegments(
-            this.getApiUrl(),
-            API_ENDPOINTS.external.v2.auth.logout
-        );
-    }
+  public getLogoutUrl(): string {
+    return this.concatSegments(
+      this.getApiUrl(),
+      API_ENDPOINTS.external.v2.auth.logout
+    );
+  }
 
-    public getValidateSessionUrl(): string {
-        return this.concatSegments(
-            this.getApiUrl(),
-            API_ENDPOINTS.external.v2.auth.validateSession
-        );
-    }
+  public getValidateSessionUrl(): string {
+    return this.concatSegments(
+      this.getApiUrl(),
+      API_ENDPOINTS.external.v2.auth.validateSession
+    );
+  }
 
-    public getKeepAliveSessionUrl(): string {
-        return this.concatSegments(
-            this.getApiUrl(),
-            API_ENDPOINTS.external.v2.auth.keepAliveSession
-        );
-    }
+  public getKeepAliveSessionUrl(): string {
+    return this.concatSegments(
+      this.getApiUrl(),
+      API_ENDPOINTS.external.v2.auth.keepAliveSession
+    );
+  }
 
-    private getApiUrl(): string {
-        const host = this._reader.get(x => x.server.baseUrl);
-        const port = this._reader.get(x => x.server.port);
-        return createUrl(
-            host,
-            port
-        );
-    }
+  private getApiUrl(): string {
+    const host = this._reader.get(x => x.server.baseUrl);
+    const port = this._reader.get(x => x.server.port);
+    return createUrl(
+      host,
+      port
+    );
+  }
 
-    private concatSegments(...segments: Array<string>): string {
-        return segments.join("");
-    }
+  private concatSegments(...segments: Array<string>): string {
+    return segments.join("");
+  }
 
 }
