ef42b0e0af2b04f5e106bbe3c31a9c38d4ba91f9
kfarkasHU <sohamar@farkask.eu>
2025-10-10 07:49:00 +0100

    style(*): autoformat

--- applications/packages/auth/core.auth.constant/src/constants/api-routes.const.ts
+++ applications/packages/auth/core.auth.constant/src/constants/api-routes.const.ts
@@ -7,18 +7,18 @@ const V1_AUTH_ROOT = `${API_BASE}${V1_BASE}${AUTH_BASE}`;
 const V1_ACCOUNT_ROOT = `${API_BASE}${V1_BASE}${ACCOUNT_BASE}`;
 
 export const API_ROUTES = Object.freeze({
-    v1: {
-        auth: {
-            root: V1_AUTH_ROOT,
-            login: "/login",
-            logout: "/logout",
-            validateSession: "/validate-session",
-            keepAlive: "/keep-alive",
-            register: "/register"
-        },
-        account: {
-            root: V1_ACCOUNT_ROOT,
-            register: "/register"
-        }
+  v1: {
+    auth: {
+      root: V1_AUTH_ROOT,
+      login: "/login",
+      logout: "/logout",
+      validateSession: "/validate-session",
+      keepAlive: "/keep-alive",
+      register: "/register"
+    },
+    account: {
+      root: V1_ACCOUNT_ROOT,
+      register: "/register"
     }
+  }
 });
