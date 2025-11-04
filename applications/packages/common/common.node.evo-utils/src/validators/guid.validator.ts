748f88615093d874d0acecd26225f1783dc2721e
kfarkasHU <sohamar@farkask.eu>
2025-11-04 07:28:30 +0100

    feat(validators): add validator for Guid
    Ref: #1349

---
+++ applications/packages/common/common.node.evo-utils/src/validators/guid.validator.ts
@@ -0,0 +1,46 @@
+import { Guid, isGuid } from "@wraithlight/core.guid";
+import { IValidator, ValidationResult } from "@wraithlight/core.validator";
+
+export class GuidValidator implements IValidator<Guid> {
+
+  validate(object: unknown): ValidationResult {
+    if (typeof object !== "string") {
+      return {
+        success: false,
+        errorList: [
+          {
+            propertyName: "object",
+            errors: ["E_GUID_TYPE"]
+          }
+        ],
+        ruleResults: [{
+          success: false,
+          propertyName: "object"
+        }]
+      }
+    }
+    const result = isGuid(object);
+    if (!result) {
+      return {
+        success: false,
+        errorList: [
+          {
+            propertyName: "object",
+            errors: ["E_NOT_GUID"]
+          }
+        ],
+        ruleResults: [{
+          success: false,
+          propertyName: "object"
+        }]
+      }
+    }
+
+    return {
+      success: true,
+      errorList: [],
+      ruleResults: []
+    }
+  }
+
+}
