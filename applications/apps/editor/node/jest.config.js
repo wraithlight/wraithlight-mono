ab61bfc593c1d8c41e19d32329684895805ccb27
kfarkasHU <sohamar@farkask.eu>
2025-11-11 17:46:00 +0100

    style(*): autoformat

--- applications/apps/editor/node/jest.config.js
+++ applications/apps/editor/node/jest.config.js
@@ -2,17 +2,17 @@ const { jestConfig } = require("@wraithlight/tools.jest.base");
 const { join } = require("path");
 
 module.exports = jestConfig({
-    preset: "ts-jest",
-    displayName: "Apps/Editor/Node",
-    testMatch: [
-      "<rootDir>/src/**/*.spec.ts"
-    ],
-    testPathIgnorePatterns: [
-      "dist"
-    ],
-    coveragePathIgnorePatterns: [
-      "dist"
-    ]
-  },
+  preset: "ts-jest",
+  displayName: "Apps/Editor/Node",
+  testMatch: [
+    "<rootDir>/src/**/*.spec.ts"
+  ],
+  testPathIgnorePatterns: [
+    "dist"
+  ],
+  coveragePathIgnorePatterns: [
+    "dist"
+  ]
+},
   join(__dirname, "../../../")
 );
