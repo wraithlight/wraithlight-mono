d4f5d51069a68b4bc233118450a5c328f4decb11
kfarkasHU <sohamar@farkask.eu>
2025-11-16 14:24:38 +0100

    feat(theme): add domain.ui.tenant-themes lib
    Ref: #1576

---
+++ applications/packages/domain/domain.ui.tenant-themes/src/tenants/index.ts
@@ -0,0 +1,7 @@
+export { tenantTheme as contentTheme } from "./content";
+export { tenantTheme as gameWebsiteTheme } from "./game-website";
+export { tenantTheme as notifierTheme } from "./notifier";
+export { tenantTheme as npsTheme } from "./nps";
+export { tenantTheme as userManagementTheme } from "./user-management";
+export { tenantTheme as websiteTheme } from "./website";
+
---
+++ applications/packages/domain/domain.ui.tenant-themes/src/tenants/notifier/theme.ts
@@ -0,0 +1,23 @@
+import {
+  TenantTheme,
+  Theme
+} from "@wraithlight/core.ui.types";
+
+const lightMode: Theme = {
+  colors: {
+    primary: "",
+    onPrimary: ""
+  }
+};
+
+const darkMode: Theme = {
+  colors: {
+    primary: "",
+    onPrimary: ""
+  }
+};
+
+export const tenantTheme: TenantTheme = {
+  lightMode: lightMode,
+  darkMode: darkMode
+};
