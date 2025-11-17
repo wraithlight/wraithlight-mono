044b7d31a3472fe81bd71576b81fa7983fa8a8b6
kfarkasHU <sohamar@farkask.eu>
2025-11-17 19:44:47 +0100

    feat(*): add website theme lib
    Ref: #1576

---
+++ applications/packages/ui/core.ui.website.theme/jest.config.js
@@ -0,0 +1,19 @@
+const { jestConfig } = require("@wraithlight/tools.jest.base");
+const { join } = require("path");
+
+module.exports = jestConfig(
+  {
+    preset: "ts-jest",
+    displayName: "Packages/UI/Core/Website Theme",
+    testMatch: [
+      "<rootDir>/src/**/*.spec.ts"
+    ],
+    testPathIgnorePatterns: [
+      "dist"
+    ],
+    coveragePathIgnorePatterns: [
+      "dist"
+    ]
+  },
+  join(__dirname, "../../..")
+);
