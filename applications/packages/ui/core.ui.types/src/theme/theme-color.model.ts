7a33e39654742cca578ec1034242a8cb70440387
kfarkasHU <sohamar@farkask.eu>
2025-11-20 19:37:06 +0100

    feat(theme): add function colors
    Ref: #1587

--- applications/packages/ui/core.ui.types/src/theme/theme-color.model.ts
+++ applications/packages/ui/core.ui.types/src/theme/theme-color.model.ts
@@ -3,4 +3,12 @@ import { Color } from "./color.type";
 export interface ThemeColors {
   primary: Color;
   onPrimary: Color;
+  success: Color;
+  onSuccess: Color;
+  warning: Color;
+  onWarning: Color;
+  danger: Color;
+  onDanger: Color;
+  info: Color;
+  onInfo: Color;
 }
