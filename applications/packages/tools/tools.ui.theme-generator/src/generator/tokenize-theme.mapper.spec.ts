36475c4e6585edbc1eaf0e1a2c5f9c92e33c3f04
kfarkasHU <sohamar@farkask.eu>
2025-11-20 19:49:42 +0100

    test(generator): add function colors
    Ref: #1587

--- applications/packages/tools/tools.ui.theme-generator/src/generator/tokenize-theme.mapper.spec.ts
+++ applications/packages/tools/tools.ui.theme-generator/src/generator/tokenize-theme.mapper.spec.ts
@@ -6,7 +6,15 @@ import { tokenizeTheme } from "./tokenize-theme.mapper";
 const MOCK_THEME: Theme = {
   colors: {
     primary: "#FFFFFF",
-    onPrimary: "#000000"
+    onPrimary: "#000000",
+    success: "#111111",
+    onSuccess: "#222222",
+    warning: "#333333",
+    onWarning: "#444444",
+    danger: "#555555",
+    onDanger: "#666666",
+    info: "#777777",
+    onInfo: "#888888"
   }
 };
 
@@ -19,6 +27,38 @@ const MOCK_TOKENIZED_THEME: TokenizedTheme = {
     onPrimary: {
       value: "#000000",
       token: "wl-colors-on-primary"
+    },
+    success: {
+      value: "#111111",
+      token: "wl-colors-success"
+    },
+    onSuccess: {
+      value: "#222222",
+      token: "wl-colors-on-success"
+    },
+    warning: {
+      value: "#333333",
+      token: "wl-colors-warning"
+    },
+    onWarning: {
+      value: "#444444",
+      token: "wl-colors-on-warning"
+    },
+    danger: {
+      value: "#555555",
+      token: "wl-colors-danger"
+    },
+    onDanger: {
+      value: "#666666",
+      token: "wl-colors-on-danger"
+    },
+    info: {
+      value: "#777777",
+      token: "wl-colors-info"
+    },
+    onInfo: {
+      value: "#888888",
+      token: "wl-colors-on-info"
     }
   }
 };
