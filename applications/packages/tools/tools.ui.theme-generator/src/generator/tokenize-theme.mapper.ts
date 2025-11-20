35843aaffa43f0fce277d19bf628aebdddd0bde9
kfarkasHU <sohamar@farkask.eu>
2025-11-20 19:37:26 +0100

    feat(generator): add function colors
    Ref: #1587

--- applications/packages/tools/tools.ui.theme-generator/src/generator/tokenize-theme.mapper.ts
+++ applications/packages/tools/tools.ui.theme-generator/src/generator/tokenize-theme.mapper.ts
@@ -12,6 +12,38 @@ export const tokenizeTheme = (theme: Theme): TokenizedTheme => {
       onPrimary: {
         value: theme.colors.onPrimary,
         token: "wl-colors-on-primary"
+      },
+      success: {
+        value: theme.colors.success,
+        token: "wl-colors-success"
+      },
+      onSuccess: {
+        value: theme.colors.onSuccess,
+        token: "wl-colors-on-success"
+      },
+      warning: {
+        value: theme.colors.warning,
+        token: "wl-colors-warning"
+      },
+      onWarning: {
+        value: theme.colors.onWarning,
+        token: "wl-colors-on-warning"
+      },
+      danger: {
+        value: theme.colors.danger,
+        token: "wl-colors-danger"
+      },
+      onDanger: {
+        value: theme.colors.onDanger,
+        token: "wl-colors-on-danger"
+      },
+      info: {
+        value: theme.colors.info,
+        token: "wl-colors-info"
+      },
+      onInfo: {
+        value: theme.colors.onInfo,
+        token: "wl-colors-on-info"
       }
     }
   };
