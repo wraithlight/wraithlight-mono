bd84f21f1b1064736fda4935b01532f08fe0bb8d
kfarkasHU <sohamar@farkask.eu>
2025-11-22 01:26:16 +0100

    feat(*): add providers table
    Ref: #1532

---
+++ database/communications-pss/src/data/provider.sql
@@ -0,0 +1,12 @@
+-- DUMMY
+INSERT INTO `Provider` (
+    `Id`,
+    `Label`,
+    `Config`,
+    `IsActive`
+) VALUES (
+    "7529aca0-2e55-1cff-bbd4-6cb8a3fc1f89",
+    "Dummy Push Provider",
+    "{}",
+    0
+);
