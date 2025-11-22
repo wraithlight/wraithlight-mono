2d994073759652bbf766979de8ee93b485f43853
kfarkasHU <sohamar@farkask.eu>
2025-11-23 00:51:26 +0100

    feat(generator): add support for surface colors
    Ref: #1586

--- applications/packages/tools/tools.ui.theme-generator/src/generator/tokenize-theme.mapper.spec.ts
+++ applications/packages/tools/tools.ui.theme-generator/src/generator/tokenize-theme.mapper.spec.ts
@@ -5,60 +5,110 @@ import { tokenizeTheme } from "./tokenize-theme.mapper";
 
 const MOCK_THEME: Theme = {
   colors: {
-    primary: "#FFFFFF",
-    onPrimary: "#000000",
-    success: "#111111",
-    onSuccess: "#222222",
-    warning: "#333333",
-    onWarning: "#444444",
-    danger: "#555555",
-    onDanger: "#666666",
-    info: "#777777",
-    onInfo: "#888888"
+    primary: "#000001",
+    onPrimary: "#000002",
+    success: "#000003",
+    onSuccess: "#000004",
+    warning: "#000005",
+    onWarning: "#000006",
+    danger: "#000007",
+    onDanger: "#000008",
+    info: "#000009",
+    onInfo: "#00000A",
+    surface1: "#00000B",
+    onSurface1: "#00000C",
+    surface2: "#00000D",
+    onSurface2: "#00000E",
+    surface3: "#00000F",
+    onSurface3: "#000010",
+    surface4: "#000011",
+    onSurface4: "#000012",
+    surface5: "#000013",
+    onSurface5: "#000014",
   }
 };
 
 const MOCK_TOKENIZED_THEME: TokenizedTheme = {
   colors: {
     primary: {
-      value: "#FFFFFF",
+      value: "#000001",
       token: "wl-colors-primary"
     },
     onPrimary: {
-      value: "#000000",
+      value: "#000002",
       token: "wl-colors-on-primary"
     },
     success: {
-      value: "#111111",
+      value: "#000003",
       token: "wl-colors-success"
     },
     onSuccess: {
-      value: "#222222",
+      value: "#000004",
       token: "wl-colors-on-success"
     },
     warning: {
-      value: "#333333",
+      value: "#000005",
       token: "wl-colors-warning"
     },
     onWarning: {
-      value: "#444444",
+      value: "#000006",
       token: "wl-colors-on-warning"
     },
     danger: {
-      value: "#555555",
+      value: "#000007",
       token: "wl-colors-danger"
     },
     onDanger: {
-      value: "#666666",
+      value: "#000008",
       token: "wl-colors-on-danger"
     },
     info: {
-      value: "#777777",
+      value: "#000009",
       token: "wl-colors-info"
     },
     onInfo: {
-      value: "#888888",
+      value: "#00000A",
       token: "wl-colors-on-info"
+    },
+    surface1: {
+      value: "#00000B",
+      token: "wl-colors-surface-1"
+    },
+    onSurface1: {
+      value: "#00000C",
+      token: "wl-colors-on-surface-1"
+    },
+    surface2: {
+      value: "#00000D",
+      token: "wl-colors-surface-2"
+    },
+    onSurface2: {
+      value: "#00000E",
+      token: "wl-colors-on-surface-2"
+    },
+    surface3: {
+      value: "#00000F",
+      token: "wl-colors-surface-3"
+    },
+    onSurface3: {
+      value: "#000010",
+      token: "wl-colors-on-surface-3"
+    },
+    surface4: {
+      value: "#000011",
+      token: "wl-colors-surface-4"
+    },
+    onSurface4: {
+      value: "#000012",
+      token: "wl-colors-on-surface-4"
+    },
+    surface5: {
+      value: "#000013",
+      token: "wl-colors-surface-5"
+    },
+    onSurface5: {
+      value: "#000014",
+      token: "wl-colors-on-surface-5"
     }
   }
 };
