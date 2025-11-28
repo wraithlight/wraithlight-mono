7e6ca8b2894482fcc5e71585e78fe1902ba3b1fa
kfarkasHU <sohamar@farkask.eu>
2025-11-28 21:49:00 +0100

    feat(*): add Provider feature
    Ref: #1602

---
+++ applications/packages/communications/common.communications.sms-sender.dal/src/service/provider/provider.service.ts
@@ -0,0 +1,57 @@
+import { Guid } from "@wraithlight/core.guid";
+import { isNil } from "@wraithlight/framework.nullable";
+import {
+  OperationResult,
+  OperationResultFactory
+} from "@wraithlight/framework.operation-result";
+
+import {
+  SMSSenderDbContextFactory,
+  ProviderDbo
+} from "../../db-context";
+
+import { ERROR_CODES } from "./provider.const";
+
+export class ProviderService {
+
+  private readonly _context = SMSSenderDbContextFactory
+    .getInstance()
+    .getDbContext()
+  ;
+
+  public async create(
+    id: Guid,
+    label: string,
+    config: string,
+    isActive: boolean
+  ): Promise<OperationResult<ProviderDbo>> {
+    const model: ProviderDbo = {
+      id: id,
+      label: label,
+      config: config,
+      isActive: isActive
+    };
+    await this._context.Providers
+      .insert(model)
+      .run()
+    ;
+    return this.getByIdCore(id);
+  }
+
+  private async getByIdCore(
+    id: Guid
+  ): Promise<OperationResult<ProviderDbo>> {
+    const result = await this._context.Providers
+      .select()
+      .where("id", id)
+      .first()
+    ;
+
+    if (isNil(result)) {
+      return OperationResultFactory.error(ERROR_CODES.NOT_FOUND);
+    }
+
+    return OperationResultFactory.success(result);
+  }
+
+}
