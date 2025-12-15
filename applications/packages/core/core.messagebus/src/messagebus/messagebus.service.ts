d6ff6cfb49a94be0a62d7b5f21d5430ef31cb858
kfarkasHU <sohamar@farkask.eu>
2025-12-15 20:46:41 +0100

    style(*): autoformat

--- applications/packages/core/core.messagebus/src/messagebus/messagebus.service.ts
+++ applications/packages/core/core.messagebus/src/messagebus/messagebus.service.ts
@@ -4,33 +4,33 @@ import { MessagebusCallbackFn, MessagebusListener } from "./messagebus.model";
 
 export class MessagebusService {
 
-    private readonly _listeners: Array<MessagebusListener> = [];
+  private readonly _listeners: Array<MessagebusListener> = [];
 
-    public removeListener(id: Guid): void {
-        const index = this._listeners.findIndex(m => m.id === id);
-        if (index > -1) {
-            this._listeners.splice(
-                index,
-                1
-            );
-        }
+  public removeListener(id: Guid): void {
+    const index = this._listeners.findIndex(m => m.id === id);
+    if (index > -1) {
+      this._listeners.splice(
+        index,
+        1
+      );
     }
+  }
 
-    public push<T>(token: string, data?: T): void {
-        this._listeners
-            .filter(m => m.token === token)
-            .forEach(m => m.callback(data))
-        ;
-    }
+  public push<T>(token: string, data?: T): void {
+    this._listeners
+      .filter(m => m.token === token)
+      .forEach(m => m.callback(data))
+      ;
+  }
 
-    public sub<T>(token: string, callback: MessagebusCallbackFn<T>): Guid {
-        const id = newGuid();
-        this._listeners.push({
-            id: id,
-            token: token,
-            callback: callback
-        });
-        return id;
-    }
+  public sub<T>(token: string, callback: MessagebusCallbackFn<T>): Guid {
+    const id = newGuid();
+    this._listeners.push({
+      id: id,
+      token: token,
+      callback: callback
+    });
+    return id;
+  }
 
 }
