b84a8c4164d076defb6d7cda4bb75e82811de842
kfarkasHU <sohamar@farkask.eu>
2025-12-15 19:30:29 +0100

    style(primitive): add eslint ignore

--- applications/packages/framework/framework.primitive/src/primitive/primitive.fn.ts
+++ applications/packages/framework/framework.primitive/src/primitive/primitive.fn.ts
@@ -1,5 +1,6 @@
 import { Primitive } from "./primitive.type";
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function isPrimitive(primitiveLike: any): primitiveLike is Primitive {
   const primitiveTypes = ["boolean", "string", "number"];
   return primitiveTypes.includes(typeof primitiveLike);
