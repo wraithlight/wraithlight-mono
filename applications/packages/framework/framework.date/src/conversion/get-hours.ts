90326784fc4d4722ba56bf069b8aa98c6e4c1e53
kfarkasHU <sohamar@farkask.eu>
2025-12-15 19:32:45 +0100

    style(*): autoformat

--- applications/packages/framework/framework.date/src/conversion/get-hours.ts
+++ applications/packages/framework/framework.date/src/conversion/get-hours.ts
@@ -2,5 +2,5 @@ import { MINUTE_IN_HOUR } from "./get-hours.const";
 import { getMinutes } from "./get-minutes";
 
 export function getHours(hours: number): number {
-    return hours * getMinutes(MINUTE_IN_HOUR);
+  return hours * getMinutes(MINUTE_IN_HOUR);
 }
--- applications/packages/framework/framework.date/src/date/add-days.ts
+++ applications/packages/framework/framework.date/src/date/add-days.ts
@@ -2,9 +2,9 @@ import { addHours } from "./add-hours";
 import { daysToHours } from "./operations";
 
 export function addDays(date: Date, days: number): Date {
-    const h = daysToHours(days);
-    return addHours(
-        date,
-        h
-    );
+  const h = daysToHours(days);
+  return addHours(
+    date,
+    h
+  );
 }
