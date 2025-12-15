fed3cb98a21b4e3f8ca28ca6238bf6076bbd7284
kfarkasHU <sohamar@farkask.eu>
2025-12-15 15:37:35 +0100

    style(*): autoformat

--- applications/packages/core/core.linq/src/property-name/get-property-name.fn.spec.ts
+++ applications/packages/core/core.linq/src/property-name/get-property-name.fn.spec.ts
@@ -4,28 +4,28 @@ import { getPropertyName } from "./get-property-name.fn";
 const PROP_NAME = "property";
 
 interface TestInterface {
-    [PROP_NAME]: string;
+  [PROP_NAME]: string;
 }
 
 const TEST_OBJECT: TestInterface = {
-    [PROP_NAME]: "wraithlight"
+  [PROP_NAME]: "wraithlight"
 }
 
 describe("getPropertyNameSpecs", () => {
 
-    const predicate: Predicate<TestInterface, typeof TEST_OBJECT.property> = (m) => m.property;
+  const predicate: Predicate<TestInterface, typeof TEST_OBJECT.property> = (m) => m.property;
 
-    describe("given the utility is initialized", () => {
-        describe("when i call the function with a valid predicate", () => {
-            let propName: string;
-            beforeEach(() => {
-                propName = getPropertyName(predicate);
-            });
-            it("should return the correct property name", () => {
-                expect(propName).toStrictEqual(PROP_NAME);
-            });
-        });
+  describe("given the utility is initialized", () => {
+    describe("when i call the function with a valid predicate", () => {
+      let propName: string;
+      beforeEach(() => {
+        propName = getPropertyName(predicate);
+      });
+      it("should return the correct property name", () => {
+        expect(propName).toStrictEqual(PROP_NAME);
+      });
     });
+  });
 
 
 });
