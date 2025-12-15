427cea5222de0e49df2959f11e949df49c7219ae
kfarkasHU <sohamar@farkask.eu>
2025-12-15 20:48:40 +0100

    style(*): autoformat

--- applications/packages/core/core.errors/src/already-initialized.error.ts
+++ applications/packages/core/core.errors/src/already-initialized.error.ts
@@ -3,15 +3,15 @@ import { HttpCode } from "@wraithlight/core.http";
 import { WraithlightError } from "./_wraithlight.error";
 
 export class AlreadyInitializedError extends WraithlightError {
-    public statusCode = HttpCode.InternalError;
-    public statusMessage = "E_ALREADY_INITIALIZED";
+  public statusCode = HttpCode.InternalError;
+  public statusMessage = "E_ALREADY_INITIALIZED";
 
-    constructor(
-        objectName: string
-    ) {
-        super(
-            "AlreadyInitializedError",
-            `Object: '${objectName}'`
-        );
-    }
+  constructor(
+    objectName: string
+  ) {
+    super(
+      "AlreadyInitializedError",
+      `Object: '${objectName}'`
+    );
+  }
 }
