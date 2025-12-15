e091fdec95f4a1142f865d989b610f00c855d3f6
kfarkasHU <sohamar@farkask.eu>
2025-12-15 15:29:33 +0100

    style(*): autoformat

--- applications/packages/core/core.crypto/src/sha/_internal/create-hash.spec.ts
+++ applications/packages/core/core.crypto/src/sha/_internal/create-hash.spec.ts
@@ -1,5 +1,5 @@
 jest.mock("crypto", () => ({
-    createHash: jest.fn()
+  createHash: jest.fn()
 }));
 import * as cryptoApi from "crypto";
 
@@ -11,28 +11,28 @@ import { DIGEST_HEX } from "./create-hash.const";
 
 describe("CreateHashSpecs", () => {
 
-    const digestSpy = jest.fn();
-    const updateSpy = jest.fn().mockReturnValue({ digest: digestSpy });
-    const mockHash = {
-        update: updateSpy,
-        digest: digestSpy
-    } as unknown as cryptoApi.Hash;
-    const createHashSpy = jest.spyOn(cryptoApi, "createHash").mockImplementation(() => mockHash);
-    const mockAlgorithm: CreateHashAlgorithm = "sha256";
+  const digestSpy = jest.fn();
+  const updateSpy = jest.fn().mockReturnValue({ digest: digestSpy });
+  const mockHash = {
+    update: updateSpy,
+    digest: digestSpy
+  } as unknown as cryptoApi.Hash;
+  const createHashSpy = jest.spyOn(cryptoApi, "createHash").mockImplementation(() => mockHash);
+  const mockAlgorithm: CreateHashAlgorithm = "sha256";
 
-    describe("given the service is initialized", () => {
-        describe("when calling it", () => {
-            beforeEach(() => {
-                hash(mockAlgorithm, SPEC_TEXT);
-            });
-            it("should call the underlying implementation", () => {
-                expect(createHashSpy).toHaveBeenCalledWith(mockAlgorithm);
-                expect(createHashSpy).toHaveBeenCalledTimes(1);
-                expect(updateSpy).toHaveBeenCalledTimes(1);
-                expect(updateSpy).toHaveBeenCalledWith(SPEC_TEXT);
-                expect(digestSpy).toHaveBeenCalledTimes(1);
-                expect(digestSpy).toHaveBeenCalledWith(DIGEST_HEX)
-            });
-        });
+  describe("given the service is initialized", () => {
+    describe("when calling it", () => {
+      beforeEach(() => {
+        hash(mockAlgorithm, SPEC_TEXT);
+      });
+      it("should call the underlying implementation", () => {
+        expect(createHashSpy).toHaveBeenCalledWith(mockAlgorithm);
+        expect(createHashSpy).toHaveBeenCalledTimes(1);
+        expect(updateSpy).toHaveBeenCalledTimes(1);
+        expect(updateSpy).toHaveBeenCalledWith(SPEC_TEXT);
+        expect(digestSpy).toHaveBeenCalledTimes(1);
+        expect(digestSpy).toHaveBeenCalledWith(DIGEST_HEX)
+      });
     });
+  });
 });
