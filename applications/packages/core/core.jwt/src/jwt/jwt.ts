09bfc364bcb48e1b3d949065a5df21003d4f44ce
kfarkasHU <sohamar@farkask.eu>
2025-12-15 15:31:36 +0100

    style(*): autoformat

--- applications/packages/core/core.jwt/src/jwt/jwt.ts
+++ applications/packages/core/core.jwt/src/jwt/jwt.ts
@@ -1,4 +1,4 @@
-import { Nullable, isNil } from "@wraithlight/core.nullable";
+import { Nullable, isNil } from "@wraithlight/framework.nullable";
 import JwtEncrypt from "jwt-encrypt";
 
 import { ALGORITHM } from "./jwt.const";
@@ -6,45 +6,45 @@ import { JWTDecryptResult } from "./jwt.model";
 
 export class CoreJWT {
 
-    public static encrypt<T extends object>(
-        payload: T,
-        iv: string,
-        key: string,
-        secret: string,
-        expiryInMinutes: number
-    ): string {
-        return JwtEncrypt.sign(
-            payload,
-            secret,
-            { iv, key, algorithm: ALGORITHM },
-            { expiresIn: `${expiryInMinutes}min` }
-        );
+  public static encrypt<T extends object>(
+    payload: T,
+    iv: string,
+    key: string,
+    secret: string,
+    expiryInMinutes: number
+  ): string {
+    return JwtEncrypt.sign(
+      payload,
+      secret,
+      { iv, key, algorithm: ALGORITHM },
+      { expiresIn: `${expiryInMinutes}min` }
+    );
+  }
+
+  public static decrypt<T extends object>(
+    token: string,
+    iv: string,
+    key: string,
+  ): JWTDecryptResult<T> {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const result: Nullable<T> = JwtEncrypt.decode(
+      token,
+      {
+        iv,
+        key,
+        algorithm: ALGORITHM
+      }
+    );
+    if (isNil(result)) {
+      return {
+        success: false
+      };
     }
 
-    public static decrypt<T extends object>(
-        token: string,
-        iv: string,
-        key: string,
-    ): JWTDecryptResult<T> {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        const result: Nullable<T> = JwtEncrypt.decode(
-            token,
-            {
-                iv,
-                key,
-                algorithm: ALGORITHM
-            }
-        );
-        if (isNil(result)) {
-            return {
-                success: false
-            };
-        }
-
-        return {
-            success: true,
-            payload: result
-        };
-    }
+    return {
+      success: true,
+      payload: result
+    };
+  }
 
 }
