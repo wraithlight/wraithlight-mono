f4f227bd570f996207074e437735622a377cdaf6
kfarkasHU <sohamar@farkask.eu>
2025-12-16 19:30:56 +0100

    feat(*): add provider types
    Ref: #1702

---
+++ applications/packages/communications/core.communications.email-sender.types/src/request/provider.model.ts
@@ -0,0 +1,12 @@
+import {
+  ProviderCreateRequestModel,
+  ProviderUpdateRequestModel
+} from "@wraithlight/core.communications.types";
+
+// eslint-disable-next-line max-len
+export interface ESSProviderCreateRequestModel extends ProviderCreateRequestModel {
+}
+
+// eslint-disable-next-line max-len
+export interface ESSProviderUpdateRequestModel extends ProviderUpdateRequestModel {
+}
---
+++ applications/packages/communications/core.communications.email-sender.types/src/response/provider.model.ts
@@ -0,0 +1,22 @@
+import {
+  ProviderCreateResponseModel,
+  ProviderUpdateResponseModel,
+  ProviderGetResponseModel,
+  ProviderListResponseModel,
+} from "@wraithlight/core.communications.types";
+
+// eslint-disable-next-line max-len
+export interface ESSProviderCreateResponseModel extends ProviderCreateResponseModel {
+}
+
+// eslint-disable-next-line max-len
+export interface ESSProviderUpdateResponseModel extends ProviderUpdateResponseModel {
+}
+
+// eslint-disable-next-line max-len
+export interface ESSProviderGetResponseModel extends ProviderGetResponseModel {
+}
+
+// eslint-disable-next-line max-len
+export interface ESSProviderListResponseModel extends ProviderListResponseModel {
+}
