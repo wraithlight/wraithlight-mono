8b7179fc73cefa533d808f70f0941ff9eb788332
kfarkasHU <sohamar@farkask.eu>
2025-12-18 21:49:51 +0100

    feat(*): add Provider models
    Ref: #1703

---
+++ applications/packages/communications/core.communications.sms-sender.types/src/request/provider.model.ts
@@ -0,0 +1,12 @@
+import {
+  ProviderCreateRequestModel,
+  ProviderUpdateRequestModel
+} from "@wraithlight/core.communications.types";
+
+// eslint-disable-next-line max-len
+export interface SSSProviderCreateRequestModel extends ProviderCreateRequestModel {
+}
+
+// eslint-disable-next-line max-len
+export interface SSSProviderUpdateRequestModel extends ProviderUpdateRequestModel {
+}
---
+++ applications/packages/communications/core.communications.sms-sender.types/src/response/provider.model.ts
@@ -0,0 +1,22 @@
+import {
+  ProviderCreateResponseModel,
+  ProviderGetResponseModel,
+  ProviderListResponseModel,
+  ProviderUpdateResponseModel,
+} from "@wraithlight/core.communications.types";
+
+// eslint-disable-next-line max-len
+export interface SSSProviderCreateResponseModel extends ProviderCreateResponseModel {
+}
+
+// eslint-disable-next-line max-len
+export interface SSSProviderUpdateResponseModel extends ProviderUpdateResponseModel {
+}
+
+// eslint-disable-next-line max-len
+export interface SSSProviderGetResponseModel extends ProviderGetResponseModel {
+}
+
+// eslint-disable-next-line max-len
+export interface SSSProviderListResponseModel extends ProviderListResponseModel {
+}
