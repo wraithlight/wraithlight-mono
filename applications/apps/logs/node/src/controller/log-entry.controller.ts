a395ca21edccb2e2683e99cfcc0e3e9c4e46d65e
kfarkasHU <sohamar@farkask.eu>
2025-12-18 22:51:43 +0100

    style(*): autoformat

--- applications/apps/logs/node/src/controller/log-entry.controller.ts
+++ applications/apps/logs/node/src/controller/log-entry.controller.ts
@@ -1,9 +1,9 @@
 import { LOGGER_API_ENDPOINTS } from "@wraithlight/core.logs.constant";
 import { BeaconLogEntry } from "@wraithlight/core.logs.types";
 import {
-    BaseController,
-    HttpController,
-    HttpPost
+  BaseController,
+  HttpController,
+  HttpPost
 } from "@wraithlight/core.node";
 
 import { LogsService } from "../service";
@@ -11,16 +11,16 @@ import { LogsService } from "../service";
 @HttpController(LOGGER_API_ENDPOINTS.v1.logs.root)
 export class LogsEntryController extends BaseController {
 
-    private readonly _logsService = new LogsService();
+  private readonly _logsService = new LogsService();
 
-    @HttpPost(LOGGER_API_ENDPOINTS.v1.logs.create)
-    public async logEntry(dto: BeaconLogEntry): Promise<void> {
-        await this._logsService.log(
-            dto.severity,
-            dto.data,
-            dto.applicationName
-        );
-        super.created();
-    }
+  @HttpPost(LOGGER_API_ENDPOINTS.v1.logs.create)
+  public async logEntry(dto: BeaconLogEntry): Promise<void> {
+    await this._logsService.log(
+      dto.severity,
+      dto.data,
+      dto.applicationName
+    );
+    super.created();
+  }
 
 }
