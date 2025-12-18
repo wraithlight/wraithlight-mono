7a7d17f24d69ea333cdaa2790dc759e2f6a99e6a
kfarkasHU <sohamar@farkask.eu>
2025-12-18 21:58:08 +0100

    feat(*): add Provider models
    Ref: #1704

--- applications/packages/communications/core.communications.push-sender.types/src/request/index.ts
+++ applications/packages/communications/core.communications.push-sender.types/src/request/index.ts
@@ -1,1 +1,2 @@
+export * from "./provider.model";
 export * from "./send.model";
---
+++ applications/packages/communications/core.communications.push-sender.types/src/response/provider.model.ts
@@ -0,0 +1,22 @@
+import {
+  ProviderCreateResponseModel,
+  ProviderGetResponseModel,
+  ProviderListResponseModel,
+  ProviderUpdateResponseModel,
+} from "@wraithlight/core.communications.types";
+
+// eslint-disable-next-line max-len
+export interface PSSProviderCreateResponseModel extends ProviderCreateResponseModel {
+}
+
+// eslint-disable-next-line max-len
+export interface PSSProviderUpdateResponseModel extends ProviderUpdateResponseModel {
+}
+
+// eslint-disable-next-line max-len
+export interface PSSProviderGetResponseModel extends ProviderGetResponseModel {
+}
+
+// eslint-disable-next-line max-len
+export interface PSSProviderListResponseModel extends ProviderListResponseModel {
+}
