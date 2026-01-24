29c9df29d9151efbbe811208ad59f0aa3ffe126f
kfarkasHU <sohamar@farkask.eu>
2026-01-24 14:23:00 +0100

    style(*): autoformat

--- applications/packages/notifier/core.notifier.const/src/const/endpoints.const.ts
+++ applications/packages/notifier/core.notifier.const/src/const/endpoints.const.ts
@@ -3,10 +3,10 @@ const API_V1_BASE = `${API_BASE}/v1`;
 const API_V1_SEND_BASE = `${API_V1_BASE}/send`;
 
 export const NOTIFIER_ENDPOINT_CONST = Object.freeze({
-    v1: {
-        send: {
-            root: API_V1_SEND_BASE,
-            mail: "/mail"
-        }
+  v1: {
+    send: {
+      root: API_V1_SEND_BASE,
+      mail: "/mail"
     }
+  }
 });
--- applications/packages/notifier/core.notifier.const/src/const/webhook.const.ts
+++ applications/packages/notifier/core.notifier.const/src/const/webhook.const.ts
@@ -3,15 +3,15 @@ const API_V1_BASE = `${API_BASE}/v1`;
 const API_V1_MAIL_BASE = `${API_V1_BASE}/mail`;
 
 export const NOTIFIER_WEBHOOK_ENDPOINTS = Object.freeze({
-    v1: {
-        webhooks: {
-            mail: {
-                root: API_V1_MAIL_BASE,
-                processingStarted: "/start",
-                processingSucceeded: "/succeed",
-                processingFailed: "/fail",
-                processingDone: "/done"
-            }
-        }
+  v1: {
+    webhooks: {
+      mail: {
+        root: API_V1_MAIL_BASE,
+        processingStarted: "/start",
+        processingSucceeded: "/succeed",
+        processingFailed: "/fail",
+        processingDone: "/done"
+      }
     }
+  }
 });
